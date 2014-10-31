4aa67014cc032a02472af99cf8ecd7240327d565
Wouter Beek <me@wouterbeek.com>
2014-10-31 10:02:44 +0100

    Spacing.

--- index.pl
+++ index.pl
@@ -1,11 +1,11 @@
 % Index for the plRdf project.
+% This assumes that file search path `plRdf` is set.
 
-user:file_search_path(plRdf_conv, plRdf(conversion)).
-user:file_search_path(plRdf_ent, plRdf(entailment)).
-user:file_search_path(plRdf_mt, plRdf(model_theory)).
-user:file_search_path(plRdf_owl, plRdf(owl)).
+user:file_search_path(plRdf_conv,  plRdf(conversion)).
+user:file_search_path(plRdf_ent,   plRdf(entailment)).
+user:file_search_path(plRdf_mt,    plRdf(model_theory)).
+user:file_search_path(plRdf_owl,   plRdf(owl)).
 user:file_search_path(plRdf_parse, plRdf(parse)).
-user:file_search_path(plRdf_rei, plRdf(reification)).
-user:file_search_path(plRdf_ser, plRdf(serialization)).
-user:file_search_path(plRdf_term, plRdf(term)).
-
+user:file_search_path(plRdf_rei,   plRdf(reification)).
+user:file_search_path(plRdf_ser,   plRdf(serialization)).
+user:file_search_path(plRdf_term,  plRdf(term)).
