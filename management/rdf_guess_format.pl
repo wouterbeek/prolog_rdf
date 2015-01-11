d09bc9d93b4182ee66fde0559f43005cce73f297
Wouter Beek <me@wouterbeek.com>
2015-01-11 13:40:22 +0100

    FIXED: Allow many more erroneous documents to be guessed for the RDF serialization format that comes closest.

--- management/rdf_guess_format.pl
+++ management/rdf_guess_format.pl
@@ -168,187 +168,98 @@ rdf_guess_format(In, _, _, Format):-
 turtle_like(Format, Options) -->
   dcg_end,
   {option(eos(true), Options)}, !,
-  turtle_or_trig(Format, Options).
+  nt_turtle_or_trig(Format, Options).
 turtle_like(Format, Options) -->
   blank, !, blanks,
   turtle_like(Format, Options).
+% Turtle comment.
 turtle_like(Format, Options) -->
   "#", !, skip_line,
   turtle_like(Format, Options).
+% @BASE
+% @PREFIX
 turtle_like(Format, Options) -->
   "@", icase_keyword(Keyword), {turtle_keyword(Keyword)}, !,
-  turtle_or_trig(Format, Options).
+  nt_turtle_or_trig(Format, Options).
+% BASE
+% PREFIX
 turtle_like(Format, Options) -->
-  "PREFIX", blank, !,
-  turtle_or_trig(Format, Options).
+  icase_keyword(Keyword), {turtle_keyword(Keyword)}, blank, !,
+  nt_turtle_or_trig(Format, Options).
+% Turtle triple.
 turtle_like(Format, Options) -->
-  "BASE", blank, !,
-  turtle_or_trig(Format, Options).
-turtle_like(Format, Options) -->
-  nt_subject, 'nt_whites+', nt_predicate, 'nt_whites+', nt_object,
-  'nt_whites+',
+  nt_subject,
+  nt_whites,
+  nt_predicate,
+  nt_whites,
+  nt_object,
+  nt_whites,
   (   "."
   ->  nt_end,
       nt_turtle_like(Format, Options)
   ;   ";"
   ->  nt_end,
-      nt_turtle_like(Format, Options)
-  ;   iriref, 'nt_whites+', ".", nt_end
+      nt_turtle_or_trig(Format, Options)
+  ;   nt_graph, nt_whites, ".", nt_end
   ->  {Format = nquads}
   ).
-% starts with a blank node
+% Turtle anonymous blank node.
 turtle_like(Format, Options) -->
   "[", !,
-  turtle_or_trig(Format, Options).
-% starts with a collection
+  nt_turtle_or_trig(Format, Options).
+% Turtle collection.
 turtle_like(Format, Options) -->
   "(", !,
-  turtle_or_trig(Format, Options).
+  nt_turtle_or_trig(Format, Options).
 
-nt_subject --> iriref.
-nt_subject --> nt_bnode.
+nt_bnode --> "_:", nt_bnode_codes.
 
-nt_bnode --> "_:", nt_bnode_first, nt_bnode_latters.
+nt_bnode_codes --> blank, !, {fail}.
+nt_bnode_codes --> [_], !, nt_bnode_codes.
+nt_bnode_codes --> [].
 
-nt_bnode_first --> 'PN_CHARS_U'(turtle, _).
-nt_bnode_first --> decimal_digit.
+nt_graph --> nt_iriref.
 
-nt_bnode_latters -->
-  nt_bnode_latter, !,
-  nt_bnode_latters.
-nt_bnode_latters --> [].
+nt_iriref --> "<", nt_iri_codes, ">".
 
-nt_bnode_latter --> 'PN_CHARS_U'(turtle, _).
-nt_bnode_latter --> ".".
+nt_iri_codes --> nt_iri_code, !, nt_iri_codes.
+nt_iri_codes --> [].
 
-turtle_keyword(base).
-turtle_keyword(prefix).
+nt_iri_code --> ">", !, {fail}.
+nt_iri_code --> [_].
 
-%! turtle_or_trig(-Format, +Options)//
-%
-%  The file starts with a Turtle construct.   It can still be TriG.
-%  We trust the content type and otherwise  we assume TriG if there
-%  is a "{" in the first section of the file.
+nt_langtag --> blank, !, {fail}.
+nt_langtag --> [_], nt_langtag.
+nt_langtag --> "".
 
-turtle_or_trig(Format, Options) -->
-  {
-    option(format(Format), Options),
-    turtle_or_trig(Format)
-  }, !.
-turtle_or_trig(Format, _) -->
-  (   ..., "{"
-  ->  {Format = trig}
-  ;   {Format = turtle}
-  ).
-
-turtle_or_trig(turtle).
-turtle_or_trig(trig).
-
-... --> "" | [_], ... .
-
-%! nt_turtle_like(
-%!   -Format:oneof([nquads,ntriples,turtle,trig]),
-%!   +Options:list(nvpair)
-%! )//
-% We found a fully qualified triple.
-% This still can be Turtle, TriG, N-Triples or N-Quads.
-
-nt_turtle_like(Format, Options) -->
-  {
-    option(format(Format), Options),
-    nt_turtle_like(Format)
-  }, !.
-nt_turtle_like(ntriples, _) -->
-  "".
-
-nt_turtle_like(turtle).
-nt_turtle_like(trig).
-nt_turtle_like(ntriples).
-nt_turtle_like(nquads).
-
-iriref --> "<", iri_codes, ">".
-
-nt_predicate --> `a`.
-nt_predicate --> iriref.
-
-nt_object --> iriref, !.
+nt_object --> nt_iriref, !.
 nt_object --> nt_bnode, !.
 nt_object -->
   nt_string,
   (   "^^"
-  ->  iriref
+  ->  nt_iriref
   ;   "@"
-  ->  langtag
-  ;
-    {true}
+  ->  nt_langtag
+  ;   ""
   ).
 
-iri_codes --> iri_code, !, iri_codes.
-iri_codes --> [].
+nt_predicate --> "a".
+nt_predicate --> nt_iriref.
 
-iri_code -->
-  [C],
-  {(   C =< 0'\s
-    ;   no_iri_code(C)
-    ), !, fail
-  }.
-iri_code -->
-  "\\",
-  (   "u"
-  ->  xdigit4
-  ;   "U"
-  ->  xdigit8
-  ).
-iri_code -->
-  [_].
+nt_string --> "'", nt_string_codes([39]), "'".
+nt_string --> "\"", nt_string_codes([34]), "\"".
+nt_string --> "'''", nt_string_codes([39,39,39]), "'''".
+nt_string --> "\"\"\"", nt_string_codes([34,34,34]), "\"\"\"".
 
-langtag --> az, azs, sublangs.
+nt_string_codes(End) --> End, !, {fail}.
+nt_string_codes(End) --> [_], !, nt_string_codes(End).
+nt_string_codes(_) --> [].
 
-sublangs --> "-", !, azd, azds, sublangs.
-sublangs --> "".
+nt_subject --> nt_iriref.
+nt_subject --> nt_bnode.
 
-az   --> [C], {between(0'a,0'z,C) ; between(0'A,0'Z,C)}, !.
-azd  --> [C], {between(0'a,0'z,C) ; between(0'A,0'Z,C), between(0'0,0'9,C)}, !.
-azs  --> az, !, azs | "".
-azds --> azd, !, azds | "".
-
-term_expansion(no_iri_code(x), Clauses):-
-  findall(no_iri_code(C),
-    string_code(_,"<>\"{}|^`\\",C),
-    Clauses).
-term_expansion(echar(x), Clauses):-
-  findall(echar(C),
-    string_code(_,"tbnrf\"\'\\",C),
-    Clauses).
-
-no_iri_code(x).
-echar(x).
-
-xdigit2 --> xdigit(_), xdigit(_).
-xdigit4 --> xdigit2, xdigit2.
-xdigit8 --> xdigit4, xdigit4.
-
-nt_string --> "\"", nt_string_codes, "\"".
-
-nt_string_codes --> string_code, !, nt_string_codes.
-nt_string_codes --> [].
-
-string_code --> "\"", !, {fail}.
-string_code --> "\n", !, {fail}.
-string_code --> "\r", !, {fail}.
-string_code --> "\\", !,
-  (   "u"
-  ->  xdigit4
-  ;   "U"
-  ->  xdigit8
-  ;   [C],
-      {echar(C)}
-  ).
-string_code --> [_].
-
-'nt_whites+' --> nt_white, 'nt_whites*'.
-'nt_whites*' --> nt_white, 'nt_whites*'.
-'nt_whites*' --> [].
+nt_whites --> nt_white, !, nt_whites.
+nt_whites --> [].
 
 nt_white --> [10], !.
 nt_white --> [13], !.
@@ -373,6 +284,50 @@ eol1 --> "\r".
 
 eols --> eol1, !, eols.
 eols --> [].
+
+turtle_keyword(base).
+turtle_keyword(prefix).
+
+%! nt_turtle_or_trig(-Format, +Options)//
+%
+%  The file starts with a Turtle construct.   It can still be TriG.
+%  We trust the content type and otherwise  we assume TriG if there
+%  is a "{" in the first section of the file.
+
+nt_turtle_or_trig(Format, Options) -->
+  {
+    option(format(Format), Options),
+    nt_turtle_or_trig(Format)
+  }, !.
+nt_turtle_or_trig(Format, _) -->
+  (   ..., "{"
+  ->  {Format = trig}
+  ;   {Format = turtle}
+  ).
+
+nt_turtle_or_trig(turtle).
+nt_turtle_or_trig(trig).
+
+%! nt_turtle_like(
+%!   -Format:oneof([nquads,ntriples,turtle,trig]),
+%!   +Options:list(nvpair)
+%! )//
+% We found a fully qualified triple.
+% This still can be Turtle, TriG, N-Triples or N-Quads.
+
+nt_turtle_like(Format, Options) -->
+  {
+    option(format(Format), Options),
+    nt_turtle_like(Format)
+  }, !.
+nt_turtle_like(ntriples, _) -->
+  "".
+
+nt_turtle_like(turtle).
+nt_turtle_like(trig).
+nt_turtle_like(ntriples).
+nt_turtle_like(nquads).
+
 
 
      /*******************************
@@ -466,6 +421,8 @@ on_cdata(_CDATA, _Parser):-
      /*******************************
      *      DCG BASICS    *
      *******************************/
+
+... --> "" | [_], ... .
 
 skip_line --> eol, !.
 skip_line --> [_], skip_line.
