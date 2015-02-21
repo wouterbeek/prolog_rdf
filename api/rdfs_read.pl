e1d60b57a662c0a22202666f489e1da0999d0058
Wouter Beek <me@wouterbeek.com>
2015-02-21 18:53:34 +0100

    DEPLOYMENT: Moved statistics and read API predicates that require some form of RDF(S) entailment to library plRdfEntailment.

--- api/rdfs_read.pl
+++ api/rdfs_read.pl
@@ -1,227 +1,37 @@
 :- module(
   rdfs_read,
   [
-    rdf_property/1, % ?Property:iri
-    rdf_property/2, % ?Property:iri
-                    % ?Graph:atom
-    rdfs_class/1, % ?Class:iri
-    rdfs_class/2, % ?Class:iri
-                  % ?Graph:atom
-    rdfs_datatype/1, % ?Datatype:iri
-    rdfs_datatype/2, % ?Datatype:iri
-                     % ?Graph:atom
-    rdfs_instance/2, % ?Instance:rdf_term
-                     % ?Class:iri
     rdfs_label/4, % ?Term:rdf_term
                   % ?Value
                   % ?LangTagPreference:list(list(atom))
                   % ?Graph:atom
     rdfs_label_value/2, % ?Term:rdf_term
                         % ?Label:atom
-    rdfs_label_value/4, % ?Term:rdf_term
-                        % ?Label:atom
-                        % ?LangTagPreference:list(list(atom))
-                        % ?Graph:atom
-    rdfs_reachable/3, % ?Subject:or([bnode,iri]),
-                      % ?Predicate:iri,
-                      % ?Object:rdf_term
-    rdfs_subclass/2, % ?Subclass:iri
-                     % ?Superclass:iri
-    rdfs_subproperty/2 % ?Subproperty:iri
-                       % ?Superproperty:iri
+    rdfs_label_value/4 % ?Term:rdf_term
+                       % ?Label:atom
+                       % ?LangTagPreference:list(list(atom))
+                       % ?Graph:atom
   ]
 ).
 
-/** <module> RDF API: Read RDFS constructs
+/** <module> RDFS Read API
 
 @author Wouter Beek
-@version 2014/11-2015/01
+@version 2014/11-2015/02
 */
 
-:- use_module(library(lists), except([delete/3])).
+:- use_module(library(lists), except([delete/3,subset/2])).
 :- use_module(library(semweb/rdf_db), except([rdf_node/1])).
-
-:- use_module(generics(closure)).
 
 :- use_module(plRdf(api/rdf_read)).
 :- use_module(plRdf(term/rdf_term)).
 
-:- rdf_meta(rdf_property(r)).
-:- rdf_meta(rdf_property(r,?)).
-:- rdf_meta(rdfs_class(r)).
-:- rdf_meta(rdfs_class(r,?)).
-:- rdf_meta(rdfs_datatype(r)).
-:- rdf_meta(rdfs_datatype(r,?)).
-:- rdf_meta(rdfs_has(r,r,o)).
-:- rdf_meta(rdfs_instance(o,r)).
 :- rdf_meta(rdfs_label(o,?,?,?)).
 :- rdf_meta(rdfs_label(o,?,?,?)).
 :- rdf_meta(rdfs_label_value(o,?)).
 :- rdf_meta(rdfs_label_value(o,?,?,?)).
-:- rdf_meta(rdfs_reachable(r,r,o)).
-:- rdf_meta(rdfs_subclass(r,r)).
-:- rdf_meta(rdfs_subproperty(r,r)).
-% Private.
-:- rdf_meta(rdf_class_term(r)).
-:- rdf_meta(rdf_property_term(r)).
-:- rdf_meta(rdfs_class_term(r)).
-:- rdf_meta(rdfs_property_term(r)).
 
 
-
-
-
-%! rdf_property(+Property:iri) is semidet.
-%! rdf_property(-Property:iri) is multi.
-
-rdf_property(Property):-
-  rdfs_instance(Property, rdfs:'Property').
-
-%! rdf_property(?Property:iri, ?Graph:atom) is nondet
-
-rdf_property(Property, Graph):-
-  rdf_property(Property),
-  once(rdf_term(Property, Graph)).
-
-
-
-%! rdfs_class(+Class:iri) is semidet.
-%! rdfs_class(-Class:iri) is multi.
-
-rdfs_class(Class):-
-  rdfs_instance(Class, rdfs:'Class').
-
-%! rdfs_class(?Class:iri, ?Graph:atom) is nondet
-
-rdfs_class(Class, Graph):-
-  rdfs_class(Class),
-  once(rdf_term(Class, Graph)).
-
-
-
-%! rdfs_datatype(+Datatype:iri) is semidet.
-%! rdfs_datatype(-Datatype:iri) is multi.
-
-rdfs_datatype(Datatype):-
-  rdfs_instance(Datatype, rdfs:'Datatype').
-
-%! rdfs_datatype(?Datatype:iri, ?Graph:atom) is nondet
-
-rdfs_datatype(Datatype, Graph):-
-  rdfs_datatype(Datatype),
-  once(rdf_term(Datatype, Graph)).
-
-
-
-%! rdfs_has(
-%!   ?Subject:or([bnode,iri]),
-%!   ?Predicate:iri,
-%!   ?Object:rdf_term
-%! ) is nondet.
-
-rdfs_has(S, P, O):-
-  rdfs_subproperty(P0, P),
-  rdf(S, P0, O).
-
-
-
-%! rdfs_instance(+Resource, +Class) is semidet.
-%! rdfs_instance(+Resource, -Class) is nondet.
-%! rdfs_instance(-Resource, +Class) is nondet.
-% Generate resources belonging to a class   or  classes a resource
-% belongs to. We assume everything at the `object' end of a triple
-% is a class. A validator should confirm this property.
-%
-% rdfs_instance_of(+, -) does  not  exploit   domain  and  range
-% properties, deriving that if rdf(R,  P,   _)  is  present R must
-% satisfy the domain of P (and similar for range).
-%
-% There are a few hacks:
-%   - Any resource is an individual of rdfs:Resource
-%   - literal(_) is an individual of rdfs:Literal
-
-% [1a] Datatypes: required.
-% "The datatype IRIs `rdf:langString` and `xsd:string` MUST be recognized
-%  by all RDF interpretations." [RDF 1.1 Semantics]
-rdfs_instance(rdf:langString, rdfs:'Datatype').
-rdfs_instance(xsd:string, rdfs:'Datatype').
-
-% [1b] Datatypes: optional.
-% "Two other datatypes `rdf:XMLLiteral` and `rdf:HTML` are defined in
-%  [RDF11-CONCEPTS]. RDF-D interpretations MAY fail to recognize these
-%  datatypes." [RDF 1.1 Semantics]
-rdfs_instance(rdf:'XMLLiteral', rdfs:'Datatype').
-rdfs_instance(rdf:'HTML', rdfs:'Datatype').
-
-% [1c] Datatypes: occurring in the data.
-% "for every IRI aaa in D, I(aaa) is in ICEXT(I(rdfs:Datatype))"
-% "When using RDFS semantics, the referents of all recognized datatype IRIs
-%  can be considered to be in the class rdfs:Datatype." [RDF 1.1 Semantics]
-rdfs_instance(Datatype, rdfs:'Datatype'):-
-  rdf_current_literal(literal(type(Datatype,_))),
-  \+ rdf_equal(Datatype, rdf:langString),
-  \+ rdf_equal(Datatype, xsd:string),
-  \+ rdf_equal(Datatype, rdf:'XMLLiteral'),
-  \+ rdf_equal(Datatype, rdf:'HTML').
-
-% [2] Datatyped values.
-rdfs_instance(literal(type(Datatype,LexicalForm)), Datatype):-
-  rdf_current_literal(type(Datatype,LexicalForm)).
-
-% [3a] Property: RDF vocabulary.
-rdfs_instance(Property, rdf:'Property'):-
-  rdf_property_term(Property).
-% [3b] Property: RDFS vocabulary.
-rdfs_instance(Property, rdf:'Property'):-
-  rdfs_property_term(Property).
-% [3c] Property: RDFS vocabulary deduction.
-rdfs_instance(Property, rdf:'Property'):-
-  rdf(_, Property, _).
-% [3d] Property: RDFS vocabulary deduction.
-rdfs_instance(Property, rdf:'Property'):-
-  rdf_has(Property, rdfs:subPropertyOf, _).
-rdfs_instance(Property, rdf:'Property'):-
-  rdf_has(_, rdfs:subPropertyOf, Property).
-% [3e] Property: occurrence in the data.
-rdfs_instance(Property, rdf:'Property'):-
-  rdf_current_predicate(Property),
-  \+ rdf_property_term(Property),
-  \+ rdfs_property_term(Property).
-
-% [4] Resource: IRIs.
-rdfs_instance(Resource, rdfs:'Resource'):-
-  rdf_resource(Resource).
-
-% [5a] Class: RDF vocabulary.
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_class_term(Class).
-% [5b] Class: RDFS vocabulary.
-rdfs_instance(Class, rdfs:'Class'):-
-  rdfs_class_term(Class).
-% [5c] Class: RDF vocabulary deduction.
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(_, rdf:type, Class).
-% [5d] Class: RDFS vocabulary deduction.
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(Class, rdf:type, rdfs:'Datatype').
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(_, rdfs:domain, Class).
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(_, rdfs:range, Class).
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(Class, rdfs:subClassOf, _).
-rdfs_instance(Class, rdfs:'Class'):-
-  rdf_has(_, rdfs:subClassOf, Class).
-
-% [6a] RDF list: RDF vocabulary.
-% "no special semantic conditions are imposed on this vocabulary
-%  other than the type of `rdf:nil` being `rdf:List`." [RDF 1.1 Semantics]
-rdfs_instance(rdf:nil, rdf:'List').
-% [6b] RDF list: RDFS vocabulary.
-rdfs_instance(List, rdf:'List'):-
-  rdf(List, rdf:first, _).
-rdfs_instance(List, rdf:'List'):-
-  rdf(List, rdf:rest, _).
 
 
 
@@ -275,47 +85,6 @@ rdfs_label_value(Term, Label, LangPrefs, Graph):-
 
 rdfs_reachable(S, P, O):-
   closure0(rdfs_has0(P), S, O).
-
-
-
-%! rdfs_sublass(?Subclass:iri, ?Superclass:iri) is nondet.
-
-rdfs_subclass(Class, Class):-
-  rdfs_instance(Class, rdfs:'Class').
-rdfs_subclass(C1, C2):-
-  rdf_global_id(rdfs:subClassOf, P),
-  (   nonvar(C1)
-  ->  closure0(rdfs_has0(P), C1, C2)
-  ;   closure0(rdfs_has_backward0(P), C1, C2)
-  ).
-
-
-
-%! rdfs_subproperty(+Subproperty:iri, +Superproperty:iri) is semidet.
-%! rdfs_subproperty(+Subproperty:iri, -Superproperty:iri) is multi.
-%! rdfs_subproperty(-Subproperty:iri, +Superproperty:iri) is multi.
-
-% [1] Reflexive-transitive closure of the subproperty hierarchy.
-rdfs_subproperty(P1, P2):-
-  subproperty_closure(Qs),
-  (   nonvar(P1)
-  ->  closure0(rdfs_forward(Qs), P1, P2)
-  ;   closure0(rdfs_backward(Qs), P2, P1)
-  ).
-% [2] Container membership properties.
-% "If x is in ICEXT(I(rdfs:ContainerMembershipProperty)) then:
-%  < x, I(rdfs:member) > is in IEXT(I(rdfs:subPropertyOf))"
-% [RDF 1.1 Semantics]
-rdfs_subproperty(P, rdfs:member):-
-  rdfs_instance(P, rdfs:'ContainerMembershipProperty').
-
-rdfs_forward(Qs, X, Y):-
-  member(Q, Qs),
-  rdf(X, Q, Y).
-
-rdfs_backward(Qs, X, Y):-
-  member(Q, Qs),
-  rdf(Y, Q, X).
 
 
 
--- vocabulary/rdf_stat.pl
+++ vocabulary/rdf_stat.pl
@@ -1,46 +1,38 @@
 :- module(
   rdf_stat,
   [
-    count_classes/2, % +Graph:atom
-                     % -Count:nonneg
-    count_entities/2, % +Graph:atom
-                      % -Count:nonneg
-    count_individuals/3, % +Class:iri
-                         % +Graph:atom
-                         % -NumberOfIndividuals:nonneg
+    iris_by_graph/2, % ?Graph:atom
+                     % -NumberOfDistinctIris:nonneg
     count_objects/4, % ?Subject:or([bnode,iri])
                      % ?Predicate:iri
                      % +Graph:atom
-                     % -Count:nonneg
-    count_properties/4, % ?Subject:or([bnode,iri])
+                     % -NumberOfDistinctObjectTerms:nonneg
+    count_predicates/4, % ?Subject:or([bnode,iri])
                         % ?Object:rdf_term
                         % +Graph:atom
-                        % -Count:nonneg
+                        % -NumberOfDistinctPredicateTerms:nonneg
     count_subjects/4, % ?Predicate:iri
                       % ?Object:rdf_term
-                      % +Graph:atom
-                      % -Count:nonneg
-    rdf_property_table/3, % +Property:iri
-                          % +Graph:atom
-                          % -Table:list(list)
-    rdf_triples_by_datatype/3, % ?RdfGraph:atom
+                      % ?Graph:atom
+                      % -NumberOfDistinctSubjectTerms:nonneg
+    rdf_triples_by_datatype/3, % ?Graph:atom
                                % ?Datatype:iri
                                % -NumberOfTriples:nonneg
-    rdf_triples_by_object/3, % ?RdfGraph:atom
+    rdf_triples_by_object/3, % ?Graph:atom
                              % ?Object:rdf_term
                              % -NumberOfTriples:nonneg
     rdf_triples_by_pattern/5, % ?Subject:or([bnode,iri])
                               % ?Predicate:iri
                               % ?Object:rdf_term
-                              % ?RdfGraph:atom
+                              % ?Graph:atom
                               % -NumberOfTriples:nonneg
-    rdf_triples_by_predicate/3, % ?RdfGraph:atom
+    rdf_triples_by_predicate/3, % ?Graph:atom
                                 % ?Predicate:iri
                                 % -NumberOfTriples:nonneg
-    rdf_triples_by_subject/3, % ?RdfGraph:atom
+    rdf_triples_by_subject/3, % ?Graph:atom
                               % ?Subject:or([bnode,iri])
                               % -NumberOfTriples:nonneg
-    rdf_triples_by_term/3 % ?RdfGraph:atom
+    rdf_triples_by_term/3 % ?Graph:atom
                           % ?RdfTerm:rdf_term
                           % -NumberOfTriples:nonneg
   ]
@@ -53,7 +45,7 @@ Statistics for RDF data.
 @author Wouter Beek
 @see Based on the definitions in section 4.6 of the VoID W3C specification,
      http://www.w3.org/TR/2011/NOTE-void-20110303/
-@version 2013/01, 2013/03-2013/04, 2013/07, 2013/09, 2014/03
+@version 2013/01, 2013/03-2013/04, 2013/07, 2013/09, 2014/03, 2015/02
 */
 
 :- use_module(library(aggregate)).
@@ -66,175 +58,90 @@ Statistics for RDF data.
 :- use_module(plRdf(graph/rdf_graph)).
 :- use_module(plRdf(term/rdf_term)).
 
-:- rdf_meta(count_individuals(r,+,-)).
 :- rdf_meta(count_objects(r,r,+,-)).
-:- rdf_meta(count_properties(r,r,+,-)).
+:- rdf_meta(count_predicates(r,r,+,-)).
 :- rdf_meta(count_subjects(r,r,+,-)).
-:- rdf_meta(rdf_property_table(r,+,-)).
 :- rdf_meta(rdf_triples_by_datatype(?,r,-)).
 :- rdf_meta(rdf_triples_by_object(?,o,-)).
 :- rdf_meta(rdf_triples_by_pattern(r,r,o,?,-)).
 :- rdf_meta(rdf_triples_by_predicate(?,r,-)).
 :- rdf_meta(rdf_triples_by_subject(?,r,-)).
 :- rdf_meta(rdf_triples_by_term(?,o,-)).
+:- rdf_meta(triples_by_resource(r,-)).
 
 
 
-%! count_classes(+Graph:atom, -Count:nonneg) is det.
-% Returns the number of distinct classes in the given graph.
-%
-% The total number of distinct classes in the graph.
-% In other words, the number of distinct class URIs
-% occuring as objects of =|rdf:type` triples in the graph.
-%
-% This definition is actually incorrect, since a class can be specified
-% using either of the following triple schemes, without the class having
-% to occur in the object position of any triple.
-% ```
-% <C,  rdf:type,        rdfs:Class>
-% <C, rdfs:subClassOf,  C'        >
-% ```
-%
-% @see Based on the definition of =|void:classes`.
 
-count_classes(G, Count):-
+
+%! iris_by_graph(?Graph:atom, -NumberOfDistinctIris:nonneg) is det.
+% Returns the numver of unique IRIs that occur in the given graph.
+
+iris_by_graph(Graph, NumberOfDistinctIris):-
   aggregate_all(
-    set(O),
-    rdf(_, rdf:type, O, G),
-    Os
+    set(Iri),
+    rdf_iri(Iri, Graph),
+    DistinctIris
   ),
-  length(Os, Count).
+  length(DistinctIris, NumberOfDistinctIris).
 
-%! count_entities(+Graph:atom, -Count:nonneg) is det.
-% Returns the numver of unique entities in the given graph.
-%
-%	The total number of entities that are described in the graph.
-% To be an entity in a graph, a resource must have a URI.
-%
-% @see Based on the definition of =|void:entities`.
 
-count_entities(G, Count):-
-  aggregate_all(
-    set(E),
-    rdf_iri(E, G),
-    Es
-  ),
-  length(Es, Count).
-
-%! count_individuals(
-%!   +Class:iri,
-%!   +Graph:atom,
-%!   -NumberOfIndividuals:nonneg
-%! ) is det.
-
-count_individuals(C, G, N):-
-  aggregate_all(
-    set(I),
-    (
-      rdfs_individual_of(I, C),
-      rdf_term(I, G)
-    ),
-    Is
-  ),
-  length(Is, N).
 
 %! count_objects(
 %!   ?Subject:or([bnode,iri]),
 %!   ?Predicate:iri,
-%!   +Graph:atom,
-%!   -Count:nonneg
+%!   ?Graph:atom,
+%!   -NumberOfDistinctObjectTerms:nonneg
 %! ) is det.
-% Returns the number of distinct objects
-%
-% The total number of distinct objects in the graph.
-% In other words, the number of distinct URIs, blank nodes, or literals
-% that occur in the object position of triples in the graph.
-%
-% @see Based on the definition of =|void:distinctObjects`.
+% Returns the number of distinct object terms for the given pattern.
 
-count_objects(S, P, G, Count):-
+count_objects(S, P, G, NumberOfDistinctObjectTerms):-
   aggregate_all(
-    set(O),
-    rdf(S, P, O, G),
-    Os
+    set(ObjectTerm),
+    rdf(S, P, ObjectTerm, G),
+    DistinctObjectTerms
   ),
-  length(Os, Count).
+  length(DistinctObjectTerms, NumberOfDistinctObjectTerms).
 
-%! count_properties(
+
+
+%! count_predicates(
 %!   ?Subject:or([bnode,iri]),
 %!   ?Object:rdf_term,
-%!   +Graph:atom,
-%!   -Count:nonneg
+%!   ?Graph:atom,
+%!   -NumberOfDistinctPredicateTerms:nonneg
 %! ) is det.
-% Returns the number of distinct properties in the given graph.
-%
-% The total number of distinct properties in the graph.
-% In other words, the number of distinct property URIs
-% that occur in the predicate position of triples in the graph.
-%
-% This definition is actually incorrect, since a property can be specified
-% using either of the following triple schemes, without the property having
-% to occur in the predicate position of any triple.
-% ```
-% <P,  rdf:type,           rdf:Property>
-% <P1, rdfs:subPropertyOf, P2          >
-% ```
-%
-% @see Based on the definition of =|void:properties`.
+% Returns the number of distinct predicate terms for the given patern.
 
-count_properties(S, O, G, Count):-
+count_predicates(S, O, G, NumberOfDistinctPredicateTerms):-
   aggregate_all(
     set(P),
     rdf(S, P, O, G),
     Ps
   ),
-  length(Ps, Count).
+  length(Ps, NumberOfDistinctPredicateTerms).
+
+
 
 %! count_subjects(
-%!   +Predicate:iri,
-%!   +Object:or([bnode,literal,iri]),
-%!   +Graph:atom
-%!   -Count:nonneg
+%!   ?Predicate:iri,
+%!   ?Object:or([bnode,literal,iri]),
+%!   ?Graph:atom
+%!   -NumberOfDistinctSubjectTerms:nonneg
 %! ) is det.
-% Returns the number of unique subject terms that occur in triples
-% with the given predicate-object pair and in the given graph.
-%
-% The total number of distinct subjects in the graph.
-% In other words, the number of distinct URIs or blank nodes
-% that occur in the subject position of triples in the graph.
-%
-% @see Based on the definition of =|void:distinctSubjects`.
+% Returns the number of distinct subject terms for the given pattern.
 
-count_subjects(P, O, G, Count):-
+count_subjects(P, O, G, NumberOfDistinctSubjectTerms):-
   aggregate_all(
-    set(S),
-    rdf(S, P, O, G),
-    Ss
+    set(SubjectTerm),
+    rdf(SubjectTerm, P, O, G),
+    DistinctSubjectTerms
   ),
-  length(Ss, Count).
+  length(DistinctSubjectTerms, NumberOfDistinctSubjectTerms).
 
-rdf_property_table(P, G, T):-
-  aggregate_all(
-    set(O),
-    rdf(_, P, O, G),
-    Os
-  ),
-  findall(
-    [O,NumberOfO],
-    (
-      member(O, Os),
-      aggregate_all(
-        count,
-        rdf(_, P, O, G),
-        NumberOfO
-      )
-    ),
-    T
-  ).
 
 
 %! rdf_triples_by_datatype(
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   +Datatype:iri,
 %!   -NumberOfTriples:nonneg
 %! ) is det.
@@ -248,8 +155,9 @@ rdf_triples_by_datatype(G, D, ND):-
   rdf_triples_by_pattern(_, _, literal(type(D,_)), G, ND).
 
 
+
 %! rdf_triples_by_object(
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   +Object:rdf_term,
 %!   -NumberOfTriples:nonneg
 %! ) is det.
@@ -258,11 +166,12 @@ rdf_triples_by_object(G, O, NO):-
   rdf_triples_by_pattern(_, _, O, G, NO).
 
 
+
 %! rdf_triples_by_pattern(
 %!   ?Subject:or([bnode,iri]),
 %!   ?Predicate:iri,
 %!   ?Object:rdf_term,
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   -NumberOfTriples:nonneg
 %! ) is det.
 
@@ -270,8 +179,9 @@ rdf_triples_by_pattern(S, P, O, G, N):-
   aggregate_all(count, rdf(S, P, O, G), N).
 
 
+
 %! rdf_triples_by_predicate(
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   +Predicate:iri,
 %!   -NumberOfTriples:nonneg
 %! ) is det.
@@ -280,8 +190,24 @@ rdf_triples_by_predicate(G, P, NP):-
   rdf_triples_by_pattern(_, P, _, G, NP).
 
 
+
+%! triples_by_resource(+Resource:iri, -N:nonneg) is det.
+
+triples_by_resource(Resource, N):-
+  rdf_global_id(owl:sameAs, P),
+  aggregate_all(
+    count,
+    (
+      rdf_reachable(Resource, P, Resource0),
+      rdf(Resource0, _, _)
+    ),
+    N
+  ).
+
+
+
 %! rdf_triples_by_subject(
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   +Subject:or([bnode,iri]),
 %!   -NumberOfTriples:nonneg
 %! ) is det.
@@ -290,8 +216,9 @@ rdf_triples_by_subject(G, S, NS):-
   rdf_triples_by_pattern(S, _, _, G, NS).
 
 
+
 %! rdf_triples_by_term(
-%!   ?RdfGraph:atom,
+%!   ?Graph:atom,
 %!   ?RdfTerm:rdf_term,
 %!   -NumberOfTriples:nonneg
 %! ) is det.
