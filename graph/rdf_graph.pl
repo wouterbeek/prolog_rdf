a52b121184e63792dcc4b3d7cdba3ba1e10dd323
Wouter Beek <me@wouterbeek.com>
2015-02-26 14:57:36 +0100

    CLEANUP: Relocated lean graphs to plRdfEntailment.

--- graph/rdf_graph.pl
+++ graph/rdf_graph.pl
@@ -14,9 +14,6 @@
                            % +Graph2:atom
     rdf_is_graph/1, % @Term
     rdf_is_ground_graph/1, % +Graph:atom
-    rdf_is_lean_graph/1, % +Graph:atom
-    rdf_lean_graph/2, % +Graph:atom
-                      % -NonleanTriple:compound
     rdf_proper_graph_instance/3, % +Graph1:atom
                                  % +Graph2:atom
                                  % -BNodeMap:list(pair(bnode,or([iri,literal])))
@@ -212,74 +209,6 @@ rdf_is_graph(Graph):-
 rdf_is_ground_graph(Graph):-
   rdf_graph(Graph),
   \+ rdf_bnode(_, Graph).
-
-
-
-%! rdf_is_lean_graph(+Lean:atom) is semidet.
-% An RDF graph is **lean** if it has no instance which is a proper subgraph
-%  of itself.
-%
-% Non-lean graphs have internal redundancy and express the same content
-% as their lean subgraphs. For example, the graph
-%
-% ```
-% ex:a ex:p _:x .
-% _:y  ex:p _:x .
-% ```
-%
-% is not lean, but
-%
-% ```
-% ex:a ex:p _:x .
-% _:x  ex:p _:x .
-% ```
-%
-% is lean.
-%
-% Ground graphs are lean.
-%
-% ### Algorithm
-%
-% The idea behind the algorith is that a non-lean graph must contain
-%  two triples: Generic and Specific such that
-%  the latter is a proper instance of the former.
-% This means that Specific entails Generic, which is therefore verbose.
-%
-% @compat RDF 1.1 Semantics
-
-rdf_is_lean_graph(Graph):-
- \+ rdf_lean_graph(Graph, _).
-
-
-
-%! rdf_lean_graph(+Graph:atom, -NonleanTriple:compound) is nondet.
-% Graph leaning process, enumerating the non-lean triples in Graph.
-
-rdf_lean_graph(Graph, rdf(S1,P,O1)):-
-  rdf_triples(Graph, Triples),
-  partition(rdf_is_ground_triple, Triples, Ground, NonGround),
-  \+ (
-    % Generic. Example 1: `_:y ex:p _:x`.
-    member(rdf(S1,P,O1), NonGround),
-    % Specific. Example 2: `ex:a ex:p _:x`.
-    % This is likelier to be found in ground triples.
-    (   member(rdf(S2,P,O2), Ground)
-    ;   member(rdf(S2,P,O2), NonGround)
-    ),
-
-    % Check whether Specific is a *proper instance* of Generic.
-    \+ (S1 == S2, O1 == O2),
-    rdf_triple_instance(rdf(S2,P,O2), rdf(S1,P,O1), Map),
-
-    % Check whether the mapping extends to the entire graph.
-    forall(
-      member(rdf(S3,P3,O3), NonGround),
-      (
-        rdf_triple_instance(rdf(S3,P3,O3), rdf(S4,P4,O4), Map),
-        memberchk(rdf(S4,P4,O4), Triples)
-      )
-    )
-  ).
 
 
 
