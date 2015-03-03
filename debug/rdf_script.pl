edc5515d18317bbbd147d16b7fe120f7e1768146
Wouter Beek <me@wouterbeek.com>
2015-03-03 09:11:56 +0100

    CLEANUP: Use of default/2 simplifying RDF script a bit.

--- debug/rdf_script.pl
+++ debug/rdf_script.pl
@@ -12,14 +12,16 @@ Scripts for asserting RDF graphs that can be used for debugging.
 [[rdfs.png]]
 
 @author Wouter Beek
-@version 2012/12-2013/02, 2013/07, 2014/06
+@version 2012/12-2013/02, 2013/07, 2014/06, 2015/03
 */
 
 :- use_module(library(semweb/rdf_db), except([rdf_node/1])).
 
+:- use_module(plc(generics/meta_ext)).
+
+:- use_module(plRdf(api/owl_build)).
 :- use_module(plRdf(api/rdf_build)).
 :- use_module(plRdf(api/rdfs_build)).
-:- use_module(plRdf(owl/owl_build)).
 
 :- rdf_register_prefix(ch, 'http://www.wouterbeek.com/ch.owl#' ).
 :- rdf_register_prefix(dbpedia, 'http://dbpedia.org/resource/').
@@ -29,30 +31,27 @@ Scripts for asserting RDF graphs that can be used for debugging.
 
 
 
-assert_visum(G):-
+assert_visum(Graph):-
   % Use a default graph name if none is given.
-  (   var(G)
-  ->  G = visum
-  ;   true
-  ),
+  default(visum, Graph),
 
   % Chinese namespace
-  rdfs_assert_class(    ch:cityWithAirport,                     G),
-  rdfs_assert_subclass( ch:capital,         ch:cityWithAirport, G),
-  rdf_assert_instance(ch:'Amsterdam',     ch:capital,         G),
-  rdfs_assert_class(    ch:visumNeeded,                         G),
-  rdfs_assert_subclass( ch:europeanCity,    ch:visumNeeded,     G),
-  rdf_assert_instance(ch:'Amsterdam',     ch:europeanCity,    G),
+  rdfs_assert_class(ch:cityWithAirport, Graph),
+  rdfs_assert_subclass(ch:capital, ch:cityWithAirport, Graph),
+  rdf_assert_instance(ch:'Amsterdam', ch:capital, Graph),
+  rdfs_assert_class(ch:visumNeeded, Graph),
+  rdfs_assert_subclass(ch:europeanCity, ch:visumNeeded, Graph),
+  rdf_assert_instance(ch:'Amsterdam', ch:europeanCity, Graph),
 
   % Dutch namespace
-  rdfs_assert_class(    nl:europeanCity,                   G),
-  rdfs_assert_subclass( nl:visumFree,    nl:europeanCity,  G),
-  rdf_assert_instance(nl:'Amsterdam',  nl:europeanCity,  G),
-  rdfs_assert_class(    nl:capital,                        G),
-  rdf_assert_instance(nl:'Amsterdam',  nl:capital,       G),
+  rdfs_assert_class(nl:europeanCity, Graph),
+  rdfs_assert_subclass( nl:visumFree, nl:europeanCity, Graph),
+  rdf_assert_instance(nl:'Amsterdam', nl:europeanCity, Graph),
+  rdfs_assert_class(nl:capital, Graph),
+  rdf_assert_instance(nl:'Amsterdam', nl:capital, Graph),
 
   % Interrelations
-  owl_assert_class_equivalence(ch:capital,      nl:capital,     G),
-  owl_assert_resource_identity(dbpedia:'Amsterdam', ch:'Amsterdam', G),
-  owl_assert_resource_identity(dbpedia:'Amsterdam', nl:'Amsterdam', G).
+  owl_assert_class_equivalence(ch:capital, nl:capital, Graph),
+  owl_assert_resource_identity(dbpedia:'Amsterdam', ch:'Amsterdam', Graph),
+  owl_assert_resource_identity(dbpedia:'Amsterdam', nl:'Amsterdam', Graph).
 
