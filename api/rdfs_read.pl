31a5c7a59952dafd6118cf29dcb8f28bebc92ddf
Wouter Beek <me@wouterbeek.com>
2015-03-22 11:55:58 +0100

    ADDED: Wrapper rdfs_label_value/3.

--- api/rdfs_read.pl
+++ api/rdfs_read.pl
@@ -1,13 +1,16 @@
 :- module(
   rdfs_read,
   [
-    rdfs_label/4, % ?Term:rdf_term
+    rdfs_label/4, % ??Subject:or([bnode,iri])
                   % ?Value
                   % ?LangTagPreference:list(list(atom))
                   % ?Graph:atom
-    rdfs_label_value/2, % ?Term:rdf_term
+    rdfs_label_value/2, % ?Subject:or([bnode,iri])
                         % ?Label:atom
-    rdfs_label_value/4 % ?Term:rdf_term
+    rdfs_label_value/3, % ?Subject:or([bnode,iri])
+                        % ?Label:atom
+                        % ?LangTagPreference:list(list(atom))
+    rdfs_label_value/4 % ?Subject:or([bnode,iri])
                        % ?Label:atom
                        % ?LangTagPreference:list(list(atom))
                        % ?Graph:atom
@@ -17,7 +20,7 @@
 /** <module> RDFS Read API
 
 @author Wouter Beek
-@version 2014/11-2015/02
+@version 2014/11-2015/03
 */
 
 :- use_module(library(lists), except([delete/3,subset/2])).
@@ -26,35 +29,44 @@
 :- use_module(plRdf(api/rdf_read)).
 :- use_module(plRdf(term/rdf_term)).
 
-:- rdf_meta(rdfs_label(o,?,?,?)).
-:- rdf_meta(rdfs_label(o,?,?,?)).
-:- rdf_meta(rdfs_label_value(o,?)).
-:- rdf_meta(rdfs_label_value(o,?,?,?)).
+:- rdf_meta(rdfs_label(r,?,?,?)).
+:- rdf_meta(rdfs_label_value(r,?)).
+:- rdf_meta(rdfs_label_value(r,?,?)).
+:- rdf_meta(rdfs_label_value(r,?,?,?)).
 
 
 
 
 
 %! rdfs_label(
-%!   ?Term:rdf_term,
+%!   ?Subject:or([bnode,iri]),
 %!   ?Value,
 %!   ?LangTagPreference:list(list(atom)),
 %!   ?Graph:atom
 %! ) is nondet.
 % Reads RDFS labels attributed to resources.
 
-rdfs_label(Term, Value, LangPrefs, Graph):-
-  rdf_plain_literal(Term, rdfs:label, Value, LangPrefs, Graph).
+rdfs_label(S, Value, LangPrefs, Graph):-
+  rdf_plain_literal(S, rdfs:label, Value, LangPrefs, Graph).
 
 
 
-%! rdfs_label_value(?Term:rdf_term, ?Label:atom) is nondet.
+%! rdfs_label_value(?Subject:or([bnode,iri]), ?Label:atom) is nondet.
 
-rdfs_label_value(Term, Label):-
-  rdfs_label_value(Term, Label, _, _).
+rdfs_label_value(S, Label):-
+  rdfs_label_value(S, Label, _, _).
 
 %! rdfs_label_value(
-%!   ?Term:rdf_term,
+%!   ?Subject:or([bnode,iri]),
+%!   ?Label:atom,
+%!   ?LangTagPreference:list(list(atom))
+%! ) is nondet.
+
+rdfs_label_value(S, Label, LangPrefs):-
+  rdfs_label_value(S, Label, LangPrefs, _).
+
+%! rdfs_label_value(
+%!   ?Subject:or([bnode,iri]),
 %!   ?Label:atom,
 %!   ?LangTagPreference:list(list(atom)),
 %!   ?Graph:atom
@@ -65,11 +77,9 @@ rdfs_label_value(Term, Label):-
 % This predicate normalizes argument Label.
 % It is either the full `Value` (if `xsd:string`)
 % or the second argument of the pair (if `rdf:langTag`).
-%
-% @see rdfs_label/4
 
-rdfs_label_value(Term, Label, LangPrefs, Graph):-
-  rdfs_label(Term, Value, LangPrefs, Graph),
+rdfs_label_value(S, Label, LangPrefs, Graph):-
+  rdfs_label(S, Value, LangPrefs, Graph),
   (   Value = Label-_
   ->  true
   ;   Label = Value
