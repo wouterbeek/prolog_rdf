c23ee94d6f5c8990abb5e80c58347d8fe85b317f
Wouter Beek <me@wouterbeek.com>
2015-06-09 10:41:34 +0200

    CLEANUP: Rewrite of owl_read.

--- api/owl_read.pl
+++ api/owl_read.pl
@@ -1,26 +1,30 @@
 :- module(
   owl_read,
   [
-    owl_class_equivalence/2, % ?Class1:uri
-                             % ?Class2:uri
-    owl_disjointWith/2, % ?Class:iri
-                        % ?Class:iri
-    owl_identity_set/2, % +IRI:iri
-                        % -IdentitySet:ordset(iri)
-    owl_resource_identity/2, % ?Resource1:uri
-                             % ?Resource2:uri
-    owl_resource_identity/3, % ?Resource1:uri
-                             % ?Resource2:uri
-                             % ?Graph:atom
-    owl_resource_identity/4, % ?Resource1:uri
-                             % ?Graph1:atom
-                             % ?Resource2:uri
-                             % ?Graph2:atom
-    owl_resource_identity/5 % ?Resource1:uri
-                            % ?Graph1:atom
-                            % ?Resource2:uri
-                            % ?Graph2:atom
-                            % ?LinkGraph:atom
+    owl_class_equiv/2, % ?Class1:iri
+                       % ?Class2:iri
+    owl_class_disjoint/2, % ?Class1:iri
+                          % ?Class2:iri
+    owl_id/2, % ?Term1:iri
+              % ?Term2:iri
+/*
+    owl_id/3, % ?Term1:iri
+              % ?Term2:iri
+              % ?Graph:atom
+    owl_id/4, % ?Term1:iri
+              % ?Graph1:atom
+              % ?Term2:iri
+              % ?Graph2:atom
+    owl_id/5, % ?Term1:iri
+              % ?Graph1:atom
+              % ?Term2:iri
+              % ?Graph2:atom
+              % ?LinkGraph:atom
+*/
+    owl_ids/2, % +Term:iri
+               % -IdTerms:ordset(iri)
+    owl_label_or_term/2 % +Term:iri
+                        % -Label:atom
   ]
 ).
 
@@ -29,63 +33,86 @@
 Predicates for reading from OWL data.
 
 @author Wouter Beek
-@version 2013/01, 2013/03-2013/05, 2013/08, 2014/06, 2014/11
+@version 2013-2015
 */
 
+:- use_module(library(aggregate)).
 :- use_module(library(ordsets)).
 :- use_module(library(semweb/rdf_db), except([rdf_node/1])).
 :- use_module(library(semweb/rdfs), except([rdfs_label/3])).
 
-:- use_module(plc(generics/meta_ext)).
-
+:- use_module(plRdf(api/rdfs_read)).
 :- use_module(plRdf(term/rdf_term)).
 
-:- rdf_meta(owl_class_equivalence(r,r)).
-:- rdf_meta(owl_disjointWith(+,r,r)).
-:- rdf_meta(owl_resource_identity(r,r)).
+:- rdf_meta(owl_class_equiv(r,r)).
+:- rdf_meta(owl_class_disjoint(r,r)).
+:- rdf_meta(owl_id(r,r)).
+:- rdf_meta(owl_ids(r,-)).
+:- rdf_meta(owl_label_or_term(r,-)).
 
 
 
 
 
-owl_class_equivalence(Class1, Class2):-
-  rdf_has(Class1, owl:equivalentClass, Class2).
+%! owl_class_equiv(+Class1:iri, +Class2:iri) is semidet.
+%! owl_class_equiv(+Class1:iri, -Class2:iri) is nondet.
+%! owl_class_equiv(-Class1:iri, +Class2:iri) is nondet.
 
-owl_disjointWith(C1, C2):-
-  rdf(C1, owl:disjointWith, C2), !,
-  rdfs_individual_of(C1, rdfs:'Class'),
-  rdfs_individual_of(C2, rdfs:'Class'),
-  \+ ((
-    rdfs_individual_of(X, C1),
-    rdfs_individual_of(X, C1)
-  )).
+owl_class_equiv(C1, C2):-
+  rdf_reachable(C1, owl:equivalentClass, C2).
 
-%! owl_identity_set(+IRI:iri, -IdentitySet:ordset(iri)) is det.
-% Returns the identity set for the given IRI.
 
-owl_identity_set(IRI, I_Set):-
-  rdf_iri(IRI), !,
-  % No double occurrences.
-  findall(
-    I_IRI,
-    owl_resource_identity(IRI, I_IRI),
-    I_IRIs
-  ),
-  update_datastructure(ord_add_element, [], [IRI|I_IRIs], I_Set).
 
-owl_resource_identity(Resource1, Resource2):-
-  rdf_has(Resource1, owl:sameAs, Resource2).
-owl_resource_identity(Resource, Resource).
+%! owl_class_disjoint(+Class1:iri, +Class2:iri) is semidet.
+%! owl_class_disjoint(+Class1:iri, -Class2:iri) is nondet.
+%! owl_class_disjoint(-Class1:iri, +Class2:iri) is nondet.
 
-owl_resource_identity(R1, R2, G):-
+owl_class_disjoint(C1, C2):-
+  rdf_reachable(C1, owl:disjointWith, C2).
+
+
+
+%! owl_id(+Term1:iri, +Term2:iri) is semidet.
+%! owl_id(+Term1:iri, -Term2:iri) is multi.
+%! owl_id(-Term1:iri, +Term2:iri) is multi.
+
+owl_id(T1, T2):-
+  rdf_reachable(T1, owl:sameAs, T2).
+
+
+
+/*
+@tbd The following require `rdf_reachable(S,P,O,G)`.
+
+owl_id(R1, R2, G):-
   rdf(R1, owl:sameAs, R2, G).
 
-owl_resource_identity(Resource1, G1, Resource2, G2):-
-  owl_resource_identity(Resource1, G1, Resource2, G2, _LinkG).
 
-owl_resource_identity(Resource1, G1, Resource2, G2, LinkG):-
-  rdf(Resource1, owl:sameAs, Resource2, LinkG),
-  rdf(Resource1, _P1, _O1, G1),
-  rdf(Resource2, _P2, _O2, G2).
-owl_resource_identity(Resource, G, Resource, G, _LinkG).
 
+owl_id(T1, G1, T2, G2):-
+  owl_id(T1, G1, T2, G2, _LinkG).
+
+
+
+owl_id(T1, G1, T2, G2, LinkG):-
+  rdf(T1, owl:sameAs, T2, LinkG),
+  rdf(T1, _P1, _O1, G1),
+  rdf(T2, _P2, _O2, G2).
+owl_id(T, G, T, G, _).
+*/
+
+
+
+%! owl_ids(+Term:iri, -IdenticalTerms:ordset(iri)) is det.
+% Returns the identity set for the given IRI.
+
+owl_ids(T0, ClT):-
+  aggregate_all(set(T), owl_id(T0, T), ClT).
+
+
+
+%! owl_label_or_term(+Term:iri, -Label:atom) is nondet.
+
+owl_label_or_term(T0, L):-
+  rdf_reachable(T0, owl:sameAs, T),
+  rdfs_label_or_term(T, L).
