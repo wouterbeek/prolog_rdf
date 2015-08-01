50f394f70616e7d14f7318fe44ec90d14ab0e3e0
Wouter Beek <me@wouterbeek.com>
2015-08-02 01:28:08 +0200

    FIXED: Datatyped assertion in rdf_assert_json/3 was a bit broken.

--- prolog/rdf/rdf_json_build.pl
+++ prolog/rdf/rdf_json_build.pl
@@ -24,8 +24,6 @@
 
 
 
-
-
 rdf_assert_json(S, P, T):-
   rdf_assert_json(S, P, T, _).
 
@@ -34,18 +32,23 @@ rdf_assert_json(S, P, L1, G):-
   is_list(L1), !,
   rdf_assert_list(L1, L2, G),
   rdf_assert2(S, P, L2, G).
+% JSON value.
 rdf_assert_json(S, P, Json, G):-
   rdf_json_value(Json, D, Lex),
   rdf_assert2(S, P, literal(type(D,Lex)), G).
 
-% JSON float.
-rdf_json_value(X, xsd:float, Y):-
-  float(X), !,
-  atom_number(X, Y).
-% JSON integer.
-rdf_json_value(X, xsd:integer, Y):-
-  integer(X), !,
-  atom_number(X, Y).
+% JSON number.
+rdf_json_value(X, D, Y):-
+  (   % JSON float.
+      float(X)
+  ->  rdf_equal(xsd:float, D),
+      atom_number(Y, X)
+  ;   % JSON integer.
+      integer(X)
+  ->  rdf_equal(xsd:integer, D),
+      atom_number(Y, X)
+  ), !.
 % JSON string.
-rdf_json_value(X, xsd:string, X):-
-  string(X), !.
+rdf_json_value(X, D, X):-
+  string(X), !,
+  rdf_equal(xsd:string, D).
