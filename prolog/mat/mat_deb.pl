7a0fc2c073f83fb054257823f940f3364c936491
Wouter Beek <me@wouterbeek.com>
2015-08-19 15:27:19 +0200

    deb

--- prolog/mat/mat_deb.pl
+++ prolog/mat/mat_deb.pl
@@ -33,7 +33,11 @@ Debug tools for calculating a materialization.
 mat_deb(R, Ps, C):-
   store_j(R, Ps, C),
   (   debugging(mat(R))
-  ->  dcg_with_output_to(string(S), print_deduction(R, Ps, C)),
+  ->  catch(
+dcg_with_output_to(string(S), print_deduction(R, Ps, C)),
+_,
+(gtrace,dcg_with_output_to(string(S), print_deduction(R, Ps, C)))
+),
       debug(mat(R), '~s', S)
   ;   true
   ).
