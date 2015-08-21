f20b7f0e48f64c886ac5bbaf157c965efe55840e
Wouter Beek <me@wouterbeek.com>
2015-08-21 16:40:59 +0200

    ADDED: rdfs_comment/2 convenience predicate.

--- prolog/rdfs/rdfs_read.pl
+++ prolog/rdfs/rdfs_read.pl
@@ -1,6 +1,8 @@
 :- module(
   rdfs_read,
   [
+    rdfs_comment/2, % ?Subject:rdf_term
+                    % ?Comment:atom
     rdfs_instance/2 % ?Instance:rdf_term
                     % ?Class:or([bnode,iri])
   ]
@@ -15,10 +17,20 @@
 */
 
 :- use_module(library(rdf/rdf_build)).
+:- use_module(library(owl/owl_read)).
 
+:- rdf_meta(rdfs_comment(o,?)).
 :- rdf_meta(rdfs_instance(o,r)).
 
 
+
+
+
+%! rdfs_comment(?Subject:rdf_term, ?Comment:atom) is nondet.
+
+rdfs_comment(S, Comm):-
+  rdf_literal(S0, rdfs:comment, xsd:string, Comm),
+  owl_id(S, S0).
 
 
 
