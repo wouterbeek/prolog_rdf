e0060ad05cf6fcdebdb298bac7dbe2554b01b1c8
Wouter Beek <me@wouterbeek.com>
2015-08-23 22:58:46 +0200

    DEBUG: Perform the RDF test inside a graph.

--- test/test_literal_pl.pl
+++ test/test_literal_pl.pl
@@ -1,33 +1,102 @@
 :- module(test_literal_pl, [run_tests/0]).
 
+/** <module> Test rdf_assert_literal_pl/[3,4]
+
+After running the tests in this module you should be able to
+read the triples back in as follows:
+
+```prolog
+?- rdf_literal(S, P, D, O, test_literal_pl).
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString',
+O = one-[en, 'US'] ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#string',
+O = '1000' ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#date',
+O = date(1000, 10, 1, _G302, _G303, _G304, 0, 0, -) ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#dateTime',
+O = date(1000, 10, 1, 10, 11, 11, 0, 0, -) ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#time',
+O = date(_G897, _G898, _G899, 11, 11, 11, 0, 0, -) ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#float',
+O = 1.11111111 ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML',
+O = [element(html, [], [element(p, [], [monkey])])] ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#integer',
+O = 1 ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#decimal',
+O = 111111111 rdiv 100000000 ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/2001/XMLSchema#string',
+O = '1' ;
+S = 'http://www.example.org/s',
+P = 'http://www.example.org/p',
+D = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral',
+O = [element(something, [], ['\n  ', element(inner, [], []), '\n'])] ;
+```
+
+Notice that all Prolog types covered by rdf_assert_literal_pl/[3,4] are
+preserved when read back in through rdf_literal/[3,4] *except for SWI7
+strings*.
+
+---
+
+@author Wouter Beek
+@version 2015/08
+*/
+
 :- use_module(library(rdf/rdf_build)).
+:- reexport(library(rdf/rdf_read)).
 :- use_module(library(rdf/rdf_print)).
 :- use_module(library(semweb/rdf_db)).
 
 :- rdf_register_prefix(ex, 'http://www.example.org/').
 
-run_tests:-
-  % Atom
-  rdf_assert_literal_pl(ex:s, ex:p, '1000'),
-  % date/3
-  rdf_assert_literal_pl(ex:s, ex:p, date(1000,10,1)),
-  % date/9
-  rdf_assert_literal_pl(ex:s, ex:p, date(1000,10,1,10,11,11.111,111,-,true)),
-  % time/3
-  rdf_assert_literal_pl(ex:s, ex:p, time(11,11,11.111)),
-  % float
-  rdf_assert_literal_pl(ex:s, ex:p, 1.11111111),
-  % HTML DOM
-  rdf_assert_literal_pl(ex:s, ex:p, [element(html,[],[element(p,[],[monkey])])]),
-  % integer
-  rdf_assert_literal_pl(ex:s, ex:p, 1),
-  % Pair denoting a language-tagged string.
-  rdf_assert_literal_pl(ex:s, ex:p, [en,'US']-one),
-  % rational
-  rdf_assert_literal_pl(ex:s, ex:p, 111111111 rdiv 100000000),
-  % string
-  rdf_assert_literal_pl(ex:s, ex:p, "1"),
-  % XML DOM
-  rdf_assert_literal_pl(ex:s, ex:p, [element(something,[],[element(inner,[],[])])]),
 
-  rdf_print_graph(user, [ellip_lit(inf),ellip_iri(inf)]).
+
+
+
+run_tests:-
+  G = test_literal_pl,
+  % Atom
+  rdf_assert_literal_pl(ex:s, ex:p, '1000', G),
+  % date/3
+  rdf_assert_literal_pl(ex:s, ex:p, date(1000,10,1), G),
+  % date/9
+  rdf_assert_literal_pl(ex:s, ex:p, date(1000,10,1,10,11,11.111,111,-,true), G),
+  % time/3
+  rdf_assert_literal_pl(ex:s, ex:p, time(11,11,11.111), G),
+  % float
+  rdf_assert_literal_pl(ex:s, ex:p, 1.11111111, G),
+  % HTML DOM
+  rdf_assert_literal_pl(ex:s, ex:p, [element(html,[],[element(p,[],[monkey])])], G),
+  % integer
+  rdf_assert_literal_pl(ex:s, ex:p, 1, G),
+  % Pair denoting a language-tagged string.
+  rdf_assert_literal_pl(ex:s, ex:p, [en,'US']-one, G),
+  % rational
+  rdf_assert_literal_pl(ex:s, ex:p, 111111111 rdiv 100000000, G),
+  % string
+  rdf_assert_literal_pl(ex:s, ex:p, "1", G),
+  % XML DOM
+  rdf_assert_literal_pl(ex:s, ex:p, [element(something,[],[element(inner,[],[])])], G),
+
+  rdf_print_graph(G, [ellip_lit(inf),ellip_iri(inf)]).
