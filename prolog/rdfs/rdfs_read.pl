40190ddd277dd911d1d1f9f35dc3ab2d208e4165
Wouter Beek <me@wouterbeek.com>
2015-08-29 12:18:54 +0200

    ADDED: rdfs_label/[4,5] that only searches for rdfs:label-related values        and that used basic language tag matching.

--- prolog/rdfs/rdfs_read.pl
+++ prolog/rdfs/rdfs_read.pl
@@ -3,8 +3,17 @@
   [
     rdfs_comment/2, % ?Subject:rdf_term
                     % ?Comment:atom
-    rdfs_instance/2 % ?Instance:rdf_term
-                    % ?Class:or([bnode,iri])
+    rdfs_instance/2, % ?Instance:rdf_term
+                     % ?Class:or([bnode,iri])
+    rdfs_label/4, % +Subject:rdf_term
+                  % ?LanguagePreference:atom
+                  % -Language:atom
+                  % -LexicalForm:atom
+    rdfs_label/5 % +Subject:rdf_term
+                 % ?LanguagePreference:atom
+                 % -Language:atom
+                 % -LexicalForm:atom
+		 % ?Graph:atom
   ]
 ).
 
@@ -21,6 +30,8 @@
 
 :- rdf_meta(rdfs_comment(o,?)).
 :- rdf_meta(rdfs_instance(o,r)).
+:- rdf_meta(rdfs_label(o,+,-,-)).
+:- rdf_meta(rdfs_label(o,+,-,-,?)).
 
 
 
@@ -42,3 +53,30 @@ rdfs_instance(I0, C):-
   rdfs_instance(I, C).
 rdfs_instance(I, C):-
   rdfs_individual_of(I, C).
+
+
+
+%! rdfs_label(
+%!   +Subject:rdf_term,
+%!   ?LanguagePreference:list(atom),
+%!   -Language:list(atom),
+%!   -LexicalForm:atom
+%! ) is nondet.
+
+rdfs_label(S, Pref, Lang, Lbl):-
+  rdfs_label(S, Pref, Lang, Lbl, _).
+
+%! rdfs_label(
+%!   +Subject:rdf_term,
+%!   ?LanguagePreference:atom,
+%!   -Language:atom,
+%!   -LexicalForm:atom,
+%!   ?Graph:atom
+%! ) is nondet.
+
+% First look for language-tagged strings with matching language tag.
+rdfs_label(S, Pref, Lang, Lex, G):-
+  rdf_langstring(S, rdfs:label, Pref, Lang-Lex, G).
+% Secondly look for XSD strings with no language tag.
+rdfs_label(S, _, _, Lex, G):-
+  rdf_literal(S, rdfs:label, xsd:string, Lex, G).
