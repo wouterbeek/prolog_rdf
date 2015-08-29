722eb3c4e7b0631e09d7c89cd19bd743a41268ab
Wouter Beek <me@wouterbeek.com>
2015-08-29 12:20:04 +0200

    CLEAN: Sync more with module html_rdf_term. ADDED: Options label_iri/1 and lang_pref/1. CLEAN: Renamed option logic_sym/1 -> symbol_iri/1.

--- prolog/rdf/rdf_print.pl
+++ prolog/rdf/rdf_print.pl
@@ -1,7 +1,8 @@
 :- module(
   rdf_print,
   [
-    rdf_print_describe/2, % +Subject, ?Graph
+    rdf_print_describe/1, % +Subject
+    rdf_print_describe/2, % +Subject, +Options
     rdf_print_describe/3, % +Subject:rdf_term
                           % ?Graph:atom
                           % +Options:list(compound)
@@ -17,13 +18,13 @@
                            % ?Object:rdf_term
                            % ?Graph:atom
                            % +Options:list(compound)
-    rdf_print_quadruples/1, % +Quadruples:list(compoud)
+    rdf_print_quadruples/1, % +Quadruples
     rdf_print_quadruples/2, % +Quadruples:list(compoud)
                             % +Options:list(compound)
-    rdf_print_statement/1, % +Statement:compoud
+    rdf_print_statement/1, % +Statement
     rdf_print_statement/2, % +Statement:compoud
                            % +Options:list(compound)
-    rdf_print_statements/1, % +Statements:list(compoud)
+    rdf_print_statements/1, % +Statements
     rdf_print_statements/2, % +Statements:list(compoud)
                             % +Options:list(compound)
     rdf_print_term/1, % +Term
@@ -41,7 +42,7 @@
                         % ?Object:rdf_term
                         % ?Graph:atom
                         % +Options:list(compound)
-    rdf_print_triples/1, % +Triples:list(compoud)
+    rdf_print_triples/1, % +Triples
     rdf_print_triples/2 % +Triples:list(compoud)
                         % +Options:list(compound)
   ]
@@ -59,23 +60,25 @@ Easy printing of RDF data to the terminal.
 
 :- use_module(library(atom_ext)).
 :- use_module(library(dcg/basics)).
-:- use_module(library(dcg/dcg_bracketed)).
 :- use_module(library(dcg/dcg_collection)).
 :- use_module(library(dcg/dcg_content)).
 :- use_module(library(dcg/dcg_logic)).
 :- use_module(library(dcg/dcg_phrase)).
 :- use_module(library(dcg/dcg_quoted)).
 :- use_module(library(error)).
+:- use_module(library(lambda)).
 :- use_module(library(option)).
 :- use_module(library(rdf/rdf_bnode_name)).
 :- use_module(library(rdf/rdf_graph)).
 :- use_module(library(rdf/rdf_list)).
 :- use_module(library(rdf/rdf_read)).
+:- use_module(library(rdfs/rdfs_read)).
 :- use_module(library(semweb/rdf_db)).
 
 :- set_prolog_flag(toplevel_print_anon, false).
 
-:- rdf_meta(rdf_print_describe(o,?)).
+:- rdf_meta(rdf_print_describe(o)).
+:- rdf_meta(rdf_print_describe(o,+)).
 :- rdf_meta(rdf_print_describe(o,?,+)).
 :- rdf_meta(rdf_print_quadruple(t)).
 :- rdf_meta(rdf_print_quadruple(t,+)).
@@ -98,32 +101,46 @@ Easy printing of RDF data to the terminal.
 :- rdf_meta(rdf_print_triples(t)).
 :- rdf_meta(rdf_print_triples(t,+)).
 
+:- predicate_options(rdf_print_bnode//2, 2, [
+     abbr_list(+boolean),
+     pass_to(rdf_print_list//2, 2)
+   ]).
+:- predicate_options(rdf_print_describe/2, 2, [
+     pass_to(rdf_print_describe/3, 3)
+   ]).
 :- predicate_options(rdf_print_describe/3, 3, [
      pass_to(rdf_print_statement/5, 5)
    ]).
-:- predicate_options(rdf_print_bnode//2, 2, [
-     abbr_list(+boolean)
-   ]).
 :- predicate_options(rdf_print_graph/2, 2, [
+     pass_to(rdf_print_quadruple/5, 5),
      pass_to(rdf_print_triple/5, 5)
    ]).
 :- predicate_options(rdf_print_iri//2, 2, [
      abbr_iri(+boolean),
      abbr_list(+boolean),
      ellip_ln(+or([nonneg,oneof([inf])])),
-     logic_sym(+boolean)
+     label_iri(+boolean),
+     lang_perf(+atom),
+     symbol_iri(+boolean),
+     pass_to(rdf_print_list//2, 2)
    ]).
 :- predicate_options(rdf_print_lexical//2, 2, [
      ellip_lit(+or([nonneg,oneof([inf])]))
    ]).
+:- predicate_options(rdf_print_list//2, 2, [
+     pass_to(rdf_print_term//2, 2)
+   ]).
 :- predicate_options(rdf_print_literal//2, 2, [
+     style(+oneof([tuple,turtle])),
+     pass_to(rdf_print_datatype//2, 2),
+     pass_to(rdf_print_language_tag//2, 2),
      pass_to(rdf_print_lexical//2, 2)
    ]).
 :- predicate_options(rdf_print_object//2, 2, [
      pass_to(rdf_print_term//2, 2)
    ]).
 :- predicate_options(rdf_print_predicate//2, 2, [
-     pass_to(rdf_print_term//2, 2)
+     pass_to(rdf_print_iri//2, 2)
    ]).
 :- predicate_options(rdf_print_quadruple/2, 2, [
      pass_to(rdf_print_statement/5, 5)
@@ -141,20 +158,19 @@ Easy printing of RDF data to the terminal.
      indent(+nonneg),
      pass_to(rdf_print_statement//5, 5)
    ]).
-:- predicate_options(rdf_print_statements/2, 2, [
-     pass_to(rdf_print_statement/2, 2)
-   ]).
 :- predicate_options(rdf_print_statement//5, 5, [
      style(+oneof([tuple,turtle])),
      pass_to(rdf_print_object//2, 2),
      pass_to(rdf_print_predicate//2, 2),
      pass_to(rdf_print_subject//2, 2)
    ]).
+:- predicate_options(rdf_print_statements/2, 2, [
+     pass_to(rdf_print_statement/2, 2)
+   ]).
 :- predicate_options(rdf_print_subject//2, 2, [
      pass_to(rdf_print_term//2, 2)
    ]).
 :- predicate_options(rdf_print_term/2, 2, [
-     pass_to(rdf_print_list//2, 2),
      pass_to(rdf_print_term//2, 2)
    ]).
 :- predicate_options(rdf_print_term//2, 2, [
@@ -176,11 +192,17 @@ Easy printing of RDF data to the terminal.
 
 
 
-%! rdf_print_describe(+Subject:rdf_term, ?Graph:atom) is det.
-% Wrapper around rdf_print_describe/3 with default options.
+%! rdf_print_describe(+Subject:rdf_term) is det.
+% Wrapper around rdf_print_describe/2 with default options.
 
-rdf_print_describe(S, G):-
-  rdf_print_describe(S, G, []).
+rdf_print_describe(S):-
+  rdf_print_describe(S, []).
+
+%! rdf_print_describe(+Subject:rdf_term, +Options:list(compound)) is det.
+% Wrapper around rdf_print_describe/3 with uninstantiated graph.
+
+rdf_print_describe(S, Opts):-
+  rdf_print_describe(S, _, Opts).
 
 %! rdf_print_describe(
 %!   +Subject:rdf_term,
@@ -192,7 +214,9 @@ rdf_print_describe(S, G):-
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * indent(+nonneg)
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 %   * style(+oneof([tuple,turtle])
 
 rdf_print_describe(S, G, Opts):-
@@ -216,28 +240,23 @@ rdf_print_graph(G):-
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * indent(+nonneg)
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 %   * style(+oneof([tuple,turtle])
 %
 % @throws existence_error
 
 rdf_print_graph(G, Opts):-
   (   var(G)
-  ->  rdf_print_graph0(Opts)
+  ->  rdf_print_quadruple(_, _, _, _, Opts),
+      fail
   ;   rdf_is_graph(G)
-  ->  rdf_print_graph0(G, Opts)
+  ->  rdf_print_triple(_, _, _, G, Opts),
+      fail
   ;   existence_error(rdf_graph, G)
   ).
-
-rdf_print_graph0(Opts):-
-  rdf_print_quadruple(_, _, _, _, Opts),
-  fail.
-rdf_print_graph0(_).
-
-rdf_print_graph0(G, Opts):-
-  rdf_print_triple(_, _, _, G, Opts),
-  fail.
-rdf_print_graph0(_, _).
+rdf_print_graph(_, _).
 
 
 
@@ -258,6 +277,7 @@ rdf_print_quadruple(rdf(S,P,O,G), Opts):-
 %!   ?Object:rdf_term,
 %!   ?Graph:atom
 %! ) is det.
+% Wrapper around rdf_print_quadruple/5 with default options.
 
 rdf_print_quadruple(S, P, O, G):-
   rdf_print_quadruple(S, P, O, G, []).
@@ -274,7 +294,9 @@ rdf_print_quadruple(S, P, O, G):-
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * indent(+nonneg)
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 %   * style(+oneof([tuple,turtle])
 
 % Ground quadruples are printing without them having to be present
@@ -337,6 +359,8 @@ rdf_print_statement(T, Opts0):-
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * indent(+nonneg)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
 %   * style(+oneof([tuple,turtle])
 
 rdf_print_statement(S, P, O, G, Opts):-
@@ -366,6 +390,7 @@ rdf_print_statements([H|T], Opts):-
 
 
 %! rdf_print_term(+Term:rdf_term) is det.
+% Wrapper around rdf_print_term/2 with default options.
 
 rdf_print_term(T):-
   rdf_print_term(T, []).
@@ -375,7 +400,9 @@ rdf_print_term(T):-
 %   * abbr_iri(+boolean)
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 
 rdf_print_term(T, Opts):-
   string_phrase(rdf_print_term(T, Opts), X),
@@ -384,6 +411,7 @@ rdf_print_term(T, Opts):-
 
 
 %! rdf_print_triple(+Triple:compound) is det.
+% Wrapper around rdf_print_triple/2 with default options.
 
 rdf_print_triple(T):-
   rdf_print_triple(T, []).
@@ -397,8 +425,9 @@ rdf_print_triple(rdf(S,P,O), Opts):-
 %!   ?Subject:rdf_term,
 %!   ?Predicate:iri,
 %!   ?Object:rdf_term,
-%!   ?Graph:atom
+%!   +Options:list(compound)
 %! ) is nondet.
+% Wrapper around rdf_print_triple/5 with default options.
 
 rdf_print_triple(S, P, O, G):-
   rdf_print_triple(S, P, O, G, []).
@@ -415,8 +444,10 @@ rdf_print_triple(S, P, O, G):-
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * indent(+nonneg)
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
 %   * style(+oneof([tuple,turtle])
+%   * symbol_iri(+boolean)
 
 % Ground triples are printing without them having to be present
 % in the RDF DB.
@@ -466,6 +497,14 @@ rdf_print_bnode(B, _) -->
 
 
 
+%! rdf_print_datatype(+Datatype:iri, +Options:list(compound))// is det.
+% Options are passed to rdf_print_iri//2.
+
+rdf_print_datatype(D, Opts) -->
+  rdf_print_iri(D, Opts).
+
+
+
 %! rdf_print_graph(+Graph:atom)// is det.
 
 rdf_print_graph(G) -->
@@ -490,7 +529,9 @@ rdf_print_graph(G) -->
 %     This is only used when `abbr_iri=true`.
 %     Default is `20` to ensure that every triple fits within
 %     an 80 character wide terminal.
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 %     Whether logic symbols should be used i.o. IRIs.
 %     Default is `true`.
 
@@ -498,33 +539,41 @@ rdf_print_iri(Iri, Opts) -->
   {option(abbr_list(true), Opts)},
   rdf_print_list(Iri, Opts), !.
 rdf_print_iri(Iri, Opts) -->
-  {option(logic_sym(true), Opts, true)},
-  rdf_print_iri_sym(Iri), !.
+  {option(symbol_iri(true), Opts, true)},
+  (   {rdf_global_id(owl:equivalentClass, Iri)}
+  ->  equivalence
+  ;   {rdf_global_id(rdfs:subClassOf, Iri)}
+  ->  subclass
+  ;   {rdf_global_id(rdf:type, Iri)}
+  ->  set_membership
+  ), !.
 rdf_print_iri(Global, Opts) -->
-  {
-    \+ option(abbr_iri(false), Opts),
-    rdf_global_id(Prefix:Local, Global), !,
-    option(ellip_ln(N), Opts, 20),
-    atom_truncate(Local, N, Local0)
-  },
+  {option(label_iri(true), Opts), !,
+   option(lang_pref(Pref), Opts, 'en-US'),
+   once(rdfs_label(Global, Pref, _, Lbl))},
+  atom(Lbl).
+rdf_print_iri(Global, Opts) -->
+  {\+ option(abbr_iri(false), Opts),
+   rdf_global_id(Prefix:Local, Global), !,
+   option(ellip_ln(N), Opts, 20),
+   atom_truncate(Local, N, Local0)},
   atom(Prefix),
   ":",
   atom(Local0).
 rdf_print_iri(Global, Opts) -->
   {option(style(turtle), Opts)}, !,
-  bracketed(angular, atom(Global)).
+  lang,
+    atom(Global),
+  rang.
 rdf_print_iri(Global, _) -->
   atom(Global).
 
-rdf_print_iri_sym(Iri) -->
-  {rdf_global_id(owl:equivalentClass, Iri)}, !,
-  equivalence.
-rdf_print_iri_sym(Iri) -->
-  {rdf_global_id(rdfs:subClassOf, Iri)}, !,
-  subclass.
-rdf_print_iri_sym(Iri) -->
-  {rdf_global_id(rdf:type, Iri)}, !,
-  set_membership.
+
+
+%! rdf_print_language_tag(+LanguageTag:atom, +Options:list(compound))// is det.
+
+rdf_print_language_tag(Lang, _) -->
+  atom(Lang).
 
 
 
@@ -547,19 +596,22 @@ rdf_print_lexical(Lex, Opts) -->
 
 %! rdf_print_list(+Term:rdf_term, +Options:list(compound))// is semidet.
 % The following options are supported:
+%   * abbr_iri(+boolean)
 %   * abbr_list(+boolean)
 %     Whether or not RDF lists are displayed using Prolog list notation.
 %     Default is `true`.
+%   * ellip_lit(+or([nonneg,oneof([inf])]))
+%   * ellip_ln(+or([nonneg,oneof([inf])]))
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 
 rdf_print_list(L0, Opts) -->
   {
     rdf_is_list(L0),
     rdf_list_raw(L0, L)
   },
-  list(rdf_print_term0(Opts), L).
-
-rdf_print_term0(Opts, T) -->
-  rdf_print_term(T, Opts).
+  list(\T^rdf_print_term(T, Opts), L).
 
 
 
@@ -568,32 +620,36 @@ rdf_print_term0(Opts, T) -->
 %   * abbr_iri(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * ellip_ln(+or([nonneg,oneof([inf])]))
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
+%   * symbol_iri(+boolean)
 
-rdf_print_literal(literal(type(D,Lex)), Opts) -->
+rdf_print_literal(literal(type(D,Lex)), Opts) --> !,
   (   {option(style(turtle), Opts)}
   ->  rdf_print_lexical(Lex, Opts),
       "^^",
-      rdf_print_iri(D, Opts)
-  ;   bracketed(langular, (
-        rdf_print_iri(D, Opts),
+      rdf_print_datatype(D, Opts)
+  ;   lang,
+        rdf_print_datatype(D, Opts),
         ", ",
-        rdf_print_lexical(Lex, Opts)
-      ))
+        rdf_print_lexical(Lex, Opts),
+      rang
   ).
-rdf_print_literal(literal(lang(LangTag,Lex)), Opts) --> !,
+rdf_print_literal(literal(lang(Lang,Lex)), Opts) --> !,
   (   {option(style(turtle), Opts)}
   ->  rdf_print_lexical(Lex, Opts),
       "@",
-      atom(LangTag)
-  ;   bracketed(langular, (
-        "rdf:langString, ",
-        bracketed(langular, (
-          atom(LangTag),
+      rdf_print_language_tag(Lang, Opts)
+  ;   {rdf_global_id(rdf:langString, D)},
+      lang,
+        rdf_print_datatype(D, Opts),
+        lang,
+          rdf_print_language_tag(Lang, Opts),
           ", ",
-          rdf_print_lexical(Lex, Opts)
-        ))
-      ))
+          rdf_print_lexical(Lex, Opts),
+        rang,
+      rang
   ).
 rdf_print_literal(literal(Lex), Opts) -->
   {rdf_global_id(xsd:string, D)},
@@ -607,6 +663,9 @@ rdf_print_literal(literal(Lex), Opts) -->
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * ellip_ln(+or([nonneg,oneof([inf])]))
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 
 rdf_print_object(O, Opts) -->
   rdf_print_term(O, Opts).
@@ -616,7 +675,9 @@ rdf_print_object(O, Opts) -->
 %! rdf_print_predicate(+Predicate:iri, +Options:list(compound))// is det.
 %   * abbr_iri(+boolean)
 %   * ellip_ln(+or([nonneg,oneof([inf])]))
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 
 rdf_print_predicate(P, Opts) -->
   rdf_print_iri(P, Opts).
@@ -634,7 +695,9 @@ rdf_print_predicate(P, Opts) -->
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * ellip_ln(+or([nonneg,oneof([inf])]))
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 %   * style(+oneof([tuple,turtle])
 %     The style that is used for printing the statement.
 %     Style `turtle` is appropriate for enumerating multiple triples.
@@ -651,15 +714,14 @@ rdf_print_statement(S, P, O, G, Opts) -->
   ({ground(G)} -> " ", rdf_print_graph(G) ; ""),
   " .".
 rdf_print_statement(S, P, O, G, Opts) -->
-  bracketed(langular, rdf_print_statement0(S, P, O, Opts)),
+  lang,
+    rdf_print_subject(S, Opts),
+    ", ",
+    rdf_print_predicate(P, Opts),
+    ", ",
+    rdf_print_object(O, Opts),
+  rang,
   ({ground(G)} -> "@", rdf_print_graph(G) ; "").
-
-rdf_print_statement0(S, P, O, Opts) -->
-  rdf_print_subject(S, Opts),
-  ", ",
-  rdf_print_predicate(P, Opts),
-  ", ",
-  rdf_print_object(O, Opts).
 
 
 
@@ -686,7 +748,9 @@ rdf_print_term(T) -->
 %   * abbr_list(+boolean)
 %   * ellip_lit(+or([nonneg,oneof([inf])]))
 %   * ellip_ln(+or([nonneg,oneof([inf])]))
-%   * logic_sym(+boolean)
+%   * label_iri(+boolean)
+%   * lang_pref(+atom)
+%   * symbol_iri(+boolean)
 
 rdf_print_term(T, Opts) -->
   {rdf_is_literal(T)}, !,
@@ -696,3 +760,12 @@ rdf_print_term(T, Opts) -->
   rdf_print_bnode(T, Opts).
 rdf_print_term(T, Opts) -->
   rdf_print_iri(T, Opts).
+
+
+
+
+
+% HELPERS %
+
+lang --> [12296].
+rang --> [12297].
