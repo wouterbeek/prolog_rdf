a4bf4619bd0a7db75786d3277543fed40beb6e20
Wouter Beek <me@wouterbeek.com>
2015-09-01 21:14:17 +0200

    FIXED: Missing module import. Thanks to Raivo Laanemets.

--- prolog/rdf/rdf_update.pl
+++ prolog/rdf/rdf_update.pl
@@ -36,6 +36,7 @@ Higher-level update operations performed on RDF data.
 :- use_module(library(deb_ext)).
 :- use_module(library(rdf/rdf_build)).
 :- use_module(library(rdf/rdf_datatype)).
+:- use_module(library(rdf/rdf_print)).
 :- use_module(library(rdf/rdf_read)).
 :- use_module(library(xsd/xsd)).
 
