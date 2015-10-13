c00e9f29de3d8aef61c4aaa16f20c4b29acc1ce9
Wouter Beek <wouter@wouterbeek.com>
2015-10-13 22:59:59 +0200

    ADDED: rdf_number_of_[objects|subjects|predicates]/[3,4].

--- prolog/lod/lod_stats.pl
+++ prolog/lod/lod_stats.pl
@@ -3,9 +3,26 @@
   [
     rdf_description_size/2, % +Resource:rdf_term
                             % -Size:nonneg
-    rdf_number_of_triples/4 % ?Subject:rdf_term
+    rdf_number_of_objects/3, % ?Subject, ?Predicate, -Size
+    rdf_number_of_objects/4, % ?Subject:rdf_term
+                             % ?Predicate:iri
+                             % ?Graph:atom
+                             % -Size:nonneg
+    rdf_number_of_predicates/3, % ?Subject, ?Object, -Size
+    rdf_number_of_predicates/4, % ?Subject:rdf_term
+                                % ?Object:rdf_term
+                                % ?Graph:atom
+                                % -Size:nonneg
+    rdf_number_of_subjects/3, % ?Predicate, ?Object, -Size
+    rdf_number_of_subjects/4, % ?Predicate:iri
+                              % ?Object:rdf_term
+                              % ?Graph:atom
+                              % -Size:nonneg
+    rdf_number_of_triples/4, % ?Subject, ?Predicate, ?Object, -Size
+    rdf_number_of_triples/5 % ?Subject:rdf_term
                             % ?Predicate:iri
                             % ?Object:rdf_term
+                            % ?Graph:atom
                             % -Size:nonneg
   ]
 ).
@@ -13,14 +30,23 @@
 /** <module> LOD statistics
 
 @author Wouter Beek
-@version 2015/08
+@version 2015/08, 2015/10
 */
 
 :- use_module(library(aggregate)).
+:- use_module(library(plunit)).
 :- use_module(library(semweb/rdf_db)).
+:- use_module(library(solution_sequences)).
 
 :- rdf_meta(rdf_description_size(o,-)).
+:- rdf_meta(rdf_number_of_objects(o,r,-)).
+:- rdf_meta(rdf_number_of_objects(o,r,?,-)).
+:- rdf_meta(rdf_number_of_predicates(o,o,-)).
+:- rdf_meta(rdf_number_of_predicates(o,o,?,-)).
+:- rdf_meta(rdf_number_of_subjects(r,o,-)).
+:- rdf_meta(rdf_number_of_subjects(r,o,?,-)).
 :- rdf_meta(rdf_number_of_triples(o,r,o,-)).
+:- rdf_meta(rdf_number_of_triples(o,r,o,?,-)).
 
 
 
@@ -33,6 +59,72 @@ rdf_description_size(S, N):-
 
 
 
+%! rdf_number_of_objects(
+%!   ?Subject:rdf_term,
+%!   ?Predicate:iri,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_objects(S, P, N):-
+  rdf_number_of_objects(S, P, _, N).
+
+
+%! rdf_number_of_objects(
+%!   ?Subject:rdf_term,
+%!   ?Predicate:iri,
+%!   ?Graph:atom,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_objects(S, P, G, N):-
+  rdf_number_of0(O, S, P, O, G, N).
+
+
+
+%! rdf_number_of_predicates(
+%!   ?Subject:rdf_term,
+%!   ?Object:rdf_term,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_predicates(S, O, N):-
+  rdf_number_of_predicates(S, O, _, N).
+
+
+%! rdf_number_of_predicates(
+%!   ?Subject:rdf_term,
+%!   ?Object:rdf_term,
+%!   ?Graph:atom,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_predicates(S, O, G, N):-
+  rdf_number_of0(P, S, P, O, G, N).
+
+
+
+%! rdf_number_of_subjects(
+%!   ?Predicate:iri,
+%!   ?Object:rdf_term,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_subjects(P, O, N):-
+  rdf_number_of_subjects(P, O, _, N).
+
+
+%! rdf_number_of_subjects(
+%!   ?Predicate:iri,
+%!   ?Object:rdf_term,
+%!   ?Graph:atom,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_subjects(P, O, G, N):-
+  rdf_number_of0(S, S, P, O, G, N).
+
+
+
 %! rdf_number_of_triples(
 %!   ?Subject:rdf_term,
 %!   ?Predicate:iri,
@@ -41,4 +133,59 @@ rdf_description_size(S, N):-
 %! ) is det.
 
 rdf_number_of_triples(S, P, O, N):-
-  aggregate_all(count, rdf(S, P, O), N).
+  rdf_number_of_triples(S, P, O, _, N).
+
+
+%! rdf_number_of_triples(
+%!   ?Subject:rdf_term,
+%!   ?Predicate:iri,
+%!   ?Object:rdf_term,
+%!   ?Graph:atom,
+%!   -Size:nonneg
+%! ) is det.
+
+rdf_number_of_triples(S, P, O, G, N):-
+  rdf_number_of0(rdf(S,P,O), S, P, O, G, N).
+
+
+
+
+
+% HELPERS %
+
+rdf_number_of0(Witness, S, P, O, G, N):-
+  aggregate_all(count, distinct(Witness, rdf(S, P, O, G)), N).
+
+
+
+
+
+% TESTS %
+
+:- begin_tests(lod_stats).
+
+test(rdf_number_of_subjects, [forall(test_case(G,N))]):-
+  setup_call_cleanup(
+    rdf_assert_graph(G),
+    rdf_number_of_subjects(_, _, N0),
+    rdf_unload_graph(G)
+  ),
+  writeln(N0),
+  N =:= N0.
+
+test_case(g1, 1).
+test_case(g2, 2).
+
+rdf_assert_graph(g1):-
+  rdf_assert(rdf:s1, rdf:p1, rdf:o1, g1).
+rdf_assert_graph(g2):-
+  % Same subject, different predicate/object.
+  rdf_assert(rdf:s1, rdf:p1, rdf:o1, g2),
+  rdf_assert(rdf:s1, rdf:p2, rdf:o1, g2),
+  rdf_assert(rdf:s1, rdf:p1, rdf:o2, g2),
+  rdf_assert(rdf:s1, rdf:p2, rdf:o2, g2),
+  % The same triple twice.
+  rdf_assert(rdf:s2, rdf:p1, rdf:o1, g2),
+  rdf_assert(rdf:s2, rdf:p1, rdf:o1, g2).
+
+:- end_tests(lod_stats).
--- tmp/vocabulary/rdf_stat.pl
+++ tmp/vocabulary/rdf_stat.pl
@@ -4,18 +4,6 @@
     count_datatype_triples/3, % ?Datatype:iri
                               % ?Graph:atom
                               % -NumberOfDistinctStatements:nonneg
-    count_objects/4, % ?Subject:or([bnode,iri])
-                     % ?Predicate:iri
-                     % ?Graph:atom
-                     % -NumberOfDistinctObjectTerms:nonneg
-    count_predicates/4, % ?Subject:or([bnode,iri])
-                        % ?Object:rdf_term
-                        % ?Graph:atom
-                        % -NumberOfDistinctPredicateTerms:nonneg
-    count_subjects/4, % ?Predicate:iri
-                      % ?Object:rdf_term
-                      % ?Graph:atom
-                      % -NumberOfDistinctSubjectTerms:nonneg
     iris_by_graph/2 % ?Graph:atom
                     % -NumberOfDistinctIris:nonneg
   ]
@@ -36,12 +24,6 @@ Predicates for calculating simple statistics over RDF data.
 :- use_module(plRdf(term/rdf_term)).
 
 :- rdf_meta(count_datatype_triples(r,?,-)).
-:- rdf_meta(count_objects(r,r,?,-)).
-:- rdf_meta(count_predicates(r,r,?,-)).
-:- rdf_meta(count_subjects(r,r,?,-)).
-
-
-
 
 
 %! count_datatype_triples(
@@ -56,61 +38,6 @@ count_datatype_triples(rdf:langString, Graph, N):- !,
 % For all other datatype IRIs.
 count_datatype_triples(Datatype, Graph, N):-
   count_triples(_, _, literal(type(Datatype,_)), Graph, N).
-
-
-
-%! count_objects(
-%!   ?Subject:or([bnode,iri]),
-%!   ?Predicate:iri,
-%!   ?Graph:atom,
-%!   -NumberOfDistinctObjectTerms:nonneg
-%! ) is det.
-% Returns the number of distinct object terms for the given pattern.
-
-count_objects(S, P, G, NumberOfDistinctObjectTerms):-
-  aggregate_all(
-    set(ObjectTerm),
-    rdf(S, P, ObjectTerm, G),
-    DistinctObjectTerms
-  ),
-  length(DistinctObjectTerms, NumberOfDistinctObjectTerms).
-
-
-
-%! count_predicates(
-%!   ?Subject:or([bnode,iri]),
-%!   ?Object:rdf_term,
-%!   ?Graph:atom,
-%!   -NumberOfDistinctPredicateTerms:nonneg
-%! ) is det.
-% Returns the number of distinct predicate terms for the given patern.
-
-count_predicates(S, O, G, NumberOfDistinctPredicateTerms):-
-  aggregate_all(
-    set(P),
-    rdf(S, P, O, G),
-    Ps
-  ),
-  length(Ps, NumberOfDistinctPredicateTerms).
-
-
-
-%! count_subjects(
-%!   ?Predicate:iri,
-%!   ?Object:or([bnode,literal,iri]),
-%!   ?Graph:atom
-%!   -NumberOfDistinctSubjectTerms:nonneg
-%! ) is det.
-% Returns the number of distinct subject terms for the given pattern.
-
-count_subjects(P, O, G, NumberOfDistinctSubjectTerms):-
-  aggregate_all(
-    set(SubjectTerm),
-    rdf(SubjectTerm, P, O, G),
-    DistinctSubjectTerms
-  ),
-  length(DistinctSubjectTerms, NumberOfDistinctSubjectTerms).
-
 
 
 %! iris_by_graph(?Graph:atom, -NumberOfDistinctIris:nonneg) is det.
