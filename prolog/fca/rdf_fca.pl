13deea5bef21c51e889231e7f04edf6667a195ca
Wouter Beek <me@wouterbeek.com>
2015-11-05 18:33:59 +0100

    Finalized FCA generation for a given RDF graph. Very simple implementation, mostly reusing predicates from PLC and plGraphViz.

--- prolog/fca/rdf_fca.pl
+++ prolog/fca/rdf_fca.pl
@@ -1,21 +1,18 @@
 :- module(
   rdf_fca,
   [
-    rdf_fca_context/2, % +Graph:atom
-                       % -Context:compound
-    rdf_fca_hasse/2 % +Graph:atom
-                    % -Hasse:ugraph
+    rdf_fca_context/2 % +Graph:atom
+                        % -Context:compound
   ]
 ).
 
 /** <module> Formal Concept Analysis for RDF
 
 @author Wouter Beek
-@version 2015/10
+@version 2015/11
 */
 
 :- use_module(library(aggregate)).
-:- use_module(library(fca/bordat)).
 :- use_module(library(profile/profile_rdf)).
 :- use_module(library(rdf/rdf_read)).
 
@@ -25,27 +22,13 @@
 
 %! rdf_fca_context(+Graph:atom, -Context:compound) is det.
 
-rdf_fca_context(G, context(Os,As,rdf_fca:rdf_a2o0(G))):-
+rdf_fca_context(G, context(Os,As,rdf_fca:rdf_instance0(G))):-
   aggregate_all(set(O), (user:rdf_subject(O), user:rdf(O, _, _, G)), Os),
   aggregate_all(set(A), rdfs_class(A, G), As).
   
 
-
-%! rdf_fca_hasse(+Graph:atom, -Hasse:ugraph) is det.
-
-rdf_fca_hasse(G, Hasse):-
-  rdf_fca_context(G, Context),
-  fca_hasse(Context, Hasse).
-
-
-
-
-
-% HELPERS %
-
-rdf_a2o0(G, O, A):-
+rdf_instance0(G, O, A):-
   rdf_instance(O, A, G).
-
 
 
 rdfs_class(C, G):-
