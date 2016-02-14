b3b853a446008904308af9bff32f0b117056bab7
Wouter Beek <me@wouterbeek.com>
2016-02-14 23:50:21 +0100

    Unified RDF/CHR store.

--- prolog/chr/rdf_chr.pl
+++ prolog/chr/rdf_chr.pl
@@ -24,53 +24,53 @@ http://lodlaundromat.org/ontology/
 :- use_module(library(settings)).
 
 :- chr_constraint
-   rdf_chr/3,
-   result/2.
-
-
+   x/2.
 
 rdf_chr(Trips, L3) :-
-  length(Trips, N),
-  debug(rdf(chr), "Adding ~D facts to CHR store.", [N]),
   maplist(rdf_chr_assert, Trips),
-  findall(Support-Term, find_chr_constraint(result(Support, Term)), Pairs1),
+  findall(Support-Term, find_chr_constraint(x(Support, Term)), Pairs1),
   sort(1, @>=, Pairs1, Pairs2),
   pairs_values(Pairs2, L1),
-  findall(rdf(S,P,O), find_chr_constraint(rdf_chr(S, P, O)), L2),
+  findall(rdf(S,P,O), find_chr_constraint(x(_, rdf(S,P,O))), L2),
   append(L1, L2, L3).
 
-rdf_chr_assert(rdf(S,P,O)) :- call(rdf_chr(S, P, O)).
-
+rdf_chr_assert(rdf(S,P,O)) :- call(x(1, rdf(S,P,O))).
 
 % Remove redundancies.
-result(X, Y) \ result(X, Y) <=> true.
+x(N, X) \ x(N, X) <=> true.
 
 % HTTP version.
-rdf_chr(_, 'http://lodlaundromat.org/ontology/version', V),
-rdf_chr(V, 'http://lodlaundromat.org/ontology/major', Major^^'http://www.w3.org/2001/XMLSchema#nonNegativeInteger'),
-rdf_chr(V, 'http://lodlaundromat.org/ontology/minor', Minor^^'http://www.w3.org/2001/XMLSchema#nonNegativeInteger')
+x(N1, rdf(_, 'http://lodlaundromat.org/ontology/HTTP-version', S)),
+x(N2, rdf(S, 'http://lodlaundromat.org/ontology/major', Major^^'http://www.w3.org/2001/XMLSchema#nonNegativeInteger')),
+x(N3, rdf(S, 'http://lodlaundromat.org/ontology/minor', Minor^^'http://www.w3.org/2001/XMLSchema#nonNegativeInteger')),
+x(N4, rdf(S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/HTTP-version'))
 <=>
-result(3, http_version(Major,Minor)).
+sum_list([N1,N2,N3,N4], N) |
+x(N, http_version(Major,Minor)).
 
 % Valid HTTP header.
-rdf_chr(S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/HTTP-header'),
-rdf_chr(S, 'http://lodlaundromat.org/ontology/parser', Status),
-rdf_chr(S, 'http://lodlaundromat.org/ontology/raw', Raw),
-rdf_chr(S, 'http://lodlaundromat.org/ontology/value', _),
-rdf_chr(_, P, S)
+x(N1, rdf(S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/HTTP-header')),
+x(N2, rdf(S, 'http://lodlaundromat.org/ontology/parser', Status)),
+x(N3, rdf(S, 'http://lodlaundromat.org/ontology/raw', Raw)),
+x(N4, rdf(S, 'http://lodlaundromat.org/ontology/value', _)),
+x(N5, rdf(_, P, S))
 <=>
-result(5, http_header(Status, P, Raw)).
+sum_list([N1,N2,N3,N4,N5], N) |
+x(N, http_header(Status, P, Raw)).
 
 % RDF processed statements counter.
-rdf_chr(S, 'http://lodlaundromat.org/ontology/processed-quadruples', NQuads),
-rdf_chr(S, 'http://lodlaundromat.org/ontology/processed-statements', NStmts),
-rdf_chr(S, 'http://lodlaundromat.org/ontology/processed-triples',    NTrips)
+x(N1, rdf(S, 'http://lodlaundromat.org/ontology/processed-quadruples', NQuads)),
+x(N2, rdf(S, 'http://lodlaundromat.org/ontology/processed-statements', NStmts)),
+x(N3, rdf(S, 'http://lodlaundromat.org/ontology/processed-triples',    NTrips))
 <=>
-result(3, rdf_processed_stmts(NQuads,NStmts,NTrips)).
+sum_list([N1,N2,N3], N) |
+x(N, rdf_processed_stmts(NQuads,NStmts,NTrips)).
 
 % Label in a preferred language.
-rdf_chr(S, P, Lbl@LTag),
-rdf_chr(S, P, ___@____)
+x(N1, rdf(S, P, Lbl@LTag)),
+x(N2, rdf(S, P, ___@____))
 <=>
-setting(user:language_priority_list, LRange), basic_filtering(LRange, LTag) |
-result(2, rdf(S,P,Lbl@LTag)).
+setting(user:language_priority_list, LRange),
+basic_filtering(LRange, LTag),
+sum_list([N1,N2], N) |
+x(N, rdf(S,P,Lbl@LTag)).
