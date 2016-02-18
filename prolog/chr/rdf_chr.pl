70ad64fd56f5fca0dbeae6ef0319716261c91c07
Wouter Beek <me@wouterbeek.com>
2016-02-19 00:23:29 +0100

    ADDED: More RDF/CHR widgets.

--- prolog/chr/rdf_chr.pl
+++ prolog/chr/rdf_chr.pl
@@ -14,8 +14,9 @@
 :- use_module(library(apply)).
 :- use_module(library(chr)).
 :- use_module(library(closure)).
-:- use_module(library(debug)).
+:- use_module(library(debug_ext)).
 :- use_module(library(pair_ext)).
+:- use_module(library(rdf/rdf_load)).
 :- use_module(library(rdf11/rdf11)).
 :- use_module(library(solution_sequences)).
 
@@ -36,8 +37,9 @@ rdf_chr(S, Widgets3) :-
   findall(widget(N,S,po_pair(P,O)), find_chr_constraint(triple(N, S, P, O)), Widgets2),
   append(Widgets1, Widgets2, Widgets3).
 
-rdf_chr_is_seed(_, S) :- rdf_is_bnode(S), !.
-rdf_chr_is_seed(1, S) :- rdf_chr:is_well_known_entity(S).
+rdf_chr_is_seed(_, _, S) :- rdf_is_bnode(S), !.
+rdf_chr_is_seed(_, P, _) :- rdf_equal(owl:sameAs, P), !.
+rdf_chr_is_seed(1, _, S) :- rdf_chr:is_well_known_entity(S).
 
 rdf_chr_assert_triple(rdf(S,P,O)) :- call(triple(1,S,P,O)).
 
@@ -55,38 +57,101 @@ rdf_chr_assert_triple(rdf(S,P,O)) :- call(triple(1,S,P,O)).
 
 s_triple(S, Trip) :- s_triple(0, S, Trip).
 
-s_triple(_, S, rdf(S,P,O)) :- rdf(S, P, O).
-s_triple(D1, S, Trip) :- rdf(S, _, O), rdf_chr_is_seed(D1, O), D2 is D1 + 1, s_triple(D2, O, Trip).
+s_triple(_, S, rdf(S,P,O)) :-
+  catch(rdf(S, P, O), _, fail).
+s_triple(D1, S, Trip) :-
+  catch(rdf(S, P, O), _, fail),
+  rdf_chr_is_seed(D1, P, O),
+  catch(rdf_load(O), _, fail),
+  D2 is D1 + 1,
+  s_triple(D2, O, Trip).
 
 
-widget(N1, S, http_header(P1-W1)),
-widget(N2, S, http_header(P2-W2))
+% Label in a preferred language.
+triple(N1, S, P, Lbl@LTag),
+triple(N2, S, P, ___@____)
+<=>
+setting(user:language_priority_list, LRange),
+basic_filtering(LRange, LTag),
+sum_list([N1,N2], N)
+| triple(N, S, P, Lbl@LTag).
+
+% Archive entry.
+triple(N1, S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/ArchiveEntry'),
+triple(N2, S, 'http://lodlaundromat.org/ontology/filetype', Filetype),
+triple(N3, S, 'http://lodlaundromat.org/ontology/format', Format),
+triple(N4, S, 'http://lodlaundromat.org/ontology/mtime', ModifiedTime),
+triple(N5, S, 'http://lodlaundromat.org/ontology/name', Name),
+triple(N6, _, _, S)
+<=>
+sum_list([N1,N2,N3,N4,N5,N6], N)
+| widget(N, S, archive_entry(Filetype,Format,ModifiedTime,Name)).
+
+% Raw archive entry.
+widget(_, _, archive_entry(_,"raw"^^_,_,_))
+<=>
+true.
+
+% RDF processed statements counter.
+triple(N1, S, 'http://lodlaundromat.org/ontology/processed_quadruples', NQuads),
+triple(N2, S, 'http://lodlaundromat.org/ontology/processed_statements', NStmts),
+triple(N3, S, 'http://lodlaundromat.org/ontology/processed_triples', NTrips)
+<=>
+sum_list([N1,N2,N3], N)
+| widget(N, S, rdf_counter(NQuads,NStmts,NTrips)).
+
+% HTTP version.
+triple(N1, O, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/Version'),
+triple(N2, O, 'http://lodlaundromat.org/ontology/major', Major),
+triple(N3, O, 'http://lodlaundromat.org/ontology/minor', Minor),
+triple(N4, S, 'http://lodlaundromat.org/ontology/version', O)
+<=>
+sum_list([N1,N2,N3,N4], N)
+| widget(N, S, http_version(Major,Minor)).
+
+% HTTP headers: recursive case.
+widget(N1, S, http_headers(T)),
+widget(N2, S, http_header(P,H))
+<=>
+sum_list([N1,N2], N),
+bot_pairs([P-H|T], L)
+| widget(N, S, http_headers(L)).
+
+% HTTP headers: base case.
+widget(N1, S, http_header(P1,W1)),
+widget(N2, S, http_header(P2,W2))
 <=>
 sum_list([N1,N2], N),
 bot_pairs([P1-W1,P2-W2], L)
 | widget(N, S, http_headers(L)).
 
-widget(N1, S, http_headers(T)),
-triple(N2, S, P, O),
-widget(N3, O, http_header(H))
-<=>
-sum_list([N1,N2,N3], N),
-bot_pairs([P-H|T], L)
-| widget(N, S, http_headers(L)).
-
-widget(N1, O, http_header(W)),
-triple(N2, S, P, O)
-<=>
-sum_list([N1,N2], N)
-| widget(N, S, http_header(P-W)).
-
-triple(N1, S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/ValidHttpHeader'),
-triple(N2, S, 'http://lodlaundromat.org/ontology/value', O),
-widget(N3, O, W)
+% HTTP info.
+widget(N1, S, http_headers(L)),
+widget(N2, S, http_version(Major,Minor)),
+triple(N3, S, 'http://lodlaundromat.org/ontology/status_code', Code)
 <=>
 sum_list([N1,N2,N3], N)
-| widget(N, S, http_header(W)).
+| widget(N, S, http_info(L,Major-Minor,Code)).
 
+% Valid HTTP header: literal value.
+triple(N1, O, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/ValidHttpHeader'),
+triple(N2, O, 'http://lodlaundromat.org/ontology/value', V),
+triple(N3, S, P, O)
+<=>
+rdf_is_literal(V),
+sum_list([N1,N2,N3], N)
+| widget(N, S, http_header(P, V)).
+
+% Valid HTTP header: complex value.
+triple(N1, O, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/ValidHttpHeader'),
+triple(N2, O, 'http://lodlaundromat.org/ontology/value', V),
+widget(N3, V, W),
+triple(N4, S, P, O)
+<=>
+sum_list([N1,N2,N3,N4], N)
+| widget(N, S, http_header(P,W)).
+
+% ETag
 triple(N1, S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/EntityTag'),
 triple(N2, S, 'http://lodlaundromat.org/ontology/opaque_tag', Tag),
 triple(N3, S, 'http://lodlaundromat.org/ontology/weak', Weak)
@@ -94,6 +159,7 @@ triple(N3, S, 'http://lodlaundromat.org/ontology/weak', Weak)
 sum_list([N1,N2,N3], N)
 | widget(N, S, etag(Tag,Weak)).
 
+% MediaType
 triple(N1, S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/MediaType'),
 triple(N2, S, 'http://lodlaundromat.org/ontology/type', Type),
 triple(N3, S, 'http://lodlaundromat.org/ontology/subtype', Subtype)
@@ -101,12 +167,20 @@ triple(N3, S, 'http://lodlaundromat.org/ontology/subtype', Subtype)
 sum_list([N1,N2,N3], N)
 | widget(N, S, media_type(Type,Subtype)).
 
+% Product
 triple(N1, S, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://lodlaundromat.org/ontology/Product'),
 triple(N2, S, 'http://lodlaundromat.org/ontology/name', Name),
 triple(N3, S, 'http://lodlaundromat.org/ontology/version', Version)
 <=>
 sum_list([N1,N2,N3], N)
 | widget(N, S, product(Name,Version)).
+
+% Skip blank nodes to get to widgets.
+widget(N1, B, W),
+triple(N2, S, P, B)
+<=>
+sum_list([N1,N2], N)
+| widget(N, S, P-W).
 
 
 
