dc808c5fd174b96157450d51bb1316f7a740b915
Wouter Beek <me@wouterbeek.com>
2016-02-21 15:49:29 +0000

    ADDED: rdfa_date_time//3.

--- prolog/rdfa/rdfa_api.pl
+++ prolog/rdfa/rdfa_api.pl
@@ -1,8 +1,7 @@
 :- module(
   rdfa_api,
   [
-    rdfa_date_time//1, % +V
-    rdfa_has_po//2,    % +S, +P
+    rdfa_date_time//3, % +P, +O, +Masks
     rdfa_prefixes/2    % +Aliases:list(atom), -Prefixes:atom
   ]
 ).
@@ -14,36 +13,36 @@
 */
 
 :- use_module(library(apply)).
+:- use_module(library(date_time/date_time)).
+:- use_module(library(html/html_date_time_human)).
+:- use_module(library(html/html_date_time_machine)).
 :- use_module(library(http/html_write)).
 :- use_module(library(pairs)).
 :- use_module(library(rdf/rdf_api)).
+:- use_module(library(rdfa/rdfa_api)).
 
 :- rdf_meta
-   rdfa_has_po(r, r, ?, ?),
-   rdfa_o(o, ?, ?).
+   rdfa_date_time(r, o, +, ?, ?).
 
 
 
-
-
-%! rdfa_date_time(+V)// is det.
-
-rdfa_date_time(V) -->
-  
-
-
-
-%! rdfa_has_po(+S, +P)// is det.
-
-rdfa_has_po(S, P) -->
-  {once(rdf_has(S, P, O))},
-  html(span(property=P, \rdfa_o(O))).
+rdfa_date_time(P1, Something^^D1, Masks) -->
+  {
+    something_to_date_time(Something, DT),
+    date_time_masks(Masks, DT, MaskedDT),
+    html_human_date_time(MaskedDT, HumanString),
+    html_machine_date_time(MaskedDT, MachineString),
+    maplist(rdfa_prefixed_iri, [P1,D1], [P2,D2])
+  },
+  html(time([datatype=D2,datetime=MachineString,property=P2], HumanString)).
 
 
 
-rdfa_o(O^^rdf:'HTML') --> !, html(\[O]).
-rdfa_o(O) --> {rdf_is_literal(O), !, rdf_lexical_form(O, Lex)}, html(Lex).
-rdfa_o(O) --> html(O).
+%! rdfa_prefixed_iri(+Iri, -PrefixedIri) is det.
+
+rdfa_prefixed_iri(Iri, PrefixedIri) :-
+  rdf_global_id(Alias:Local, Iri),
+  atomic_list_concat([Alias,Local], :, PrefixedIri).
 
 
 
