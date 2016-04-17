d47e9e3ffa52296d3fc116924ab3dda1daf672d3
Wouter Beek <me@wouterbeek.com>
2016-04-17 09:05:26 +0200

    ADDED: rdf_error from LOD-Laundromat.

--- app/deref.pl
+++ app/deref.pl
@@ -1,8 +1,24 @@
-:- module(deref, [deref_all/1,deref/1]).
+:- module(
+  deref,
+  [
+    deref_all/0,
+    deref_all/1, % +N
+    deref_iri/1  % +Iri
+  ]
+).
+
+/** <module> Dereference application
+
+@author Wouter Beek
+@author Niels Ockeloen
+@version 2016/04
+*/
 
 :- use_module(library(call_ext)).
 :- use_module(library(dcg/dcg_ext)).
+:- use_module(library(os/thread_ext)).
 :- use_module(library(print_ext)).
+:- use_module(library(rdf/rdf_error)).
 :- use_module(library(rdf/rdf_ext)).
 :- use_module(library(rdf/rdf_load)).
 :- use_module(library(rdf/rdf_print)).
@@ -17,51 +33,38 @@
 
 
 
+
+
+%! deref_all is det.
+%! deref_all(+N) is det.
+
+deref_all :-
+  default_number_of_threads(N),
+  deref_all(N).
+
+
 deref_all(N) :-
+  flag(deref, _, 0),
   setup_call_cleanup(
-    gzopen('/scratch/lodlab/crawls/13/iri.gz', read, Source),
-    deref_all(Source, N),
-    close(Source)
+    gzopen('/scratch/lodlab/crawls/iri.gz', read, In),
+    deref_all(In, N),
+    close(In)
   ).
 
 
-deref_all(Source, N) :-
+deref_all(In, N) :-
   % Remove the first line which contains RocksDB rubbish.
-  read_line_to_codes(Source, _),
-  deref_all_nonfirst(Source, N).
+  read_line_to_codes(In, _),
+  deref_all_nonfirst(In, N).
 
 
-deref_all_nonfirst(Source, N) :-
-  read_lines(Source, N, Lines),
+deref_all_nonfirst(In, N) :-
+  read_lines(In, N, Lines),
   (   Lines == []
   ->  true
   ;   concurrent_maplist(deref_line, Lines),
-      sleep(10),
-      deref_all_nonfirst(Source, N)
+      deref_all_nonfirst(In, N)
   ).
-
-
-deref_line(Cs) :-
-  phrase(deref_iri(NumDocs, Iri), Cs),
-  deref(Iri, NumDocs).
-
-
-deref_iri(NumDocs, Iri) -->
-  integer(NumDocs), " ", rest(Cs), {atom_codes(Iri, Cs)}.
-
-
-deref(Iri) :-
-  gtrace,
-  Opts = [metadata(M),parse_headers(true),triples(NumTriples),quads(NumQuads)],
-  rdf_call_on_graph(Iri, deref_graph(Iri), Opts),
-  print_dict(M),
-  format(user_output, "Number of triples: ~D~n", [NumTriples]),
-  format(user_output, "Number of quads: ~D~n", [NumQuads]).
-
-
-deref(Iri, NumDocs) :-
-  deref(Iri),
-  format(user_output, "Number of documents: ~D~n", [NumDocs]).
 
 
 deref_graph(Iri, G) :-
@@ -74,6 +77,41 @@ deref_graph(Iri, G) :-
   format(user_output, "Appears in object position: ~D~n", [NO]).
 
 
+deref_iri(Iri) :-
+  flag(deref, X, X + 1),
+  format(user_output, "~D~n", [X]),
+  (X = -1 -> gtrace ; true),
+  Opts = [metadata(M),parse_headers(true),triples(NumTriples),quads(NumQuads)],
+  (   catch(rdf_call_on_graph(Iri, deref_graph(Iri), Opts), E, writeln(E))
+  ->  (   var(E)
+      ->  print_dict(M),
+          msg_success("Number of triples: ~D~n", [NumTriples]),
+          msg_success("Number of quads: ~D~n", [NumQuads])
+      ;   store_exception(Iri, E)
+      ->  true
+      ;   print_message(E, error)
+      )
+  ;   msg_warning("HOPEFULLY THE ARCHIVE_CLOSE/1 HACK...~n")
+  ).
+
+
+deref_iri(Iri, NumDocs) :-
+  deref_iri(Iri),
+  format(user_output, "Number of documents: ~D~n", [NumDocs]).
+
+
+deref_iri(NumDocs, Iri) -->
+  integer(NumDocs), " ", rest(Cs), {atom_codes(Iri, Cs)}.
+
+
+
+deref_line(Cs) :-
+  phrase(deref_iri(NumDocs, Iri), Cs),
+  deref_iri(Iri, NumDocs).
+
+
+
+
 
 % DEBUG %
 
@@ -81,14 +119,25 @@ iri('http://dbpedia.org/resource/Tim_Berners-Lee').
 
 
 
+
+
 % HELPERS %
 
 read_lines(_, 0, []) :- !.
-read_lines(Source, N1, L) :-
-  read_line_to_codes(Source, H),
+read_lines(In, N1, L) :-
+  read_line_to_codes(In, H),
   (   H == end_of_file
   ->  L = []
   ;   N2 is N1 - 1,
       L = [H|T],
-      read_lines(Source, N2, T)
+      read_lines(In, N2, T)
+  ).
+
+
+
+store_exception(Iri, E) :-
+  setup_call_cleanup(
+    open('warn.nt', write, Out),
+    rdf_store_warning(Out, Iri, E),
+    close(Out)
   ).
---
+++ prolog/rdf/rdf_error.pl
@@ -0,0 +1,172 @@
+:- module(
+  rdf_error,
+  [
+    rdf_store_warning/3 % +Out, +Doc, +E
+  ]
+).
+
+/** <module> RDF error
+
+@author Wouter Beek
+@version 2016/03-2016/04
+*/
+
+:- use_module(library(atom_ext)).
+:- use_module(library(gen/gen_ntuples)).
+:- use_module(library(rdf/rdf_prefix)).
+:- use_module(library(semweb/rdf11)).
+:- use_module(library(uri)).
+
+:- rdf_meta
+   rdf_store(+, r, r, o).
+
+
+
+
+
+%! rdf_store(+Out, +S, +P, +O) is det.
+
+rdf_store(Out, S, P, O) :-
+  with_output_to(Out, gen_ntriple(S, P, O)).
+
+
+
+%! rdf_store_warning(+Out, +Doc, +E) is det.
+
+% Archive error
+rdf_store_warning(Out, Doc, error(archive_error(Code,_),_)) :-
+  (   Code == 2
+  ->  Name = missing_type_keyword_in_mtree_spec
+  ;   Code == 25
+  ->  Name = invalid_central_directory_signature
+  ), !,
+  rdf_global_id(llo:Name, O),
+  rdf_store(Out, Doc, llo:arhive_error, O).
+% Encoding: character
+rdf_store_warning(Out, Doc, error(type_error(character,Char),context(_,_))) :- !,
+  rdf_store(Out, Doc, llo:character_encoding_error, Char^^xsd:integer).
+% Existence: directory
+rdf_store_warning(Out, Doc, error(existence_error(directory,Dir),context(_,'File exists'))) :- !,
+  uri_file_name(Uri, Dir),
+  rdf_store(Out, Doc, llo:directory_existence_error, Uri^^xsd:anyURI).
+% Existence: file
+rdf_store_warning(Out, Doc, error(existence_error(file,File),context(_,Msg))) :-
+  (   Msg == 'Directory not empty'
+  ->  Name = directory_not_empty
+  ;   Msg == 'No such file or directory'
+  ->  Name = file_existence_error
+  ), !,
+  rdf_global_id(llo:Name, P),
+  uri_file_name(Uri, File),
+  rdf_store(Out, Doc, P, Uri^^xsd:anyURI).
+% Existence: source sink?
+rdf_store_warning(Out, Doc, error(existence_error(source_sink,Path),context(_,'Is a directory'))) :- !,
+  uri_file_name(Uri, Path),
+  rdf_store(Out, Doc, llo:is_a_directory_error, Uri^^xsd:anyURI).
+% HTTP status
+rdf_store_warning(Out, Doc, error(http_status(Status),_)) :-
+  (between(400, 499, Status) ; between(500, 599, Status)), !,
+  rdf_store(Out, Doc, llo:http_error, Status^^xsd:positiveInteger).
+% IO: read
+rdf_store_warning(Out, Doc, error(io_error(read,_),context(_,Msg))) :-
+  (   Msg == 'Connection reset by peer'
+  ->  Name = connection_reset_by_peer
+  ;   Msg == 'Inappropriate ioctl for device'
+  ->  Name = not_a_typewriter
+  ;   Msg = 'Is a directory'
+  ->  Name = is_a_directory
+  ), !,
+  rdf_global_id(llo:Name, O),
+  rdf_store(Out, Doc, llo:io_read_error, O).
+% IO: write
+rdf_store_warning(Out, Doc, error(io_error(write,_),context(_,'Encoding cannot represent character'))) :- !,
+  rdf_store(Out, Doc, llo:io_write_error, llo:encoding_error).
+% IO warning
+rdf_store_warning(Out, Doc, io_warning(_,Msg)) :-
+  (   Msg == 'Illegal UTF-8 continuation'
+  ->  Name = illegal_utf8_continuation
+  ;   Msg == 'Illegal UTF-8 start'
+  ->  Name = illegal_utf8_start
+  ),
+  rdf_global_id(llo:Name, O),
+  rdf_store(Out, Doc, llo:io_warning, O).
+% IRI
+rdf_store_warning(Out, Doc, E) :-
+  (   E = error(existence_error(source_sink,Name),_)
+  ->  true
+  ;   E = error(representation_error(max_path_length),context(system:is_absolute_file_name/1, _))
+  ->  Name = Doc
+  ;   E = error(permission_error(open,source_sink,Name),context(system:open/4,'Permission denied'))
+  ->  true
+  ), !,
+  rdf_store(Out, Doc, llo:not_an_iri, Name^^xsd:string).
+rdf_store_warning(Out, Doc, error(type_error(http_iri,Name),_)) :- !,
+  rdf_store(Out, Doc, llo:non_https_iri, Name^^xsd:anyURI).
+% Malformed URL
+rdf_store_warning(Out, Doc, error(domain_error(url,Url),_)) :- !,
+  rdf_store(Out, Doc, llo:malformed_url, Url^^xsd:anyURI).
+% No RDF
+rdf_store_warning(Out, Doc, error(no_rdf(_))) :- !,
+  rdf_store(Out, Doc, llo:rdf_serialization_format, llo:unrecognized_format).
+% Permission: redirect
+rdf_store_warning(Out, Doc, error(permission_error(redirect,http,Object),context(_,Msg1))) :- !,
+  atom_truncate(Msg1, 500, Msg2),
+  format(string(String), "[~a] ~a", [Object,Msg2]),
+  rdf_store(Out, Doc, llo:http_redirect_permission_error, String^^xsd:string).
+% SGML parser
+rdf_store_warning(Out, Doc, sgml(sgml_parser(_),_,Line,Msg1)) :- !,
+  atom_truncate(Msg1, 500, Msg2),
+  format(string(String), "[~w] ~a", [Line,Msg2]),
+  rdf_store(Out, Doc, llo:sgml_parser_error, String^^xsd:string).
+% Socket error
+rdf_store_warning(Out, Doc, error(socket_error(Msg),_)) :-
+  (   Msg == 'Connection timed out'
+  ->  Name = connection_timed_out
+  ;   Msg == 'Connection refused'
+  ->  Name = connection_refused
+  ;   Msg == 'No Data'
+  ->  Name = no_data
+  ;   Msg == 'No route to host'
+  ->  Name = no_route_to_host
+  ;   Msg == 'Host not found'
+  ->  Name = host_not_found
+  ;   Msg == 'Try Again'
+  ->  Name = try_again
+  ), !,
+  rdf_global_id(llo:Name, O),
+  rdf_store(Out, Doc, llo:socket_error, O).
+% SSL: SSL verify
+rdf_store_warning(Out, Doc, error(ssl_error(ssl_verify),_)) :- !,
+  rdf_store(Out, Doc, llo:ssl_error, llo:ssl_verify).
+% Syntax error
+rdf_store_warning(Out, Doc, error(syntax_error(Msg1),stream(_,Line,Col,Char))) :- !,
+  atom_truncate(Msg1, 500, Msg2),
+  format(string(String), "[~w:~w:~w] ~a", [Line,Col,Char,Msg2]),
+  rdf_store(Out, Doc, llo:syntax_error, String^^xsd:string).
+% Timeout: read
+rdf_store_warning(Out, Doc, error(timeout_error(read,_),context(_,_))) :- !,
+  rdf_store(Out, Doc, llo:timeout_error, llo:read).
+% Turtle: undefined prefix
+rdf_store_warning(Out, Doc, error(existence_error(turtle_prefix,Prefix), stream(_,Line,Col,Char))) :- !,
+  format(string(String), "[~w:~w:~w] ~a", [Line,Col,Char,Prefix]),
+  rdf_store(Out, Doc, llo:missing_turtle_prefix_defintion, String^^xsd:string).
+% RDF/XML: multiple definitions
+rdf_store_warning(Out, Doc, rdf(redefined_id(Uri))) :- !,
+  rdf_store(Out, Doc, llo:redefined_rdf_id, Uri^^xsd:anyURI).
+% RDF/XML: name
+rdf_store_warning(Out, Doc, rdf(not_a_name(XmlName))) :- !,
+  rdf_store(Out, Doc, llo:xml_name_error, XmlName^^xsd:string).
+% RDF/XML: unparsable
+rdf_store_warning(Out, Doc, rdf(unparsed(Dom))) :- !,
+  rdf11:in_xml_literal(xml, Dom, A1),
+  atom_truncate(A1, 500, A2),
+  rdf_store(Out, Doc, llo:rdf_xml_parser_error, A2^^xsd:string).
+% Non-canonical lexical form.
+rdf_store_warning(Out, Doc, non_canonical_lexical_form(D1,Lex)) :- !,
+  abbr_iri(D1, D2),
+  atom_concat(noncanonical_, D2, Name),
+  rdf_global_id(llo:Name, P),
+  rdf_store(Out, Doc, P, Lex^^xsd:string).
+% Unhandled error term.
+rdf_store_warning(Out, _, Term) :-
+  format(Out, "~w~n", [Term]).
