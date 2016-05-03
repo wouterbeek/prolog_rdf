ed898e796eca954b6b9cad6bec6fb585085fc4f6
Wouter Beek <me@wouterbeek.com>
2016-05-03 17:15:03 +0200

    CLEAN: Cleanups in closure module.

--- tmp/graph/rdf_graph_closure.pl
+++ tmp/graph/rdf_graph_closure.pl
@@ -1,24 +1,22 @@
 :- module(
   rdf_graph_closure,
   [
-    rdf_graph_closure/4 % +Resources1:ordset(rdf_term)
-                        % :Predicate
-                        % -Resources2:ordset(rdf_term)
-                        % -Propositions:ordset(list)
+    rdf_graph_closure/4 % +Ts1, :P, -Ts2, -Quads
   ]
 ).
 
 /** <module> RDF Graph Closure
 
 @author Wouter Beek
-@version 2014/01, 2015/12
+@version 2015/12, 2016/05
 */
 
 :- use_module(library(apply)).
 :- use_module(library(lists)).
 :- use_module(library(ordsets)).
 
-:- meta_predicate(rdf_graph_closure(+,3,-,-)).
+:- meta_predicate
+    rdf_graph_closure(+, 3, -, -).
 
 
 
@@ -28,6 +26,7 @@ rdf_graph_closure([], _, [], []) :- !.
 rdf_graph_closure([H|T], Pred, VSol, PropsSol) :-
   rdf_graph_closure([H|T], Pred, [H], VSol, [], PropsSol).
 
+
 rdf_graph_closure([H1|T1], Pred, Vs1, VSol, Props1, PropsSol) :-
   call(Pred, H1, Neighbors, NewProps),
   exclude(in_ordset(Vs1), Neighbors, NewNeighbors),
@@ -35,6 +34,7 @@ rdf_graph_closure([H1|T1], Pred, Vs1, VSol, Props1, PropsSol) :-
   ord_union(Vs1, Neighbors, Vs2),
   ord_union(Props1, NewProps, Props2),
   rdf_graph_closure(T2, Pred, Vs2, VSol, Props2, PropsSol).
+
 
 in_ordset(Set, Element) :-
   memberchk(Element, Set).
