2bf17e829227ae2c0b2817b97fd848421d1f2830
Wouter Beek <me@wouterbeek.com>
2016-05-13 15:44:45 +0200

    ADDED: register_prefix_cc/0. FIXED: Make several RDF prefix declarations in line with Prefix.cc CHANGED: Reimplements CSV-2-RDF conversion. CLEAN: Relocated BibTeX-2-RDF conversion.

--- prolog/rdf/rdf_csv.pl
+++ prolog/rdf/rdf_csv.pl
@@ -1,11 +1,7 @@
 :- module(
-  csv_to_rdf,
+  rdf_csv,
   [
-    csv_to_rdf/5 % +Source:stream
-                 % +Graph:rdf_graph
-                 % +SchemePrefix:iri
-                 % +DataPrefix:iri
-                 % +ClassName:atom
+    csv_to_rdf/2 % +Source, +Opts
   ]
 ).
 
@@ -14,50 +10,50 @@
 Automatic conversion from CSV to RDF.
 
 @author Wouter Beek
-@version 2014/02, 2014/05, 2014/08, 2014/11, 2015/03, 2015/12
+@version 2016/05
 */
 
 :- use_module(library(apply)).
 :- use_module(library(csv)).
+:- use_module(library(dcg/dcg_ext)).
+:- use_module(library(dcg/dcg_table)).
 :- use_module(library(debug)).
+:- use_module(library(os/open_any2)).
 :- use_module(library(pure_input)).
-:- use_module(library(rdf/rdf_ext)).
-:- use_module(library(rdfs/rdfs_api)).
-
-:- rdf_meta(csv_to_rdf(+,r,+,+,+)).
+:- use_module(library(semweb/rdf11)).
 
 
 
 
 
-%! csv_to_rdf(
-%!   +Source:stream,
-%!   +Graph:rdf_atom,
-%!   +SchemaPrefix:atom,
-%!   +DataPrefix:atom,
-%!   +ClassName:atom
-%! ) is det.
+%! csv_to_rdf(+Source, +Opts) is det.
 
-csv_to_rdf(Source, G, SchemaPrefix, DataPrefix, CName) :-
-  is_stream(Source), !,
+csv_to_rdf(Source, Opts) :-
+  call_on_stream(Source, {Opts}/[In,M,M]>>stream_csv_to_rdf(In, Opts)).
 
-  % Parse the CSV data.
-  phrase_from_stream(csv(Rows1), Source),
+
+stream_csv_to_rdf(In, Opts) :-
+  phrase_from_stream(csv(Rows, Opts), In),
+  gtrace,
+  dcg_with_output_to(current_output, dcg_table(Rows)).
+
+
+/*
   maplist(row_to_list, Rows1, Rows2),
   Rows2 = [Header|Rows3],
 
   % Convert the header row.
-  csv_header_to_rdf(G, SchemaPrefix, Header, Ps),
+  csv_header_to_rdf(G, TBox, Header, Ps),
 
   % Convert data rows.
-  rdf_global_id(SchemaPrefix:CName, C),
-  maplist(csv_row_to_rdf(DataPrefix, G, C, Ps), Rows3).
+  rdf_global_id(TBox:CName, C),
+  maplist(csv_row_to_rdf(ABox, G, C, Ps), Rows3).
 
 
 
 %! csv_header_to_rdf(
 %!   +Graph:rdf_graph,
-%!   +SchemaPrefix:atom,
+%!   +TBox:atom,
 %!   +Header:list,
 %!   -Properties:list(iri)
 %! ) is det.
@@ -66,21 +62,21 @@ csv_to_rdf(Source, G, SchemaPrefix, DataPrefix, CName) :-
 %   1. the unnamed entity represented by a data row, and
 %   2. the value that appears in a specific cell of that data row.
 
-csv_header_to_rdf(G, SchemaPrefix, Header, Ps) :-
-  maplist(csv_header_entry_to_rdf(G, SchemaPrefix), Header, Ps).
+csv_header_to_rdf(G, TBox, Header, Ps) :-
+  maplist(csv_header_entry_to_rdf(G, TBox), Header, Ps).
 
 
 
 %! csv_header_entry_to_rdf(
 %!   +Graph:rdf_graph,
-%!   +SchemaPrefix:atom,
+%!   +TBox:atom,
 %!   +HeaderEntry,
 %!   -Property:iri
 %! ) is det.
 
-csv_header_entry_to_rdf(G, SchemaPrefix, HeaderEntry, P) :-
+csv_header_entry_to_rdf(G, TBox, HeaderEntry, P) :-
   atom_phrase(rdf_property_name, HeaderEntry, LocalName),
-  rdf_global_id(SchemaPrefix:LocalName, P),
+  rdf_global_id(TBox:LocalName, P),
   rdfs_assert_domain(P, rdfs:'Resource', G),
   rdfs_assert_range(P, xsd:string, G).
 
@@ -112,7 +108,7 @@ rdf_property_name --> [].
 
 
 %! csv_row_to_rdf(
-%!   +DataPrefix:iri,
+%!   +ABox:iri,
 %!   +Graph:rdf_graph,
 %!   +Class:iri,
 %!   +Properties:list(iri),
@@ -121,9 +117,9 @@ rdf_property_name --> [].
 % Converts a CSV data row to RDF, using the RDF properties that were created
 %  based on the header row.
 
-csv_row_to_rdf(DataPrefix, G, C, Ps, Row) :-
+csv_row_to_rdf(ABox, G, C, Ps, Row) :-
   % A row is translated into an instance of the given class.
-  rdf_create_next_resource(DataPrefix, [], C, G, Entry),
+  rdf_create_next_resource(ABox, [], C, G, Entry),
   
   % Assert each cell in the given row.
   maplist(csv_cell_to_rdf(G, Entry), Ps, Row).
@@ -152,3 +148,4 @@ csv_cell_to_rdf(_, _, _, Val) :-
     "Will not convert non-graphic value \"~w\" to RDF.",
     [Val]
   ).
+*/
