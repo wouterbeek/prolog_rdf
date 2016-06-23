5ba90ff4b2dc59751178e50c5da44d3dace22302
Wouter Beek <me@wouterbeek.com>
2016-06-24 01:26:50 +0200

    update

--- prolog/conv/xml2rdf.pl
+++ prolog/conv/xml2rdf.pl
@@ -3,8 +3,8 @@
   [
     marcxml2rdf_graph/3, % +Source, +RecordNames, +G
     marcxml2rdf_graph/3, % +Source, +RecordNames, +G, +Opts
-    xml2rdf_graph/3,     % +Source, :RecordNames, +G
-    xml2rdf_graph/4      % +Source, :RecordNames, +G, +Opts
+    xml2rdf_graph/3,     % +Source, +RecordNames, +G
+    xml2rdf_graph/4      % +Source, +RecordNames, +G, +Opts
   ]
 ).
 
@@ -15,20 +15,15 @@
 */
 
 :- use_module(library(apply)).
+:- use_module(library(atom_ext)).
 :- use_module(library(conv/rdf_conv)).
 :- use_module(library(dict_ext)).
-:- use_module(library(http/http_download)).
 :- use_module(library(lists)).
+:- use_module(library(rdf/rdf_print)).
 :- use_module(library(semweb/rdf11)).
 :- use_module(library(xml/marcxml)).
 :- use_module(library(xml/xml_stream)).
 :- use_module(library(yall)).
-
-:- meta_predicate
-    xml2rdf_graph(+, 2, +),
-    xml2rdf_graph(+, 2, +, +).
-
-:- rdf_register_prefix(ex, 'http://example.org/').
 
 
 
@@ -43,14 +38,15 @@ marcxml2rdf_graph(Source, RecordNames, G) :-
 
 marcxml2rdf_graph(Source, RecordNames, G, Opts1) :-
   rdf_conv_alias_options(Opts1, Opts2),
+  Alias = Opts2.alias,
   xml_stream_record(
     Source,
     RecordNames,
-    {Opts2,G}/[Dom]>>marcxml2rdf_assert_record0(Dom, Opts2.alias, G)
+    {Alias,G}/[Dom]>>marcxml2rdf_assert_record0(Dom, Alias, G)
   ).
 
 
-marcxml2rdf_record0(Dom, Alias, G) :-
+marcxml2rdf_assert_record0(Dom, Alias, G) :-
   rdf_create_bnode(S),
   forall(
     marcxml2rdf_stmt0(Dom, Alias, P, Val),
@@ -91,16 +87,22 @@ xml2rdf_assert_record0(Dom, G, Opts) :-
   xml2rdf_assert_record0(Dom, [], S, G, Opts).
 
 
-xml2rdf_record0([], _, _, _, _) :- !.
-xml2rdf_record0([element(H, _, Os)|Dom], T, S, G, Opts) :-
+xml2rdf_assert_record0([], _, _, _, _) :- !.
+xml2rdf_assert_record0([Empty|Dom], T, S, G, Opts) :-
+  is_empty_atom(Empty), !,
+  xml2rdf_assert_record0(Dom, T, S, G, Opts).
+xml2rdf_assert_record0([element(H, _, Os)|Dom], T, S, G, Opts) :-
   (   maplist(atomic, Os)
   ->  reverse([H|T], L),
       atomic_list_concat(L, '_', Name),
       rdf_global_id(Opts.tbox_alias:Name, P),
-      forall(member(O, Os), (
+      forall((
+        member(O, Os),
+        \+ is_empty_atom(O)
+      ), (
         rdf_print_quad(S, P, O^^xsd:string, G),
         rdf_assert(S, P, O^^xsd:string, G)
       ))
-  ;   xml2rdf_record0(Os, [H|T], S, G, Opts)
+  ;   xml2rdf_assert_record0(Os, [H|T], S, G, Opts)
   ),
-  xml2rdf_record0(Dom, [], S, G, Opts).
+  xml2rdf_assert_record0(Dom, [], S, G, Opts).
