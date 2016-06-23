e20528d6a09ffc461490056c3635e40801f05945
Wouter Beek <me@wouterbeek.com>
2016-06-23 20:38:56 +0200

    ENHANCED: Rewrote RDF convert generic structure to (1) support custom file search path and (2) support vocabulary files.

--- prolog/conv/rdf_conv.pl
+++ prolog/conv/rdf_conv.pl
@@ -42,6 +42,12 @@
 %   * module(+atom) The name of the module which defines the goals.
 %   By default this is Name.
 %
+%   * search_path(+atom) The file search path where data files should
+%   be read/written from/to.  The default is Alias.
+%
+%   * vocab(+boolean) Whether or not a vocabulary is generates as
+%   well.  The default is `false`.
+%
 %   * void(+boolean) Whether or not a VoID description is generates as
 %   well.  The default is `false`.
 
@@ -50,35 +56,44 @@ rdf_conv(Name) :-
   rdf_conv(Name, _{}).
 
 
-rdf_conv(Name, Opts) :-
-  dict_get(alias, Opts, Name, Alias),
-  dict_get(module, Opts, Name, Mod),
-  rdf_conv_data(Alias, Mod, Name, Opts),
-  (get_dict(void, Opts, true) -> rdf_conv_void(Alias, Mod, Name, Opts) ; true).
+rdf_conv(Name, Opts1) :-
+  dict_get(alias, Opts1, Name, Alias),
+  dict_get(format, Opts1, ntriples, Format),
+  dict_get(module, Opts1, Name, Mod),
+  dict_get(search_path, Opts1, Alias, Search),
+  merge_dict(
+    Opts1,
+    _{alias: Alias, format: Format, module: Mod, search_path: Search},
+    Opts2
+  ),
+  rdf_conv_data(Name, Opts2),
+  (get_dict(vocab, Opts1, true) -> rdf_conv_vocab(Name, Opts2) ; true),
+  (get_dict(void, Opts1, true) -> rdf_conv_void(Name, Opts2) ; true).
 
 
-rdf_conv_data(Alias, Mod, Name, Opts) :-
-  dict_get(format, Opts, ntriples, Format),
-  rdf_conv_spec(Alias, Name, data, Format, Spec),
+rdf_conv_data(Name, Opts) :-
+  rdf_conv_spec(Name, data, Opts, Spec),
   rdf_conv_goal(Name, data, Pred_1),
-  Goal_1 = Mod:Pred_1,
-  rdf_conv_graph(Alias, Name, data, G),
-  rdf_load_file_or_call(Spec, Goal_1, G, [rdf_format(Format)]).
+  Goal_1 = Opts.module:Pred_1,
+  rdf_conv_graph(Name, data, Opts, G),
+  rdf_load_file_or_call(Spec, Goal_1, G, [rdf_format(Opts.format)]).
 
 
-rdf_conv_void(Alias, Mod, Name, Opts) :-
-  dict_get(format, Opts, ntriples, Format),
-  rdf_conv_spec(Alias, Name, data, Format, Spec0),
-  rdf_conv_spec(Alias, Name, void, Format, Spec),
+rdf_conv_vocab(Name, Opts) :-
+  rdf_conv_spec(Name, vocab, Opts, Spec),
+  rdf_conv_goal(Name, vocab, Pred_1),
+  Goal_1 = Opts.module:Pred_1,
+  rdf_conv_graph(Name, vocab, Opts, G),
+  rdf_load_file_or_call(Spec, Goal_1, G, [rdf_format(Opts.format)]).
+
+
+rdf_conv_void(Name, Opts) :-
+  rdf_conv_spec(Name, data, Opts, Spec0),
+  rdf_conv_spec(Name, void, Opts, Spec),
   rdf_conv_goal(Name, void, Pred_1),
-  Goal_1 = Mod:Pred_1,
-  rdf_conv_graph(Alias, Name, void, G),
-  rdf_load_file_or_call(
-    Spec,
-    source_to_void(Spec0, Goal_1),
-    G,
-    [rdf_format(Format)]
-  ).
+  Goal_1 = Opts.module:Pred_1,
+  rdf_conv_graph(Name, void, Opts, G),
+  rdf_load_file_or_call(Spec, source_to_void(Spec0, Goal_1), G, [rdf_format(Opts.format)]).
 
 
 
@@ -93,11 +108,11 @@ rdf_conv_base(Name, Kind, Base) :-
 
 
 
-%! rdf_conv_file(+Name, +Kind, +Format, -File) is det.
+%! rdf_conv_file(+Name, +Kind, +Opts, -File) is det.
 
-rdf_conv_file(Name, Kind, Format, File) :-
+rdf_conv_file(Name, Kind, Opts, File) :-
   rdf_conv_base(Name, Kind, Base),
-  rdf_file_extension(Ext, Format),
+  rdf_file_extension(Ext, Opts.format),
   file_name_extension(Base, Ext, File).
 
 
@@ -109,16 +124,16 @@ rdf_conv_goal(Name, Kind, Goal_1) :-
 
 
 
-%! rdf_conv_graph(+Alias, +Name, +Kind, -G) is det.
+%! rdf_conv_graph(+Name, +Kind, +Opts, -G) is det.
 
-rdf_conv_graph(Alias, Name, Kind, G) :-
+rdf_conv_graph(Name, Kind, Opts, G) :-
   rdf_conv_base(Name, Kind, Base),
-  rdf_global_id(Alias:Base, G).
+  rdf_global_id(Opts.alias:Base, G).
 
 
 
-%! rdf_conv_spec(+Alias, +Name, +Kind, +Format, -Spec) is det.
+%! rdf_conv_spec(+Name, +Kind, +Opts, -Spec) is det.
 
-rdf_conv_spec(Alias, Name, Kind, Format, Spec) :-
-  rdf_conv_file(Name, Kind, Format, File0),
-  Spec =.. [Alias,File0].
+rdf_conv_spec(Name, Kind, Opts, Spec) :-
+  rdf_conv_file(Name, Kind, Opts, File0),
+  Spec =.. [Opts.search_path,File0].
