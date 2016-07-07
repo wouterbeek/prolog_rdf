2a59274b070436cb1895667a0865188f6a73ac17
Wouter Beek <me@wouterbeek.com>
2016-07-07 17:53:06 +0200

    ADDED: Finalize LDF client.

--- prolog/service/ldf.pl
+++ prolog/service/ldf.pl
@@ -32,23 +32,16 @@
 %! ldf(?S, ?P, ?O, +Endpoint) is nondet.
 
 ldf(S, P, O, Endpoint) :-
-  ldf_iri(S, P, O, Endpoint, Iri, MetaG),
-  ldf_triple(Iri, MetaG, S, P, O).
-
-
-
-%! ldf_iri(?S, ?P, ?O, +Endpoint, -Iri, -MetaG) is det.
-%
-% Creates an IRI implementing a Linked Triple Fragments request.
-
-ldf_iri(S, P, O, Endpoint, Iri, MetaG) :-
   uri_components(Endpoint, uri_components(Scheme,Auth,Path,_,_)),
   maplist(ldf_parameter, [subject,predicate,object], [S,P,O], [S0,P0,O0]),
   exclude(var, [S0,P0,O0], Query0),
   uri_query_components(Query, Query0),
+  % An IRI that implements a Linked Triple Fragments request.
   uri_components(Iri, uri_components(Scheme,Auth,Path,Query,_)),
   atom_phrase(uri_optional_query_enc, Query, EncQuery),
-  uri_components(MetaG, uri_components(Scheme,Auth,Path,EncQuery,metadata)).
+  % The name of the metadata graph.
+  uri_components(MetaG, uri_components(Scheme,Auth,Path,EncQuery,metadata)),
+  ldf_from_iri(Iri, S, P, O, MetaG).
 
 
 
@@ -62,52 +55,46 @@ ldf_parameter(Key, Lit, Key=Val) :-
   (   q_is_lts(Lit)
   ->  Lit = _@LTag,
       format(atom(Val), '"~a"@~a', [Lex,LTag])
-  ;   z_literal_datatype(Lit, D),
-      format(atom(Val), '"~a"^^~a', [Lex,D])  Scheme = http,
-  Auth = 'ldf.lodlaundromat.org',
-  doc_name(Doc, Name),
-  atomic_list_concat(['',Name], /, Path),
-
+  ;   q_literal_datatype(Lit, D),
+      format(atom(Val), '"~a"^^~a', [Lex,D])
   ).
 ldf_parameter(Key, Val, Key=Val).
 
 
 
-%! ldf_triple(+Iri, +MetaG, ?S, ?P, ?O) is det.
+%! ldf_from_iri(+Iri, ?S, ?P, ?O, +MetaG) is det.
 
-ldf_triple(Iri, MetaG, S, P, O) :-
-  qb_iri(ex, G),
-  rdf_create_graph(G),
-  debug(ldf(load), "[LDF] Loading ~w", [Iri]),
-  catch(
-    rdf_load_file(Iri, [graph(G),rdf_format(nquads),silent(true)]),
-    E,
-    debug(ldf(load), "~w", [E])
-  ),
-  debug(ldf(load), "[LDF] Loaded ~w", [Iri]),
-  ldf_read_triple(MetaG, S, P, O, G).
+ldf_from_iri(Iri, S, P, O, MetaG) :-
+  rdf_tmp_graph(DataG),
+  rdf_load_file(Iri, [force_graph(DataG),rdf_format(nquads)]),
+  ldf_from_graph(S, P, O, DataG, MetaG).
 
 
 
-%! ldf_read_triple(+MetaG, ?S, ?P, ?O, ?G) is nondet.
+%! ldf_from_graph(?S, ?P, ?O, +DataG, +MetaG) is nondet.
 
-ldf_read_triple(_, S, P, O, G) :-
-  % NONDET
-  q(rdf, S, P, O, G).
-% If the metadata graph does not contain any triples then something is wrong.
-ldf_read_triple(MetaG, _, _, _, _) :-
+ldf_from_graph(S, P, O, DataG, _) :-
+  q(rdf, S, P, O, DataG).
+% If the metadata graph does not contain any triples then something is
+% wrong.
+ldf_from_graph(_, _, _, _, MetaG) :-
   \+ q(rdf, _, _, _, MetaG), !,
-  fail.
-  %%%%existence_error(ldf_metadata_graph, MetaG).
-% There is a metadata graph: check whether there is a next page with more results.
-ldf_read_triple(MetaG, S, P, O, G) :-
+  existence_error(ldf_metadata_graph, MetaG).
+% There is a metadata graph: check whether there is a next page with
+% more results.
+ldf_from_graph(S, P, O, DataG, MetaG) :-
   q(rdf, _, hydra:nextPage, Iri, MetaG), !,
-  debug(ldf(nextpage), "[LDF] Next page: ~w", [Iri]),
-  rdf_unload_graph(G),
-  rdf_unload_graph(MetaG),
-  ldf_triple(Iri, MetaG, S, P, O).
+  ldf_unload(DataG, MetaG),
+  ldf_from_iri(Iri, S, P, O, MetaG).
 % There is a metadata graph but it contains no more next page links.
-ldf_read_triple(MetaG, _, _, _, G) :-
-  rdf_unload_graph(G),
-  rdf_unload_graph(MetaG),
+ldf_from_graph(_, _, _, DataG, MetaG) :-
+  ldf_unload(DataG, MetaG),
   fail.
+
+
+
+%! ldf_unload(+DataG, +MetaG) is det.
+
+ldf_unload(DataG, MetaG) :-
+  rdf_unload_graph(DataG),
+  rdf_unload_graph(MetaG).
