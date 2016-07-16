46eed972f9f91151d65305e450512002cdd0649c
Wouter Beek <me@wouterbeek.com>
2016-07-16 09:03:23 +0200

    ADDED: Expose internal predicates for maximal control from within the calling context.   A GML writing event contains the following stages: (1) start: file init, (2) middle:   triple writing, (3) end: file close/sort/compress.

--- prolog/conv/rdf2gml.pl
+++ prolog/conv/rdf2gml.pl
@@ -1,33 +1,67 @@
 :- module(
   rdf2gml,
   [
-    rdf2gml/2, % +Source, +Sink
-    rdf2gml/3  % +Source, +Sink, +Opts
+    rdf2gml/1,       % +Source
+    rdf2gml/2,       % +Source, +Opts
+    rdf2gml_end/3,   % +NFile, +EFile, +GFile
+    rdf2gml_end/4,   % +NFile, +EFile, +GFile, +Opts
+    rdf2gml_start/4, % -NFile, -EFile, -GFile, -ExportOpts
+    rdf2gml_start/5, % +Opts, -NFile, -EFile, -GFile, -ExportOpts
+    rdf2gml_triple/6 % +EOut, +NOut, +S, +P, +O, +Opts
   ]
 ).
 
-/** <module> RDF GML
+/** <module> RDF-2-GML
+
+Grammar
+=======
+
+```bnf
+GML        ::= List
+List       ::= (whitespace* Key whitespace+ Value)*
+Value      ::= Integer | Real | String | [ List ]
+Key        ::= [ a-z A-Z ] [ a-z A-Z 0-9 ]*
+Integer    ::= sign digit+
+Real       ::= sign digit* . digit* mantissa
+String     ::= " instring "
+sign       ::= empty | + | -
+digit      ::= [0-9]
+Mantissa   ::= empty | E sign digit
+instring   ::= ASCII - {&,"} | & character+ ;
+whitespace ::= space | tabulator | newline
+```
+
+Keys
+====
+
+  - `comment` `string` Defines a comment embedded in a GML
+  file. Comments are ignored by the application.
+
+  - `id` `int` Defines an identification number for an object. This is
+  usually used to represent pointers.
+  
+  - `label` `string` Defines a label attached to an object.
 
 @author Wouter Beek
-@version 2016/02-2016/04, 2016/06
+@version 2016/02-2016/04, 2016/06-2016/07
 */
 
-:- use_module(library(semweb/rdf11)).
 :- use_module(library(apply)).
 :- use_module(library(dcg/dcg_ext)).
-:- use_module(library(debug_ext)).
 :- use_module(library(dict_ext)).
-:- use_module(library(hash_ext)).
 :- use_module(library(option)).
 :- use_module(library(os/compress_ext)).
 :- use_module(library(os/gnu_sort)).
 :- use_module(library(os/io)).
 :- use_module(library(os/process_ext)).
 :- use_module(library(pairs)).
+:- use_module(library(q/q_bnode_map)).
 :- use_module(library(q/q_print)).
 :- use_module(library(rdf/rdfio)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(thread)).
 :- use_module(library(uuid)).
+:- use_module(library(yall)).
 
 :- meta_predicate
     gml_label(4, +, -, +).
@@ -36,63 +70,108 @@
 
 
 
-%! rdf2gml(+Source, +Sink) is det.
-%! rdf2gml(+Source, +Sink, +Opts) is det.
+%! rdf2gml(+Source) is det.
+%! rdf2gml(+Source, +Opts) is det.
 %
 % Source can be any source, but Sink must be a file.  The reason for
 % this is that GML cannot be written in-stream because vertices and
 % edges are stored separately.
-%
-% Options are passed to:
-%
-%   * call_onto_streams/7
-%
-%   * gml_edge/5
-%
-%   * gml_node/4
-%
-%   * rdf_call_on_tuples/3
 
-rdf2gml(Source, Sink) :-
-  rdf2gml(Source, Sink, _{}).
+rdf2gml(Source) :-
+  rdf2gml(Source, []).
 
 
-rdf2gml(Source, Sink, Opts) :-
-  absolute_file_name(Sink, GFile, [access(write)]),
-  uuid(Base),
-  atomic_list_concat([Base,edges,tmp], ., EFile),
-  atomic_list_concat([Base,nodes,tmp], ., NFile),
-  call_cleanup(
-    (
-      call_onto_streams(Source, EFile, NFile, rdf2gml_stream(Opts), Opts, [], []),
-      % Sort the nodes to ensure there are no duplicates.
-      sort_file(NFile),
-      % Concatenate the nodes and edges into one file.
-      call_to_stream(GFile, writeln("graph [~n  directed 1")),
-      format(atom(CatCmd), "cat ~a ~a >> ~a", [NFile,EFile,GFile]),
-      run_process(sh, ['-c',CatCmd]),
-      call_to_stream(GFile, writeln("]")),
-      compress_file(GFile)
-    ),
-    concurrent_maplist(delete_file, [EFile,NFile])
-  ).
+rdf2gml(Source, Opts) :-
+  rdf2gml_start(Opts, NFile, EFile, GFile, ExportOpts),
+  call_onto_streams(
+    Source, NFile, EFile,
+    rdf2gml_stream0(Opts, ExportOpts),
+    Opts, [compression(false)]
+  ),
+  rdf2gml_end(NFile, EFile, GFile, Opts).
 
-rdf2gml_stream(Opts, In, Meta, Meta, NOut, EOut) :-
-  rdf_call_on_tuples_stream(In, gml_triple(EOut, NOut, Opts), Meta, Opts).
+
+rdf2gml_stream0(Opts, ExportOpts, In, Meta, Meta, NOut, EOut) :-
+  rdf_call_on_tuples_stream(In, rdf2gml_triple0(NOut, EOut, ExportOpts), Meta, Opts).
+
+
+rdf2gml_triple0(NOut, EOut, ExportOpts, _, S, P, O, _) :-
+  rdf2gml_triple(NOut, EOut, S, P, O, ExportOpts).
 
 
 
-
-%! gml_edge(+EOut, +NId1, +P, +NId2, +Opts) is det
+%! rdf2gml_end(+NFile, +EFile, +GFile) is det.
+%! rdf2gml_end(+NFile, +EFile, +GFile, +Opts) is det.
 %
 % The following options are supported:
 %
-%   * edge_label_printer(+callable)
+%   * compression(+boolean) Whether or not the GML file is compressed.
+%   Default is `true`.
 
-gml_edge(EOut, NId1, P, NId2, Opts) :-
-  get_dict(edge_label_printer, Opts, Dcg_4, dcg_print_predicate),
-  gml_label(Dcg_4, P, EL, Opts),
-  format(EOut, "  edge [ label \"~a\" source ~a target ~a ]~n", [EL,NId1,NId2]).
+rdf2gml_end(NFile, EFile, GFile) :-
+  rdf2gml_end(NFile, EFile, GFile, []).
+
+
+rdf2gml_end(NFile, EFile, GFile, Opts) :-
+  % Sort the nodes and edges to ensure there are no duplicates.
+  concurrent_maplist(sort_file, [NFile,EFile]),
+  % Concatenate the nodes and edges into one file.
+  call_to_stream(GFile, [Out]>>format(Out, "graph [~n  directed 1~n", []), [compression(false)]),
+  format(atom(CatCmd), "cat ~a ~a >> ~a", [NFile,EFile,GFile]),
+  run_process(sh, ['-c',CatCmd]),
+  call_to_stream(GFile, [Out]>>format(Out, "]~n", []), [compression(false)]),
+  (option(compression(true), Opts, true) -> compress_file(GFile) ; true),
+  concurrent_maplist(delete_file, [NFile,EFile]).
+
+
+
+%! rdf2gml_start(-NFile, -EFile, -GFile, -ExportOpts) is det.
+%! rdf2gml_start(+Opts, -NFile, -EFile, -GFile, -ExportOpts) is det.
+%
+% The following options are supported:
+%
+%   * compression(+boolean) Whether or not the GML file is compressed.
+%   Default is `true`.
+%
+%   * export_options(+dict) Options dictionary passed to GML writers.
+%   This includes the following options:
+%
+%     * edge_label_printer(+callable) The DCG writer for GML edges.
+%     The default is dcg_print_predicate//2.
+%
+%     * node_label_printer(+callable) The DCG writer for GML nodes.
+%     The default is dcg_print_node//2.
+
+rdf2gml_start(NFile, EFile, GFile, ExportOpts) :-
+  rdf2gml_start([], NFile, EFile, GFile, ExportOpts).
+
+
+rdf2gml_start(Opts, NFile, EFile, GFile, ExportOpts2) :-
+  option(export_options(ExportOpts1), Opts, _{}),
+  q_print:dcg_print_default_options(DefExportOpts),
+  merge_dicts(DefExportOpts, ExportOpts1, ExportOpts2),
+  uuid(Base),
+  atomic_list_concat([Base,nodes,tmp], ., NFile),
+  atomic_list_concat([Base,edges,tmp], ., EFile),
+  (option(compression(true), Opts, true) -> T = [gz] ; T = []),
+  atomic_list_concat([Base,graph,gml|T], ., GFile).
+
+
+
+%! rdf2gml_triple(+EOut, +NOut, +S, +P, +O, +ExportOpts) is det.
+
+rdf2gml_triple(NOut, EOut, S, P, O, ExportOpts) :-
+  maplist(gml_node(NOut, ExportOpts), [S,O], [NId1,NId2]),
+  gml_edge(EOut, NId1, P, NId2, ExportOpts).
+
+
+
+%! gml_edge(+EOut, +NId1, +P, +NId2, +ExportOpts) is det
+
+gml_edge(EOut, NId1, P, NId2, ExportOpts) :-
+  get_dict(edge_label_printer, ExportOpts, Dcg_4, dcg_print_predicate),
+  gml_label(Dcg_4, P, EL, ExportOpts),
+  format(EOut, "  edge [ label \"~a\" source ~d target ~d ]~n", [EL,NId1,NId2]).
 
 
 
@@ -112,34 +191,22 @@ gml_encode_label --> [].
 
 
 
-%! gml_label(:Dcg_4, +T, -Label, +Opts) is det.
+%! gml_label(:Dcg_4, +T, -Label, +ExportOpts) is det.
 
-gml_label(Dcg_4, T, Lbl, Opts) :-
-  phrase(dcg_call(Dcg_4, T, Opts), Cs1),
+gml_label(Dcg_4, T, Lbl, ExportOpts) :-
+  phrase(dcg_call(Dcg_4, T, ExportOpts), Cs1),
   phrase(gml_encode_label, Cs1, Cs2),
-  string_codes(Lbl, Cs2).
+  atom_codes(Lbl, Cs2).
 
 
 
-%! gml_node(+NOut, +Opts, +N, -NId) is det.
-%
-% The following options are supported:
-%
-%   * node_label_printer(+callable)
+%! gml_node(+NOut, +ExportOpts, +N, -NId) is det.
 
-gml_node(NOut, Opts, N, NId) :-
-  get_dict(node_label_printer, Opts, Dcg_4, dcg_print_term),
-  md5(N, NId),
-  gml_label(Dcg_4, N, VL, Opts),
-  format(NOut, "  node [ id ~a label \"~a\" ]~n", [NId,VL]).
-
-
-
-%! gml_triple(+EOut, +NOut, +Opts, +Meta, +S, +P, +O, +G) is det.
-
-gml_triple(EOut, NOut, Opts, _, S, P, O, _) :-
-  maplist(gml_node(NOut, Opts), [S,O], [NId1,NId2]),
-  gml_edge(EOut, NId1, P, NId2, Opts).
+gml_node(NOut, ExportOpts, N, NId) :-
+  get_dict(node_label_printer, ExportOpts, Dcg_4, dcg_print_node),
+  q_bnode_map(N, NId),
+  gml_label(Dcg_4, N, VL, ExportOpts),
+  format(NOut, "  node [ id ~d label \"~a\" ]~n", [NId,VL]).
 
 
 
