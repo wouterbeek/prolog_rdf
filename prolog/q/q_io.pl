348afd8c3202b81999be84d443618fad18ab478c
Wouter Beek <me@wouterbeek.com>
2016-08-05 21:20:05 +0200

    DOC: Documentation for Quine I/O.

--- prolog/q/q_io.pl
+++ prolog/q/q_io.pl
@@ -65,6 +65,46 @@
 
 /** <module> Quine file system
 
+Design principles:
+
+  1. _any_ file can be uploaded, starting with:
+
+    a. CSV
+
+    b. Turtle 1.1, N-Triples 1.1, N-Quads 1.1, TRiG 1.1
+
+    c. RDFa
+
+    d. JSON, NDJSON, JSONLD
+
+    e. Shapefile
+
+    f. XML, RDF/XML 1.1
+
+  2. over _any_ protocol, starting with:
+
+    a. file
+
+    b. HTTP(S) download
+
+  3. in _any_ archive format, starting with:
+
+    a. 7zip, ar, cab, cpio, empty, GNU Tar, ISO-9660, lha, RAR, raw,
+       tar, xar, zip
+
+  4. under _any_ compression filter, starting with:
+
+    a. bzip2, compress, gzip, grzip, lrzip, lzip, lzma, lzop, none,
+       rpm, uu, xz
+
+  5. If the file contains graphs then these are store into independent
+     files.  If the file does not contain graphs then all data is
+     stored in one graph.
+
+  6. Datasets are defined as non-empty lists of graph pointers.
+
+  7. Each graph has an IRI and a file name.  IRI and file name have a
+     one-to-one mapping.
 
 **Source layer** contains the raw sources that can be in any (also
 **non-RDF) format.
