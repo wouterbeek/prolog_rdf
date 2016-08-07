1353ecf3d3108ceb733b79fafdd3a3580e16b300
Wouter Beek <me@wouterbeek.com>
2016-08-07 17:17:32 +0000

    ADDED: Main namespace initialization @q_iri

--- prolog/q/q_iri.pl
+++ prolog/q/q_iri.pl
@@ -2,6 +2,7 @@
   q_iri,
   [
     q_abox_iri/4, % ?Domain, ?Concept, ?Ref, ?Iri
+    q_init_ns/0,
     q_tbox_iri/3  % ?Domain, ?Term, ?Iri
   ]
 ).
@@ -12,6 +13,8 @@
 @version 2016/08
 */
 
+:- use_module(library(q/qb)).
+:- use_module(library(settings)).
 :- use_module(library(uri)).
 
 
@@ -28,6 +31,20 @@ q_abox_iri(Domain, Concept, Ref, Iri) :-
 q_abox_iri(Domain, Concept, Ref, Iri) :-
   atomic_list_concat(['',id,Concept,Ref], /, Path),
   uri_components(Iri, uri_components(http,Domain,Path,_,_)).
+
+
+
+q_init_ns :-
+  setting(iri:data_scheme, Scheme),
+  setting(iri:data_auth, Host),
+  uri_components(Prefix1, uri_components(Scheme,Host,'/',_,_)),
+  qb_alias(ns, Prefix1),
+  uri_components(Prefix2, uri_components(Scheme,Host,'/def',_,'')),
+  qb_alias(nsdef, Prefix2),
+  uri_components(Prefix3, uri_components(Scheme,Host,'/doc/',_,_)),
+  qb_alias(nsdoc, Prefix3),
+  uri_components(Prefix4, uri_components(Scheme,Host,'/id/',_,_)),
+  qb_alias(nsid, Prefix4).
 
 
 
