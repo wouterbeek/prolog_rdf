ef9ebab3b2804b2321592737ede121c49ef7209d
Wouter Beek <me@wouterbeek.com>
2016-08-08 23:56:14 +0200

    FIXED: rdf_search/2 now works for multiple token patterns.

--- prolog/rdf/rdf_search.pl
+++ prolog/rdf/rdf_search.pl
@@ -1,27 +1,39 @@
 :- module(
   rdf_search,
   [
-    rdf_search/2 % +Pattern, -S
+    rdf_search/3 % +M, +Pattern, -Quad
   ]
 ).
 
 /** <module> RDF search
 
 @author Wouter Beek
-@version 2016/07
+@version 2016/07-2016/08
 */
 
+:- use_module(library(lists)).
+:- use_module(library(pl_term)).
+:- use_module(library(porter_stem)).
+:- use_module(library(q/q_stmt)).
+:- use_module(library(semweb/rdf_litindex)).
 :- use_module(library(semweb/rdf11)).
 
 :- rdf_meta
-   rdf_search(+, t).
+   rdf_search(+, +, t).
 
 
 
 
 
-%! rdf_search(+Pattern, -Quad) is nondet.
+%! rdf_search(+M, +Pattern, -Quad) is nondet.
 
-rdf_search(Pattern, rdf(S,P,Str^^D,G)) :-
-  {substring(Str, Pattern)},
-  rdf(S, P, Str^^D, G).
+rdf_search(M, Pattern, rdf(S,P,Lit,G)) :-
+  tokenize_atom(Pattern, [H|T]),
+  (T == [] -> Query = H ; n_ary_term(and, [H|T], Query)),
+  rdf_find_literals(Query, Strs),
+  member(Str, Strs),
+  (   q(M, S, P, Str^^D, G),
+      Lit = Str^^D
+  ;   q(M, S, P, Str@LTag, G),
+      Lit = Str@LTag
+  ).
