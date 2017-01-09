fb869a7d1e9fc687308b3eccf79d83fcd2b00861
Wouter Beek <me@wouterbeek.com>
2017-01-09 14:20:19 +0100

    CLEAN: RDF format guessing now works for all sources.
    This used to be only streams and files.

--- prolog/rdf/rdf_guess.pl
+++ prolog/rdf/rdf_guess.pl
@@ -1,10 +1,8 @@
 :- module(
   rdf_guess,
   [
-    rdf_guess_media_type/2,      % +In, -MT
-    rdf_guess_media_type/3,      % +In, -MT, +Opts
-    rdf_guess_media_type_file/2, % +File, -MT
-    rdf_guess_media_type_file/3  % +File, -MT, +Opts
+    rdf_guess_media_type/2, % +Source, -MT
+    rdf_guess_media_type/3  % +Source, -MT, +Opts
   ]
 ).
 
@@ -12,7 +10,7 @@
 
 @author Wouter Beek
 @author Jan Wielemaker
-@version 2015/08-2015/12, 2016/02-2016/04, 2016/11
+@version 2015/08-2015/12, 2016/02-2016/04, 2016/11, 2017/01
 */
 
 :- use_module(library(dcg/dcg_ext)).
@@ -27,22 +25,24 @@
 
 
 
-%! rdf_guess_media_type(+In, -MT) is det.
-%! rdf_guess_media_type(+In, -MT, +Opts) is det.
+%! rdf_guess_media_type(+Source, -MT) is det.
+%! rdf_guess_media_type(+Source, -MT, +Opts) is det.
 %
 % The following options are supported:
 %
 %   * default_rdf_media_type(+rdf_media_type)
 
-rdf_guess_media_type(In, MT) :-
-  rdf_guess_media_type(In, MT, []).
+rdf_guess_media_type(Source, MT) :-
+  rdf_guess_media_type(Source, MT, []).
 
 
-rdf_guess_media_type(In, MT, Opts) :-
-  rdf_guess_media_type0(In, 0, MT, Opts).
+rdf_guess_media_type(Source, MT, Opts) :-
+  call_on_stream(
+    Source,
+    {MT,Opts}/[In,InPath,InPath]>>rdf_guess_media_type_stream(In, 0, MT, Opts)
+  ).
 
-
-rdf_guess_media_type0(In, I, MT, Opts) :-
+rdf_guess_media_type_stream(In, I, MT, Opts) :-
   N is 1000 * 2 ^ I,
   peek_string(In, N, S),
   debug(rdf(guess), "[RDF-GUESS] ~s", [S]),
@@ -53,33 +53,15 @@ rdf_guess_media_type0(In, I, MT, Opts) :-
   ;   rdf_guess_xml(S, MT)
   ), !,
   debug(rdf(guess), "Assuming ~a based on heuristics.", [MT]).
-rdf_guess_media_type0(In, I1, MT, Opts) :-
+rdf_guess_media_type_stream(In, I1, MT, Opts) :-
   I1 < 4,
   I2 is I1 + 1,
-  rdf_guess_media_type0(In, I2, MT, Opts).
+  rdf_guess_media_type_stream(In, I2, MT, Opts).
 
-
-% For Turtle-family media_types it matters whether or not end of
+% For Turtle-family media_types it matters whether or not the end of
 % stream has been reached.
 rdf_guess_turtle(S, N, MT, Opts) :-
   % Do not backtrack if the whole stream has been peeked.
   string_length(S, M),
   ((M =:= 0 ; M < N) -> !, EoS = true ; EoS = false),
   string_phrase(rdf_guess_turtle(EoS, MT, Opts), S, _).
-
-
-
-
-%! rdf_guess_media_type_file(+File, -MT) is det.
-%! rdf_guess_media_type_file(+File, -MT, +Opts) is det.
-
-rdf_guess_media_type_file(File, MT) :-
-  rdf_guess_media_type_file(File, MT, []).
-
-
-rdf_guess_media_type_file(File, MT, Opts) :-
-  call_on_stream(
-    File,
-    {MT,Opts}/[In,Meta,Meta]>>rdf_guess_media_type(In, MT, Opts),
-    Opts
-  ).
