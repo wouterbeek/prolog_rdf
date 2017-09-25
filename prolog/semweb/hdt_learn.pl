fae2a7e83444592898abffa0226757cbb6ebafcd
Wouter Beek <wouter@triply.cc>
2017-09-25 23:40:55 +0200

    SYNC to ID-based HDT API.

--- prolog/semweb/hdt_learn.pl
+++ prolog/semweb/hdt_learn.pl
@@ -1,11 +1,12 @@
 :- module(
   hdt_learn,
   [
-    hdt_rnd_step/4, % +Node1, -EdgeLabel, -Node2, +Hdt
-    hdt_rnd_walk/4  % +Node, +Depth, -Walk, +Hdt
+    hdt_node_degree/3, % +Hdt, +Node, ?Degree
+    hdt_step_random/4, % +Hdt, +Node1, -EdgeLabel, -Node2
+    hdt_walk_random/4  % +Hdt, +Node, +Depth, -Walk
   ]
 ).
-:- reexport(library(hdt)).
+:- reexport(library(hdt_id)).
 
 /** <module> HDT as a Machine Learning bakcend
 
@@ -13,44 +14,90 @@
 @version 2017/09
 */
 
+:- use_module(library(dcg/dcg_ext)).
+:- use_module(library(debug)).
+:- use_module(library(lists)).
 :- use_module(library(random)).
 :- use_module(library(semweb/rdf11)).
+:- use_module(library(semweb/rdf_print)).
+
+:- debug(hdt_learn).
 
 
 
 
 
-%! hdt_rnd_step(+Node1:rdf_term, -EdgeLabel:rdf_iri, -Node2:rdf_term,
-%!              +Hdt:blob) is det.
+%! hdt_node_degree(+Hdt:blob, +Node, ?Degree:nonneg) is nondet.
 
-hdt_rnd_step(Node1, P, Node2, Hdt) :-
-  rdf_is_literal(Node1), !,
-  hdt_rnd(Node2, P, Node1, Hdt).
-hdt_rnd_step(Node1, P, Node2, Hdt) :-
-  hdt_count(Node1, P, _, A, Hdt),
-  (A =:= 0 -> true ; hdt_rnd(Node1, P, Node2A, Hdt)),
-  hdt_count(_, P, Node1, B, Hdt),
-  (B =:= 0 -> true ; hdt_rnd(Node2B, P, Node1, Hdt)),
-  AB is A + B,
-  random_between(1, AB, Offset),
-  (Offset =< A -> Node2 = Node2A ; Node2 = Node2B).
+hdt_node_degree(Hdt, Node, Degree) :-
+  hdt_triple_count(Node, _, _, OutDegree, Hdt),
+  hdt_triple_count(_, _, Node, InDegree, Hdt),
+  sum_list([OutDegree,InDegree], Degree).
 
 
 
-%! hdt_rnd_walk(+Node:rdf_node, +Depth:nonneg, -Walk:list(rdf_term),
-%!              +Hdt:blob) is det.
+%! hdt_step_random(+Hdt:blob, +Node1, -EdgeLabel, -Node2) is det.
+
+hdt_step_random(Hdt, V, E, W) :-
+  hdt_triple_count(Hdt, V, _, _, OutDegree),
+  hdt_triple_count(Hdt, _, _, V, InDegree),
+  sum_list([OutDegree,InDegree], Degree),
+  random_between(1, Degree, Random),
+  (   Random =< OutDegree
+  ->  % forward link
+      hdt_triple_random(Hdt, V, E, W),
+      (   debugging(hdt_learn)
+      ->  maplist(
+            hdt_term_translate(Hdt),
+            [term(_,ETerm),term(_,WTerm)],
+            [E,W]
+          ),
+          flag(number_of_steps, N, N+1),
+          dcg_debug(hdt_learn, (
+            thousands(N),
+            " ⮞ ",
+            rdf_dcg_iri(ETerm),
+            " ",
+            rdf_dcg_term(WTerm)
+          ))
+      ;   true
+      )
+  ;   % backward link
+      hdt_triple_random(Hdt, W, E, V),
+      (   debugging(hdt_learn)
+      ->  maplist(
+            hdt_term_translate(Hdt),
+            [term(_,ETerm),term(_,WTerm)],
+            [E,W]
+          ),
+          flag(number_of_steps, N, N+1),
+          dcg_debug(hdt_learn, (
+            thousands(N),
+            " ⮜ ",
+            rdf_dcg_term(WTerm),
+            " ",
+            rdf_dcg_iri(ETerm)
+          ))
+      ;   true
+      )
+  ).
+
+
+
+%! hdt_walk_random(+Hdt:blob, +Node, +Depth:nonneg, -Walk:list) is det.
 %
 % Walk is unified with a randomly chosen alternating sequence of
 % vertices and edges, not necessarily starting and ending at the same
 % vertex (/open/ walk).
 
-hdt_rnd_walk(Node, Depth, Walk, Hdt) :-
-  must_be(nonneg, Depth),
-  hdt_rnd_walk_(Node, Depth, Walk, Hdt).
+hdt_walk_random(Hdt, V, D, Walk) :-
+  must_be(nonneg, D),
+  (debugging(hdt_learn) -> flag(number_of_steps, _, 1) ; true),
+  hdt_walk_random_(Hdt, V, D, Walk).
 
 
-hdt_rnd_walk_(Node, 0, [Node], _) :- !.
-hdt_rnd_walk_(Node1, Depth1, [Node1,EdgeLabel|Walk], Hdt) :-
-  hdt_rnd_step(Node1, EdgeLabel, Node2, Hdt),
-  Depth2 is Depth1 - 1,
-  hdt_rnd_walk(Node2, Depth2, Walk, Hdt).
+hdt_walk_random_(_, V, 0, [V]) :- !.
+hdt_walk_random_(Hdt, V, D1, [V,E|Walk]) :-
+  hdt_step_random(Hdt, V, E, W),
+  D2 is D1 - 1,
+  hdt_walk_random_(Hdt, W, D2, Walk).
