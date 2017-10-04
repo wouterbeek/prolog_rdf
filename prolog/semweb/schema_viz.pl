174902399f5df866ee2b49a37afa5df2d557c6f5
Wouter Beek <wouter@triply.cc>
2017-10-04 15:47:41 +0200

    FIXED: Various small fixes to automatic schema export.

--- prolog/semweb/schema_viz.pl
+++ prolog/semweb/schema_viz.pl
@@ -15,9 +15,9 @@
 :- use_module(library(aggregate)).
 :- use_module(library(apply)).
 :- use_module(library(debug_ext)).
-:- use_module(library(file_ext)).
 :- use_module(library(graph/dot)).
 :- use_module(library(semweb/rdf_api)).
+:- use_module(library(zlib)).
 
 :- rdf_meta
    schema_viz(r, +).
@@ -31,28 +31,28 @@
 schema_viz(Format, ProcOut) :-
   setup_call_cleanup(
     graphviz(dot, ProcIn, Format, ProcOut),
-    call_to_stream(ProcIn, schema_viz(_), [compression(none)]),
+    schema_viz_stream(ProcIn),
     close(ProcIn)
   ).
 
 % Nodes can be described in multiple vocabularies: OWL, SHACL, RDF(S).
 % We therefore first group all nodes with some description, and then
 % generate nodes for each one in sequence.
-schema_viz(G, Out, Meta, Meta) :-
+schema_viz_stream(Out) :-
   viz_top(Out),
-  aggregate_all(set(Class), class(Class, G), Classes),
-  maplist(viz_class(Out, G), Classes),
-  aggregate_all(set(Edge), edge(Edge, G), Edges),
+  aggregate_all(set(Class), class(Class), Classes),
+  maplist(viz_class(Out), Classes),
+  aggregate_all(set(Edge), edge(Edge), Edges),
   maplist(viz_edge(Out), Edges),
   viz_bottom(Out).
 
 viz_top(Out) :-
   format_debug(dot, Out, "digraph G {").
 
-viz_class(Out, G, Class1) :-
+viz_class(Out, Class1) :-
   aggregate_all(
     set(Value),
-    rdf_list_member(trp, Class1, owl:oneOf, Value, G),
+    rdf_list_member(Class1, owl:oneOf, Value),
     Values
   ),
   Values \== [], !,
@@ -65,9 +65,9 @@ viz_class(Out, G, Class1) :-
     format_debug(dot, Out, "    <TR><TD>~a</TD></TR>", [Value])
   ),
   format_debug(dot, Out, "  </TABLE>>,shape=\"node\"];").
-viz_class(Out, G, Class) :-
+viz_class(Out, Class) :-
   viz_class_top(Out, Class),
-  aggregate_all(set(Pair), pp(Class, Pair, G), Pairs),
+  aggregate_all(set(Pair), pp(Class, Pair), Pairs),
   maplist(viz_class_pp(Out), Pairs),
   viz_class_bottom(Out).
 
@@ -95,9 +95,13 @@ viz_class_pp(Out, Segments-Target1) :-
 viz_class_bottom(Out) :-
   format_debug(dot, Out, "  </TABLE>>,shape=\"node\"];").
 
+
+
+%! viz_edge(+Out:stream, +Edge:compound) is det.
+
 viz_edge(Out, edge(Class1,⊆,Class2)) :- !,
   maplist(dot_id, [Class1,Class2], [Id1,Id2]),
-  format_debug(dot, Out, "  ~a -> ~a [arrowHead=\"empty\"];", [Id1,Id2]).
+  format_debug(dot, Out, "  ~a -> ~a [arrowhead=\"onormal\"];", [Id1,Id2]).
 viz_edge(Out, edge(Class1,Segments,Class2)) :-
   segments_sequence(Segments, Sequence),
   maplist(dot_id, [Class1,Class2], [Id1,Id2]),
@@ -106,49 +110,57 @@ viz_edge(Out, edge(Class1,Segments,Class2)) :-
 viz_bottom(Out) :-
   format_debug(dot, Out, "}").
 
-class(Class, G) :-
-  rdf(Class, owl:oneOf, _, G).
-class(Class, G) :-
-  rdf(Class, rdfs:subClassOf, _, G).
-class(Class, G) :-
-  rdf(_, rdfs:subClassOf, Class, G).
-class(Class, G) :-
-  rdf(Shape, rdf:type, sh:'NodeShape', G),
-  rdf(Shape, sh:targetClass, Class, G).
+class(Class) :-
+  rdf(Class, owl:oneOf, _).
+class(Class) :-
+  rdf(Class, rdfs:subClassOf, _).
+class(Class) :-
+  rdf(_, rdfs:subClassOf, Class).
+class(Class) :-
+  rdf(Shape, rdf:type, sh:'NodeShape'),
+  rdf(Shape, sh:targetClass, Class).
 
-pp(Class, Segments-Target, G) :-
-  rdf(Shape, sh:targetClass, Class, G),
-  rdf(Shape, sh:property, Property, G),
-  rdf(Property, sh:path, Path, G),
-  path_segments(Path, Segments, G),
-  target(Property, Target, G).
 
-target(Property, Datatype, G) :-
-  rdf(Property, sh:datatype, Datatype, G).
-target(Property, Class, G) :-
-  rdf(Property, sh:class, Class, G).
+%! pp(+Class:atom, -Pair:pair(list(atom),atom)) is nondet.
+%
+% Enumerates property path-target pairs for the given Class.
 
-edge(edge(Class1,⊆,Class2), G) :-
-  rdf(Class1, rdfs:subClassOf, Class2, G).
-edge(edge(Class1,Segments,Class2), G) :-
-  rdf(Shape, sh:targetClass, Class1, G),
-  rdf(Shape, sh:property, Property, G),
-  rdf(Property, sh:path, Path, G),
-  path_segments(Path, Segments, G),
-  rdf(Property, sh:class, Class2, G).
+pp(Class, Segments-Target) :-
+  rdf(Shape, sh:targetClass, Class),
+  rdf(Shape, sh:property, Property),
+  rdf(Property, sh:path, Path),
+  path_segments(Path, Segments),
+  target(Property, Target).
+
+target(Property, Datatype) :-
+  rdf(Property, sh:datatype, Datatype).
+target(Property, Class) :-
+  rdf(Property, sh:class, Class).
+
+edge(edge(Class1,⊆,Class2)) :-
+  rdf(Class1, rdfs:subClassOf, Class2).
+edge(edge(Class1,Segments,Class2)) :-
+  rdf(Shape, sh:targetClass, Class1),
+  rdf(Shape, sh:property, Property),
+  rdf(Property, sh:path, Path),
+  path_segments(Path, Segments),
+  rdf(Property, sh:class, Class2).
 
 dot_iri(Iri, Label) :-
   prefix_local_iri(Prefix, Local, Iri), !,
   atomic_list_concat([Prefix,Local], :, Label).
 dot_iri(Iri, Iri).
 
-path_segments(Path, Segments, G) :-
+% A property path is either composed on one segment or of a sequence
+% (`rdf:List') of segments.
+path_segments(Path, Segments) :-
   aggregate_all(
     set(Segment),
-    rdf_list_member(trp, Path, Segment, G),
+    rdf_list_member(Path, Segment),
     Segments
-  ), !.
-path_segments(Segment, [Segment], _).
+  ),
+  Segments \== [], !.
+path_segments(Segment, [Segment]).
 
 segments_sequence(Segments1, Sequence) :-
   maplist(dot_iri, Segments1, Segments2),
