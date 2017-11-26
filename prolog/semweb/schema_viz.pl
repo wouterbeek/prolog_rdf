a82b1999b028ae8b4193434ea76e43e5b0f1903c
Wouter Beek <wouter@triply.cc>
2017-11-26 11:06:24 +0100

    ADDED: extracted SHACL support to a separate module + rewrite.

--- prolog/semweb/schema_viz.pl
+++ prolog/semweb/schema_viz.pl
@@ -1,8 +1,7 @@
 :- module(
   schema_viz,
   [
-    class_hierarchy/2, % +Out, +G
-    shacl_viz/2        % +Out, +G
+    export_class_hierarchy/2, % +Out, +G
   ]
 ).
 :- reexport(library(graph/gv)).
@@ -20,21 +19,19 @@
 :- use_module(library(graph/graph_ext)).
 :- use_module(library(graph/gv)).
 :- use_module(library(semweb/rdf_api)).
+:- use_module(library(semweb/rdf_print)).
 :- use_module(library(uri/uri_ext)).
-:- use_module(library(yall)).
 
 :- rdf_meta
-   edge(t, r),
-   export_edge(+, t),
-   shacle_viz(+, r).
+   export_class_hierarchy(+, r).
 
 
 
 
 
-%! class_hierarchy(+Out:stream, +G:iri) is det.
+%! export_class_hierarchy(+Out:stream, +G:iri) is det.
 
-class_hierarchy(Out, G) :-
+export_class_hierarchy(Out, G) :-
   format_debug(dot, Out, "digraph class_hierarchy {"),
   rdf_equal(P, rdfs:subClassOf),
   aggregate_all(set(edge(C,[P],D)), rdf(C, P, D, G), Edges),
@@ -42,199 +39,6 @@ class_hierarchy(Out, G) :-
   edges_to_vertices(Edges, Nodes),
   maplist(export_node(Out), Nodes),
   format_debug(dot, Out, "}").
-
-
-
-%! shacl_viz(+Out:stream, +G:iri) is det.
-
-shacl_viz(Out, G) :-
-  % Nodes can be described in multiple vocabularies: OWL, SHACL,
-  % RDF(S).  We therefore first group all nodes with some description,
-  % and then generate nodes for each one in sequence.
-  format_debug(dot, Out, "digraph shacl {"),
-  aggregate_all(set(C), class(C, G), Cs),
-  maplist({Out,G}/[C]>>export_class(Out, C, G), Cs),
-  aggregate_all(set(Edge), edge(Edge, G), Edges),
-  maplist(export_edge(Out), Edges),
-  format_debug(dot, Out, "}").
-
-
-
-
-
-% QUERY %
-
-%! class(-C:atom, +G:atom) is nondet.
-%
-% Enumerates classes.
-
-class(C, G) :-
-  rdf(C, owl:oneOf, _, G).
-%class(C, G) :-
-%  rdf(_, rdf:type, C, G).
-class(C, G) :-
-  rdf(C, rdfs:subClassOf, _, G).
-class(C, G) :-
-  rdf(_, rdfs:subClassOf, C, G).
-class(C, G) :-
-  shape_class(C, G).
-
-
-
-%! class_values(+C:atom, -Values:list(atom), +G:atom) is det.
-
-class_values(C, Values, G) :-
-  aggregate_all(
-    set(Value),
-    rdf_triple_list_member(C, owl:oneOf, Value, G),
-    Values
-  ),
-  Values \== [].
-
-
-
-%! edge(-Edge:compound, +G:atom) is det.
-%
-% Enumerates edges that describe schema and shapes.
-
-edge(edge(C1,[rdfs:subClassOf],C2), G) :-
-  rdf(C1, rdfs:subClassOf, C2, G).
-edge(edge(C1,PP,C2), G) :-
-  rdf(Shape, sh:targetClass, C1, G),
-  rdf(Shape, sh:property, P, G),
-  rdf(P, sh:path, Path, G),
-  path_pp(Path, PP, G),
-  rdf(P, sh:class, C2, G).
-
-
-
-%! path_pp(+Path:atom, -PP:list(atom), +G:atom) is det.
-%
-% A property path is either composed on one segment or of a sequence
-% (`rdf:List').
-
-path_pp(Path, PP, G) :-
-  aggregate_all(
-    set(P),
-    rdf_list_member(P, Path, G),
-    PP
-  ),
-  PP \== [], !.
-path_pp(P, [P], _).
-
-
-
-%! pp(+C:atom, -Pair:pair(list(atom),atom), +G:atom) is nondet.
-%
-% Enumerates property path-target pairs for the given class `C'.
-
-pp(C, PP-Target, G) :-
-  rdf(Shape, sh:targetClass, C, G),
-  rdf(Shape, sh:property, Property, G),
-  rdf(Property, sh:path, Path, G),
-  path_pp(Path, PP, G),
-  target(Property, Target, G).
-
-
-
-%! shape_class(+C:atom, +G:atom) is semidet.
-%! shape_class(-C:atom, +G:atom) is nondet.
-
-shape_class(C, G) :-
-  rdf(Shape, rdf:type, sh:'NodeShape', G),
-  rdf(Shape, sh:targetClass, C, G).
-
-
-
-%! target(+P:atom, -Target:atom, +G:atom) is det.
-%
-% Target is the shape range for P.
-
-target(P, D, G) :-
-  rdf(P, sh:datatype, D, G).
-target(P, C, G) :-
-  rdf(P, sh:class, C, G).
-
-
-
-
-
-% LABELS %
-
-%! iri_label(+Iri:atom, -Label:string) is det.
-
-iri_label(Iri, Label) :-
-  rdf_prefix_iri(Prefix:Local, Iri), !,
-  atomics_to_string([Prefix,Local], ":", Label).
-iri_label(Iri, Label) :-
-  atom_string(Iri, Label).
-
-
-
-%! pp_label(+PP:list(atom), -PP_Label:string) is det.
-
-pp_label(PP, PP_Label) :-
-  maplist(iri_label, PP, PP_Labels),
-  atomics_to_string(PP_Labels, /, PP_Label).
-
-
-
-
-
-% EXPORT %
-
-%! export_class(+Out:stream, +C:atom, +G:atom) is det.
-
-export_class(Out, C, G) :-
-  class_values(C, Values, G), !,
-  dot_id(C, CId),
-  format_debug(dot, Out, "  ~a [label=<<TABLE>", [CId]),
-  iri_label(C, CLabel),
-  format_debug(dot, Out, "    <TR><TD><B>~a</B></TD></TR>", [CLabel]),
-  maplist(export_value(Out), Values),
-  format_debug(dot, Out, '  </TABLE>>,shape="none",URL="~a"];', [C]).
-export_class(Out, C, G) :-
-  export_class_top(Out, C),
-  aggregate_all(set(PP_Target), pp(C, PP_Target, G), PP_Targets),
-  maplist(export_class_pp(Out), PP_Targets),
-  export_class_bottom(Out, C).
-
-
-
-%! export_class_bottom(+Out:stream, +C:atom) is det.
-
-export_class_bottom(Out, C) :-
-  format_debug(dot, Out, '  </TABLE>>,shape="none",URL="~a"];', [C]).
-
-
-
-%! export_class_pp(+Out:stream, +PP_Target:pair(list(atom),atom)) is det.
-
-export_class_pp(Out, PP-Target) :-
-  pp_label(PP, PP_Label),
-  iri_label(Target, TargetLabel),
-  format_debug(
-    dot,
-    Out,
-    "    <TR><TD>~a</TD><TD>~a</TD></TR>",
-    [PP_Label,TargetLabel]
-  ).
-
-
-
-%! export_class_top(+Out:stream, +C:atom) is det.
-
-export_class_top(Out, C) :-
-  dot_id(C, CId),
-  format_debug(dot, Out, "  ~a [label=<<TABLE>", [CId]),
-  iri_label(C, CLabel),
-  format_debug(
-    dot,
-    Out,
-    '    <TR><TD COLSPAN="2"><B>~a</B></TD></TR>',
-    [CLabel]
-  ).
-
 
 
 %! export_edge(+Out:stream, +Edge:compound) is det.
@@ -245,14 +49,13 @@ export_edge(Out, edge(C1,[P],C2)) :-
   % By swapping the order in which the nodes are asserted, we are able
   % to show superclasses above subclasses.
   dot_edge(Out, Id2, Id1, [arrowtail(onormal),dir(back),'URL'(P)]).
-export_edge(Out, edge(C1,PP,C2)) :-
-  pp_label(PP, Label),
+export_edge(Out, edge(C1,Ps,C2)) :-
+  property_path_label(Ps, Label),
   % We cannot put in URLs for all property path members, so we only
   % take the first one.
-  PP = [P|_],
+  Ps = [P|_],
   maplist(dot_id, [C1,C2], [Id1,Id2]),
   dot_edge(Out, Id1, Id2, [label(Label),'URL'(P)]).
-
 
 
 %! export_node(+Out:stream, +Node:iri) is det.
@@ -265,11 +68,3 @@ export_node(Out, Node) :-
   iri_label(Node, Label),
   (is_http_uri(Node) -> T = ['URL'(Node)] ; T = []),
   dot_node(Out, Id, [label(Label),shape(rect)|T]).
-
-
-
-%! export_value(+Out:stream, +Value:atom) is det.
-
-export_value(Out, Value) :-
-  iri_label(Value, ValueLabel),
-  format_debug(dot, Out, "    <TR><TD>~a</TD></TR>", [ValueLabel]).
---
+++ prolog/semweb/shacl.pl
@@ -0,0 +1,189 @@
+:- module(
+  shacl,
+  [
+    shacl_assert_class/1, % +Entry
+    shacl_assert_class/2, % +Entry, +G
+    shacl_export/2        % +Out, +G
+  ]
+).
+
+/** <module> SHACL
+
+@author Wouter Beek
+@version 2017/11
+*/
+
+:- use_module(library(aggregate)).
+:- use_module(library(apply)).
+:- use_module(library(debug_ext)).
+:- use_module(library(graph/gv)).
+:- use_module(library(semweb/rdf_api)).
+:- use_module(library(semweb/rdf_print)).
+:- use_module(library(yall)).
+
+:- rdf_meta
+   shacl_assert_class(+, r),
+   shacle_export(+, r).
+
+
+
+
+
+%! shacl_assert_class(+Entry:compound) is det.
+%! shacl_assert_class(+Entry:compound, +G:iri) is det.
+
+shacl_assert_class(Entry) :-
+  rdf_default_graph(G),
+  shacl_assert_class(Entry, G).
+
+
+shacl_assert_class(C-Groups, G) :-
+  atom_concat(C, 'Shape', Shape),
+  rdf_assert(Shape, rdf:type, sh:'NodeShape', G),
+  rdf_assert(Shape, sh:targetClass, C, G),
+  rdf_assert_list(Shape, sh:ignoredProperties, [rdf:type], G),
+  maplist(shacl_assert_property(C, G), Groups).
+
+shacl_assert_property(C, G, P-Os) :-
+  rdf_create_bnode(BNode),
+  rdf_assert(C, sh:property, BNode, G),
+  rdf_assert(BNode, sh:path, P, G),
+  (   Os = [O]
+  ->  rdf_assert(BNode, sh:class, O, G)
+  ;   maplist(shacl_assert_object(G), Os, BNodes),
+      rdf_assert_list(BNode, sh:or, BNodes, G)
+  ).
+
+shacl_assert_object(G, O, BNode) :-
+  rdf_create_bnode(BNode),
+  rdf_assert(BNode, sh:class, O, G).
+
+
+
+%! shacl_export(+Out:stream, +G:iri) is det.
+
+shacl_export(Out, G) :-
+  % top of graph
+  format_debug(dot, Out, "digraph shacl {"),
+  % Nodes can be described in multiple vocabularies: OWL, SHACL,
+  % RDF(S).  We therefore first group all nodes with some description,
+  % and then generate nodes for each one in sequence.
+  aggregate_all(set(C), shacl_class(C, G), Cs),
+  maplist({Out,G}/[C]>>shacl_export_class(Out, C, G), Cs),
+  aggregate_all(set(Edge), shacl_edge(Edge, G), Edges),
+  maplist(shacl_export_edge(Out), Edges),
+  % bottom of graph
+  format_debug(dot, Out, "}").
+
+
+
+%! shacl_export_class(+Out:stream, +C:atom, +G:atom) is det.
+
+% The instances of class `C' form an OWL value list.
+shacl_export_class(Out, C, G) :-
+  aggregate_all(
+    set(Value),
+    rdf_triple_list_member(C, owl:oneOf, Value, G),
+    Values
+  ),
+  Values \== [], !,
+  dot_id(C, CId),
+  % top of node
+  format_debug(dot, Out, "  ~a [label=<<TABLE>", [CId]),
+  iri_label(C, CLabel),
+  format_debug(dot, Out, "    <TR><TD><B>~a</B></TD></TR>", [CLabel]),
+  % middle of node
+  forall(
+    member(Value, Values),
+    (
+      iri_label(Value, ValueLabel),
+      format_debug(dot, Out, "    <TR><TD>~a</TD></TR>", [ValueLabel])
+    )
+  ),
+  % bottom of node
+  format_debug(dot, Out, '  </TABLE>>,shape="none",URL="~a"];', [C]).
+% The instances of class `C' are related to certain property/range
+% pairs.
+shacl_export_class(Out, C, G) :-
+  dot_id(C, CId),
+  % top of node
+  format_debug(dot, Out, "  ~a [label=<<TABLE>", [CId]),
+  iri_label(C, CLabel),
+  format_debug(
+    dot,
+    Out,
+    '    <TR><TD COLSPAN="2"><B>~a</B></TD></TR>',
+    [CLabel]
+  ),
+  % middle of node
+  aggregate_all(set(Pair), shacl_property_path_datatype(C, Pair, G), Pairs),
+  forall(
+    member(Ps-O, Pairs),
+    (
+      property_path_label(Ps, PsLabel),
+      iri_label(O, OLabel),
+      format_debug(
+        dot,
+        Out,
+        "    <TR><TD>~a</TD><TD>~a</TD></TR>",
+        [PsLabel,OLabel]
+      )
+    )
+  ),
+  % bottom of node
+  format_debug(dot, Out, '  </TABLE>>,shape="none",URL="~a"];', [C]).
+  
+
+
+
+
+% HELPERS %
+
+%! shacl_class(-C:iri, ?G:rdf_graph) is nondet.
+
+shacl_class(C, G) :-
+  rdf(C, owl:oneOf, _, G).
+%shacl_class(C, G) :-
+%  rdf(_, rdf:type, C, G).
+shacl_class(C, G) :-
+  rdf(C, rdfs:subClassOf, _, G).
+shacl_class(C, G) :-
+  rdf(_, rdfs:subClassOf, C, G).
+shacl_class(C, G) :-
+  rdf(Shape, rdf:type, sh:'NodeShape', G),
+  rdf(Shape, sh:targetClass, C, G).
+
+
+
+%! shacl_edge(-Edge:edge, ?G:rdf_graph) is nondet.
+
+shacl_edge(edge(C,[rdfs:subClassOf],D), G) :-
+  rdf(C, rdfs:subClassOf, D, G).
+shacl_edge(edge(C,Ps,D), G) :-
+  shacl_property_path_class(C, Ps-D, G).
+
+
+
+%! shacl_property_path_class(+C:iri, -Pair:pair(list(iri),iri),
+%!                           ?G:rdf_graph) is nondet.
+
+shacl_property_path_class(C, Ps-D, G) :-
+  rdf(Shape, sh:targetClass, C, G),
+  rdf(Shape, sh:property, Property, G),
+  rdf(Property, sh:path, Path, G),
+  aggregate_all(set(P), rdf_list_member(P, Path, G), Ps),
+  assertion(Ps \== []),
+  rdf(Property, sh:class, D, G).
+
+
+
+%! shacl_property_path_datatype(+C:iri, -Pair:pair(list(iri),iri),
+%!                              ?G:rdf_graph) is nondet.
+
+shacl_property_path_datatype(C, Ps-D, G) :-
+  rdf(Shape, sh:targetClass, C, G),
+  rdf(Shape, sh:property, Property, G),
+  rdf(Property, sh:path, Path, G),
+  aggregate_all(set(P), rdf_list_member(P, Path, G), Ps),
+  assertion(Ps \== []),
+  rdf(Property, sh:datatype, D, G).
