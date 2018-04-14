86aece3cec0525234c76315a90d51090c14d38e6
Wouter Beek <wouter@triply.cc>
2018-04-14 22:04:49 +0200

    ADDED: Support for pretty-printing generalized triple patterns.

--- prolog/sw/rdf_print.pl
+++ prolog/sw/rdf_print.pl
@@ -2,8 +2,22 @@
 :- module(
   rdf_print,
   [
-    rdf_dcg_term//1, % +Term
-    rdf_dcg_term//2  % +Term, +Options
+    rdf_dcg_generalized_triple_pattern//1, % +TriplePattern
+    rdf_dcg_generalized_triple_pattern//2, % +TriplePattern, +Options
+    rdf_dcg_generalized_triple_pattern//3, % ?S, ?P, ?O
+    rdf_dcg_generalized_triple_pattern//4, % ?S, ?P, ?O, +Options
+    rdf_dcg_term//1,                       % +Term
+    rdf_dcg_term//2,                       % +Term, +Options
+    rdf_dcg_term_or_var//1,                % ?Term
+    rdf_dcg_term_or_var//2,                % ?Term, +Options
+    rdf_dcg_triple//1,                     % +Triple
+    rdf_dcg_triple//2,                     % +Triple, +Options
+    rdf_dcg_triple//3,                     % +S, +P, +O
+    rdf_dcg_triple//4,                     % +S, +P, +O, +Options
+    rdf_dcg_triple_pattern//1,             % +TriplePattern
+    rdf_dcg_triple_pattern//2,             % +TriplePattern, +Options
+    rdf_dcg_triple_pattern//3,             % ?S, ?P, ?O
+    rdf_dcg_triple_pattern//4              % ?S, ?P, ?O, +Options
   ]
 ).
 
@@ -13,6 +27,7 @@
 @version 2018
 */
 
+:- use_module(library(error)).
 :- use_module(library(lists)).
 
 :- use_module(library(dcg)).
@@ -20,7 +35,53 @@
 :- use_module(library(sw/rdf_prefix)).
 :- use_module(library(sw/rdf_term)).
 
+:- rdf_meta
+   rdf_dcg_generalized_triple_pattern(t, ?, ?),
+   rdf_dcg_generalized_triple_pattern(t, +, ?, ?),
+   rdf_dcg_generalized_triple_pattern(r, r, o, ?, ?),
+   rdf_dcg_generalized_triple_pattern(r, r, o, +, ?, ?),
+   rdf_dcg_term(o, ?, ?),
+   rdf_dcg_term(o, +, ?, ?),
+   rdf_dcg_term_or_var(o, ?, ?),
+   rdf_dcg_term_or_var(o, +, ?, ?),
+   rdf_dcg_triple(t, ?, ?),
+   rdf_dcg_triple(t, +, ?, ?),
+   rdf_dcg_triple(r, r, o, ?, ?),
+   rdf_dcg_triple(r, r, o, +, ?, ?),
+   rdf_dcg_triple_pattern(t, ?, ?),
+   rdf_dcg_triple_pattern(t, +, ?, ?),
+   rdf_dcg_triple_pattern(r, r, o, ?, ?),
+   rdf_dcg_triple_pattern(r, r, o, +, ?, ?).
 
+
+
+
+
+%! rdf_dcg_generalized_triple_pattern(+TriplePattern:rdf_triple_pattern)// is det.
+%! rdf_dcg_generalized_triple_pattern(+TriplePattern:rdf_triple_pattern, +Options:dict)// is det.
+%! rdf_dcg_generalized_triple_pattern(?S:rdf_nonliteral, ?P:iri, ?O:rdf_term)// is det.
+%! rdf_dcg_generalized_triple_pattern(?S:rdf_nonliteral, ?P:iri, ?O:rdf_term, +Options:dict)// is det.
+
+rdf_dcg_generalized_triple_pattern(TriplePattern) -->
+  rdf_dcg_generalized_triple_pattern(TriplePattern, _{}).
+
+
+rdf_dcg_generalized_triple_pattern(rdf(S,P,O), Options) -->
+  rdf_dcg_generalized_triple_pattern(S, P, O, Options).
+
+
+rdf_dcg_generalized_triple_pattern(S, P, O) -->
+  rdf_dcg_generalized_triple_pattern(S, P, O, _{}).
+
+
+rdf_dcg_generalized_triple_pattern(S, P, O, Options) -->
+  "〈",
+  rdf_dcg_term_or_var(S, Options),
+  ", ",
+  rdf_dcg_term_or_var(P, Options),
+  ", ",
+  rdf_dcg_term_or_var(O, Options),
+  "〉".
 
 
 
@@ -60,6 +121,16 @@ inf_minus(X, Y, Z) :-
 
 
 
+%! rdf_dcg_iri_or_var(?Iri:rdf_iri, +Options:dict)// is det.
+
+rdf_dcg_iri_or_var(NonGround, _) -->
+  {\+ ground(NonGround)}, !,
+  term(NonGround).
+rdf_dcg_iri_or_var(Iri, Options) -->
+  rdf_dcg_iri(Iri, Options).
+
+
+
 %! rdf_dcg_lexical_form(+Lex:atom, +Options:dict)// is det.
 
 rdf_dcg_lexical_form(Lex, Options) -->
@@ -78,10 +149,36 @@ rdf_dcg_literal(literal(lang(LTag,Lex)), Options) --> !,
   "@",
   atom(LTag).
 % typed literal
-rdf_dcg_literal(literal(type(D,Lex)), Options) -->
+rdf_dcg_literal(literal(type(D,Lex)), Options) --> !,
   rdf_dcg_lexical_form(Lex, Options),
   "^^",
   rdf_dcg_iri(D, Options).
+rdf_dcg_literal(Term, _) -->
+  {type_error(rdf_literal, Term)}.
+
+
+
+%! rdf_dcg_nonliteral(+Term:rdf_nonliteral, +Options:dict)// is det.
+
+rdf_dcg_nonliteral(BNode, _) -->
+  {rdf_is_bnode(BNode)}, !,
+  atom(BNode).
+rdf_dcg_nonliteral(Iri, Options) -->
+  {rdf_is_iri(Iri)}, !,
+  rdf_dcg_iri(Iri, Options).
+rdf_dcg_nonliteral(Term, _) -->
+  {type_error(rdf_nonliteral, Term)}.
+
+
+
+%! rdf_dcg_nonliteral_or_var(?Term:rdf_nonliteral, +Options:dict)// is det.
+
+rdf_dcg_nonliteral_or_var(NonGround, _) -->
+  {\+ ground(NonGround)}, !,
+  term(NonGround).
+rdf_dcg_nonliteral_or_var(Term, Options) -->
+  rdf_dcg_nonliteral(Term, Options).
+
 
 
 
@@ -92,11 +189,79 @@ rdf_dcg_term(Term) -->
   rdf_dcg_term(Term, _{}).
 
 
-rdf_dcg_term(BNode, _) -->
-  {rdf_is_bnode(BNode)}, !,
-  atom(BNode).
-rdf_dcg_term(Iri, Options) -->
-  {rdf_is_iri(Iri)}, !,
-  rdf_dcg_iri(Iri, Options).
 rdf_dcg_term(Lit, Options) -->
-  rdf_dcg_literal(Lit, Options), !.
+  {rdf_is_literal(Lit)}, !,
+  rdf_dcg_literal(Lit, Options).
+rdf_dcg_term(Term, Options) -->
+  rdf_dcg_nonliteral(Term, Options).
+
+
+
+%! rdf_dcg_term_or_var(?Term:rdf_term, +Options:dict)// is det.
+%! rdf_dcg_term_or_var(?Term:rdf_term, +Options:dict)// is det.
+
+rdf_dcg_term_or_var(Term) -->
+  rdf_dcg_term_or_var(Term, _{}).
+
+
+rdf_dcg_term_or_var(NonGround, _) -->
+  {\+ ground(NonGround)}, !,
+  term(NonGround).
+rdf_dcg_term_or_var(Term, Options) -->
+  rdf_dcg_term(Term, Options).
+
+
+
+%! rdf_dcg_triple(+Triple:rdf_triple)// is det.
+%! rdf_dcg_triple(+Triple:rdf_triple, +Options:dict)// is det.
+%! rdf_dcg_triple(+S:rdf_nonliteral, +P:iri, +O:rdf_term)// is det.
+%! rdf_dcg_triple(+S:rdf_nonliteral, +P:iri, +O:rdf_term, +Options:dict)// is det.
+
+rdf_dcg_triple(Triple) -->
+  rdf_dcg_triple(Triple, _{}).
+
+
+rdf_dcg_triple(rdf(S,P,O), Options) -->
+  rdf_dcg_triple(S, P, O, Options).
+
+
+rdf_dcg_triple(S, P, O) -->
+  rdf_dcg_triple(S, P, O, _{}).
+
+
+rdf_dcg_triple(S, P, O, Options) -->
+  "〈",
+  rdf_dcg_nonliteral(S, Options),
+  ", ",
+  rdf_dcg_iri(P, Options),
+  ", ",
+  rdf_dcg_term(O, Options),
+  "〉".
+
+
+
+%! rdf_dcg_triple_pattern(+TriplePattern:rdf_triple_pattern)// is det.
+%! rdf_dcg_triple_pattern(+TriplePattern:rdf_triple_pattern, +Options:dict)// is det.
+%! rdf_dcg_triple_pattern(?S:rdf_nonliteral, ?P:iri, ?O:rdf_term)// is det.
+%! rdf_dcg_triple_pattern(?S:rdf_nonliteral, ?P:iri, ?O:rdf_term, +Options:dict)// is det.
+
+rdf_dcg_triple_pattern(TriplePattern) -->
+  rdf_dcg_triple_pattern(TriplePattern, _{}).
+
+
+rdf_dcg_triple_pattern(rdf(S,P,O), Options) -->
+  rdf_dcg_triple_pattern(S, P, O, Options).
+
+
+rdf_dcg_triple_pattern(S, P, O) -->
+  rdf_dcg_triple_pattern(S, P, O, _{}).
+
+
+rdf_dcg_triple_pattern(S, P, O, Options) -->
+  "〈",
+  rdf_dcg_nonliteral_or_var(S, Options),
+  ", ",
+  rdf_dcg_iri_or_var(P, Options),
+  ", ",
+  rdf_dcg_term_or_var(O, Options),
+  "〉".
