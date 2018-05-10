b0038d6b6bb9524dba90b4b63c58827c1b655cf3
Wouter Beek <wouter@triply.cc>
2018-05-10 21:48:05 +0200

    FIXED: Typo in Prolog pack file.

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 home('https://github.com/wouterbeek/Pro-RDF').
 keywords(['RDF','Resource Description Framework','Semantic Web']).
 name('Pro-RDF').
-title('Prolog-based RDF library (backend-agnostic)')
+title('Prolog-based RDF library (backend-agnostic)').
 version('0.0.0').
