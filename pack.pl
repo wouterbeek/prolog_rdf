66019523995f0bcaaff87a5b1e6dc0370244def9
Wouter Beek <wouter@triply.cc>
2018-07-07 16:06:10 +0200

    DEPLOY: Use PPM i.o. Prolog Pack.

--- pack.pl
+++
@@ -1,5 +0,0 @@
-home('https://github.com/wouterbeek/prolog_rdf').
-keywords(['Linked Data','RDF','Semantic Web']).
-name(prolog_rdf).
-title('Pro-RDF: Prolog-based RDF library (backend-agnostic).').
-version('0.0.0').
