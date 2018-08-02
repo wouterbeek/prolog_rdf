84217c43740e4c643d4a45b36d57092dbf078dfe
Wouter Beek <wouter@triply.cc>
2018-08-02 23:29:50 +0200

    ADDED: Generalized several RDF predicates.
    CLEAN: Moved SHACL modules to reparate repository.

--- prolog/semweb/rdf_api.pl
+++ prolog/semweb/rdf_api.pl
@@ -1,8 +1,11 @@
 :- module(
   rdf_api,
   [
-    triple/4,      % ?Backend, ?S, ?P, ?O
-    triple_count/5 % ?Backend, ?S, ?P, ?O, -N
+    assert_triple/4,     % ?Backend, +S, +P, +O
+    list_member/3,       % ?Backend, ?X, ?L
+    triple/4,            % ?Backend, ?S, ?P, ?O
+    triple_count/5,      % ?Backend, ?S, ?P, ?O, -N
+    triple_list_member/4 % ?Backend, ?S, ?P, ?X
   ]
 ).
 
@@ -17,20 +20,45 @@ Backend-independent RDF API.
 :- use_module(library(semweb/rdf_prefix)).
 
 :- multifile
+    rdf_api:assert_triple_/4,
     rdf_api:triple_/4,
     rdf_api:triple_count_/5.
 
 :- rdf_meta
+   assert_triple(t, r, r, o),
+   list_member(t, o, r),
    triple(t, r, r, o),
-   triple_count(t, r, r, o, -).
+   triple_count(t, r, r, o, -),
+   triple_list_member(t, r, r, o).
 
+
+
+
+
+%! assert_triple(+Backend, +S:rdf_nonliteral, +P:iri, +O:rdf_term) is nondet.
+
+assert_triple(B, S, P, O) :-
+  rdf_api:assert_triple_(B, S, P, O).
+
+rdf_api:assert_triple_(dummy, _, _, _) :-
+  fail.
+
+
+
+%! list_member(?Backend, ?X:rdf_nonliteral, ?L:rdf_list) is nondet.
+
+list_member(B, X, L) :-
+  triple(B, L, rdf:first, X).
+list_member(B, X, L) :-
+  triple(B, L, rdf:rest, T),
+  list_member(B, X, T).
 
 
 
 %! triple(?Backend, ?S:rdf_nonliteral, ?P:iri, ?O:rdf_term) is nondet.
 
-triple(Backend, S, P, O) :-
-  rdf_api:triple_(Backend, S, P, O).
+triple(B, S, P, O) :-
+  rdf_api:triple_(B, S, P, O).
 
 rdf_api:triple_(dummy, _, _, _) :-
   fail.
@@ -39,8 +67,20 @@ rdf_api:triple_(dummy, _, _, _) :-
 
 %! triple_count(?Backend, ?S:rdf_nonliteral, ?P:iri, ?O:rdf_term, -N:nonneg) is nondet.
 
-triple_count(Backend, S, P, O, N) :-
-  rdf_api:triple_count_(Backend, S, P, O, N).
+triple_count(B, S, P, O, N) :-
+  rdf_api:triple_count_(B, S, P, O, N).
 
 rdf_api:triple_count_(dummy, _, _, _, 0) :-
   fail.
+
+
+
+%! triple_list_member(?Backend, ?S:rdf_nonliteral, ?P:iri, ?X:rdf_term) is nondet.
+
+triple_list_member(B, S, P, X) :-
+  ground(X), !,
+  list_member(B, X, L),
+  triple(B, S, P, L).
+triple_list_member(B, S, P, X) :-
+  triple(B, S, P, L),
+  list_member(B, X, L).
