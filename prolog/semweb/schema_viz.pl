4133ee83d7e87c4a43160aabcc2e475cda534275
Wouter Beek <wouter@triply.cc>
2019-02-06 13:00:48 +0100

    ENHANCED: Allow hierarchies to be exported on a per-parent basis.

--- prolog/semweb/schema_viz.pl
+++ prolog/semweb/schema_viz.pl
@@ -1,40 +1,41 @@
 :- module(
   schema_viz,
   [
-    export_hierarchy/3, % +File, +Backend, +P
-    export_hierarchy/4, % +File, +Backend, +P, +Options
-    view_hierarchy/2,   % +Backend, +P
-    view_hierarchy/3    % +Backend, +P, +Options
+    export_hierarchy/5, % +File, +Backend, ?C, +P, +Options
+    view_hierarchy/4    % +Backend, ?C, +P, +Options
   ]
 ).
 
 /** <module> Schema visualization
 
 @author Wouter Beek
-@version 2017-2018
+@version 2017-2019
 */
 
 :- use_module(library(aggregate)).
 :- use_module(library(apply)).
 :- use_module(library(option)).
+:- use_module(library(ordsets)).
+:- use_module(library(pairs)).
 :- use_module(library(yall)).
 
 :- use_module(library(dcg)).
 :- use_module(library(debug_ext)).
-:- use_module(library(graph/graph_export)).
+:- use_module(library(graph/dot)).
 :- use_module(library(graph/graph_ext)).
+:- use_module(library(graph/gv)).
 :- use_module(library(os_ext)).
-:- use_module(library(semweb/rdf_mem)).
+:- use_module(library(semweb/rdf_api)).
 :- use_module(library(semweb/rdf_prefix)).
 :- use_module(library(semweb/rdf_print)).
 :- use_module(library(semweb/rdf_term)).
-:- use_module(library(uri_ext)).
+:- use_module(library(uri_parse)).
+
+is_meta(label_goal).
 
 :- rdf_meta
-   export_hierarchy(+, t, r),
-   export_hierarchy(+, t, r, +),
-   view_hierarchy(t, r),
-   view_hierarchy(t, r, +).
+   export_hierarchy(+, t, o, r, :),
+   view_hierarchy(t, o, r, :).
 
 
 
@@ -42,7 +43,7 @@
 
 %! export_arc(+Out:stream, +Backend, +Arc:compound, +Options:list(compound)) is det.
 
-export_arc(Out, B, arc(C1,[P],C2), _) :-
+export_arc(Out, _, arc(C1,[P],C2), _) :-
   rdf_prefix_memberchk(P, [rdfs:subClassOf,rdfs:subPropertyOf]), !,
   % By swapping the order in which the nodes are asserted, we are able
   % to show superclasses/superproperties above
@@ -58,27 +59,38 @@ export_arc(Out, B, arc(C1,Ps,C2), Options) :-
 
 
 
-%! export_hierarchy(+File:atom, +Backend, +P:rdf_predicate) is det.
-%! export_hierarchy(+File:atom, +Backend, +P:rdf_predicate, +Options:list(compound)) is det.
+%! export_hierarchy(+File:atom, +Backend, ?C:rdf_term, +P:rdf_predicate, +Options:list(compound)) is det.
 
-export_hierarchy(File, B, P) :-
-  export_hierarchy(File, B, P, []).
-
-
-export_hierarchy(File, B, P, Options) :-
-  export_graph(
+export_hierarchy(File, B, C, P, Options0) :-
+  meta_options(is_meta, Options0, Options1),
+  merge_options([directed(true)], Options1, Options2),
+  gv_export(
     File,
-    {B,P,Options}/[Out]>>export_hierarchy_(Out, B, P, Options),
-    Options
+    {B,C,P,Options1}/[Out]>>export_hierarchy_(Out, B, C, P, Options1),
+    Options2
   ).
 
-export_hierarchy_(Out, B, P, Options) :-
-  aggregate_all(set(arc(C,[P],D)), tp(B, C, P, D), Arcs),
+export_hierarchy_(Out, B, C, P, Options) :-
+  collect_hierarchy_arcs(B, [C], P, []-Arcs),
   maplist({Out,B,Options}/[Arc]>>export_arc(Out, B, Arc, Options), Arcs),
   arcs_to_vertices(Arcs, Nodes),
   maplist({Out,B,Options}/[Node]>>export_node(Out, B, Node, Options), Nodes).
 
-
+collect_hierarchy_arcs(B, Cs1, P, Arcs1-Arcs) :-
+  aggregate_all(
+    set(C2-arc(C2,[P],C1)),
+    (
+      member(C1, Cs1),
+      tp(B, C2, P, C1)
+    ),
+    Pairs
+  ),
+  (   Pairs == []
+  ->  Arcs = Arcs1
+  ;   pairs_keys_values(Pairs, Cs2, Arcs2),
+      ord_union(Arcs1, Arcs2, Arcs3),
+      collect_hierarchy_arcs(B, Cs2, P, Arcs3-Arcs)
+  ).
 
 %! export_node(+Out:stream, +Backend, +Node:rdf_node, +Options:list(compound)) is det.
 %
@@ -93,9 +105,17 @@ export_node(Out, B, Node, Options) :-
 
 
 %! export_node_label(+Backend, +Node:rdf_node, -Label:string, +Options:list(compound)) is det.
+%
+% @arg Options The following options are supported:
+%
+%   * label_goal(Goal_2)
+%   * label_predicate(iri)
 
+export_node_label(_, Node, Label, Options) :-
+  option(label_goal(Goal_2), Options),
+  call(Goal_2, Node, Label), !.
 export_node_label(B, Node, Label, Options) :-
-  option(label_property(P0), Options),
+  option(label_predicate(P0), Options),
   rdf_prefix_iri(P0, P),
   tp(B, Node, P, Label), !.
 export_node_label(_, Node, Label, _) :-
@@ -103,12 +123,12 @@ export_node_label(_, Node, Label, _) :-
 
 
 
-%! view_hierarchy(+Backend, +P:rdf_predicate) is det.
-%! view_hierarchy(+Backend, +P:rdf_predicate, +Options:list(compound)) is det.
+%! view_hierarchy(+Backend, ?C:rdf_term, +P:rdf_predicate, +Options:list(compound)) is det.
 
-view_hierarchy(B, P) :-
-  view_hierarchy(B, P, []).
-
-
-view_hierarchy(B, P, Options) :-
-  view_graph({B,P,Options}/[Out]>>export_hierarchy_(Out, B, P, Options), Options).
+view_hierarchy(B, C, P, Options0) :-
+  meta_options(is_meta, Options0, Options1),
+  merge_options([directed(true)], Options1, Options2),
+  gv_view(
+    {B,C,P,Options1}/[Out]>>export_hierarchy_(Out, B, C, P, Options1),
+    Options2
+  ).
