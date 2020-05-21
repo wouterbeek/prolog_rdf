53687afd8afa62fabbada9de739a171cb09ebab8
Wouter Beek <wouter@triply.cc>
2020-05-21 23:11:09 +0200

    Version 1.0.2.

--- pack.pl
+++ pack.pl
@@ -4,5 +4,8 @@ home('https://github.com/wouterbeek/prolog_rdf').
 maintainer('Wouter Beek', 'wouter@triply.cc').
 name(prolog_rdf).
 packager('Wouter Beek', 'wouter@triply.cc').
+requires(prolog_geo).
+requires(prolog_library_collection).
+requires(prolog_uriparser).
 title('Prolog RDF Library').
-version('1.0.1').
+version('1.0.2').
