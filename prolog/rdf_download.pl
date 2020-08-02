f1ebee0cb86c0c1021d50bbe990e8386a61607a6
Wouter Beek <wouter@triply.cc>
2020-08-02 08:26:09 +0200

    ENHANCED: Allow options to be passed for rdf_download/[1-3].

--- prolog/rdf_download.pl
+++ prolog/rdf_download.pl
@@ -2,7 +2,8 @@
   rdf_download,
   [
     rdf_download/1, % +Uri
-    rdf_download/2  % +Uri, ?File
+    rdf_download/2, % +Uri, ?File
+    rdf_download/3  % +Uri, ?File, +Options
   ]
 ).
 
@@ -24,22 +25,29 @@
 
 %! rdf_download(+Uri:atom) is det.
 %! rdf_download(+Uri:atom, ?File:atom) is det.
+%! rdf_download(+Uri:atom, ?File:atom, +Options:list(compound)) is det.
+%
+% @param Options The options are passed to rdf_deref_uri/3.
 
 rdf_download(Uri) :-
   rdf_download(Uri, _).
 
 
 rdf_download(Uri, File) :-
+  rdf_download(Uri, File, []).
+
+
+rdf_download(Uri, File, Options) :-
   (var(File) -> uri_data_file(Uri, File) ; true),
   (   exists_file(File),
       file_size(File, Size),
       Size =\= 0
   ->  true
-  ;   write_to_file(File, rdf_download_(Uri))
+  ;   write_to_file(File, rdf_download_(Uri, Options))
   ).
 
-rdf_download_(Uri, Out) :-
-  rdf_deref_uri(Uri, callback_(Out)).
+rdf_download_(Uri, Options, Out) :-
+  rdf_deref_uri(Uri, callback_(Out), Options).
 
 callback_(Out, Site, Tuples1, _) :-
   convlist(rdf_clean_tuple(Site), Tuples1, Tuples2),
